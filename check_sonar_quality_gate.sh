561458cb429cde08f8c4258e593ca4f5619aecd4
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-09-02 02:00:01 +0200

    Fix logs

--- check_sonar_quality_gate.sh
+++ check_sonar_quality_gate.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # $1 := SONAR CLOUD PROJECT KEY
-echo "Get quality gate for project $1"
+echo "==> Get quality gate for project $1"
 
 quality_gate_result=$(curl -s "https://sonarcloud.io/api/qualitygates/project_status?projectKey=$1" | python -mjson.tool)
 
-echo "Result: $quality_gate_result"
-
-exit_code = 0
+echo "==> Result: $quality_gate_result"
 
 if echo "$quality_gate_result" | grep -q ERROR; then
-    echo "Found ERROR! Quality gate failure"
+    echo "==> Found ERROR! Quality gate failure"
     exit 1
 else
-    echo "Quality gate passed"
+    echo "==> Quality gate passed"
     exit 0
 fi
