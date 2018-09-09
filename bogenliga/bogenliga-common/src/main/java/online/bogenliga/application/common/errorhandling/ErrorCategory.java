b092fd284708c9316d2f28baf8b0f625d9c3756c
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-09-09 23:06:50 +0200

    SONAR: Fix issue

--- bogenliga/bogenliga-common/src/main/java/online/bogenliga/application/common/errorhandling/ErrorCategory.java
+++ bogenliga/bogenliga-common/src/main/java/online/bogenliga/application/common/errorhandling/ErrorCategory.java
@@ -1,11 +1,7 @@
-//======================================================================================================================
-// Module: BMW Remote Software Update (RSU) - Zentrales Fahrzeug Update System (ZFUS)
-// Copyright (c) 2018 BMW Group. All rights reserved.
-//======================================================================================================================
 package online.bogenliga.application.common.errorhandling;
 
 /**
- * TODO [AL] class documentation
+ * I define the categories for the error codes.
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
