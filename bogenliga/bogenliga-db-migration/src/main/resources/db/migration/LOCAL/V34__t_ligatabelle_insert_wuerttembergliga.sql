3e39776e5d22c75709a9308a03e963e30ee92c3e
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2018-09-22 07:06:20 +0200

    Ligatablle

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V34__t_ligatabelle_insert_wuerttembergliga.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V34__t_ligatabelle_insert_wuerttembergliga.sql
@@ -1,16 +1,29 @@
-INSERT INTO wettkampf ( -- Recurve Ligen
-  wettkampf_id,
-  wettkampf_veranstaltung_id,
-  wettkampf_datum,
-  wettkampf_ort,
-  wettkampf_beginn,
-  wettkampf_tag,
-  wettkampf_disziplin_id
+INSERT INTO ligatablle ( -- Recurve Ligen
+  ligatabelle_veranstaltung_id,
+  ligatabelle_wettkampf_tag,
+  ligatabelle_mannschaft_id,
+  igatabelle_tabellenplatz,
+  ligatabelle_matchpkt,
+  ligatabelle_matchpkt_gegen,
+  ligatabelle_satzpkt,
+  ligatabelle_satzpkt_gegen
 VALUES
-(30, 0, 19.11.2017, 'Sporthalle, 88454 Hochdorf/Riss', '13:30', 1, 0),
-(31, 0, 09.12.2017, 'Franz-Baum Bogenhalle, 73642 Welzheim', '13:30', 2, 0),
-(32, 0, 14.01.2018, 'Bogenhalle am Schützenhaus, Gurgelbergweg, 88499 Altheim-Waldhausen', '13:30', 3, 0),
-(33, 0, 03.02.2018, 'Franz-Baum Bogenhalle, 73642 Welzheim', '9:30', 4, 0)
+(0, 0, 101, 1, 0,0,0,0),-- initial
+(0, 0, 102, 2, 0,0,0,0),
+(0, 0, 103, 3, 0,0,0,0),
+(0, 0, 104, 4, 0,0,0,0),
+(0, 0, 105, 5, 0,0,0,0),
+(0, 0, 106, 6, 0,0,0,0),
+(0, 0, 107, 7, 0,0,0,0),
+(0, 0, 108, 8, 0,0,0,0),
+(0, 1, 105, 1, 12,2,36,14),-- Wettkampftag1
+(0, 1, 103, 2, 10,4,36,24),
+(0, 1, 102, 3, 10,4,34,26),
+(0, 1, 107, 4, 8,6,34,24),
+(0, 1, 101, 5, 6,8,28,34),
+(0, 1, 106, 6, 6,8,22,30),
+(0, 1, 108, 7, 4,10,23,37),
+(0, 1, 104, 8, 0,14,15,43),
 ;
 
 
---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/all/V33__t_ligatabelle__create.sql
@@ -0,0 +1,39 @@
+/*
+ * general conventions:
+ * - lower case
+ * - use "_"
+ * - use table name for column prefixes
+ *
+ * example:
+ * table name = "user"
+ * column prefix = "user_"
+ */
+
+/**
+ * Eine Klasse dient der Dokumentation der LigaTabelle zu Beginn der Liga (manuell) und nach
+ * jedem Wettkampftag gem der Match und Satzpunkte
+ **/
+CREATE TABLE ligatabelle (
+  ligatabelle_veranstaltung_id   DECIMAL(19,0) NOT NULL, --bezug zum Sportjahr
+  ligatabelle_wettkampf_tag      DECIMAL(1,0) NOT NULL, -- Liga hat 4 Wettkampftage, initiale Ligatabelle für Tag 0
+  ligatabelle_mannschaft_id      DECIMAL(19,0) NOT NULL, --bezug zur Mannschaft
+  ligatabelle_tabellenplatz          DECIMAL(1,0) NOT NULL, -- resultierender Tabellenplatz nach dem Wettkampf
+  ligatabelle_matchpkt               DECIMAL(4,0) NOT NULL, -- Summe aller eigene Matchpunkte
+  ligatabelle_matchpkt_gegen         DECIMAL(4,0) NOT NULL, -- Summe aller gegnerischen Matchpunkte
+  ligatabelle_satzpkt                DECIMAL(4,0) NOT NULL, -- Summe aller eigenen Satzpunkte
+  ligatabelle_satzpkt_gegen          DECIMAL(4,0) NOT NULL, -- Summe aller gegnerischen Satzpunkte
+
+
+  CONSTRAINT uc_ligatabelle UNIQUE (ligatabelle_veranstaltung_id, ligatabelle_wettkampf_tag, mannschaft_id),
+
+
+   -- foreign key (fk)
+  -- schema: fk_{current table name}_{foreign key origin table name}
+  CONSTRAINT fk_ligatabelle_veranstaltung FOREIGN KEY (ligatabelle_veranstaltung_id) REFERENCES veranstaltung (veranstaltung_id)
+    ON DELETE CASCADE, -- das Löschen eines wettkampftyps löscht auch die zugehörigen wettkämpfe
+
+  CONSTRAINT fk_ligatabelle_mannschaft FOREIGN KEY (ligatabelle_mannschaft_id) REFERENCES mannschaft (mannschaft_id)
+    ON DELETE CASCADE -- das Löschen eines wettkampftyps löscht auch die zugehörigen wettkämpfe
+
+
+);
