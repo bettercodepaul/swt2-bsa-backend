f6f583226732f7592b3a27e63a4c09c7ed4c3251
aeble <alexa.eble@exxcellent.de>
2018-09-27 16:16:53 +0200

    Cross Origins

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/configuration/controller/ConfigurationService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/configuration/controller/ConfigurationService.java
@@ -6,11 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import de.bogenliga.application.business.configuration.api.ConfigurationComponent;
 import de.bogenliga.application.business.configuration.api.types.ConfigurationVO;
 import de.bogenliga.application.common.service.ServiceFacade;
@@ -33,6 +29,7 @@ import de.bogenliga.application.services.v1.configuration.model.ConfigurationDTO
  * @see <a href="https://www.baeldung.com/spring-autowire">Guide to Spring @Autowired</a>
  */
 @RestController
+@CrossOrigin
 @RequestMapping("v1/configuration")
 public class ConfigurationService implements ServiceFacade {
 
