a9666533d230e8832d8bd4505c6e9add47ff892e
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-02 16:32:45 +0200

    BSAPP-14: Modify session to login history

---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V46__benutzer_login_verlauf__insert_test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO benutzer_login_verlauf (benutzer_login_verlauf_benutzer_id,
+                                    benutzer_login_verlauf_timestamp,
+                                    benutzer_login_verlauf_login_ergebnis)
+VALUES (2, '2018-09-30 18:36:17.701184', 'LOGIN_FAILED'),
+       (2, '2018-09-30 18:40:17.701184', 'LOGIN_SUCCESS')
--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V46__session__insert_test_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO session(
-session_benutzer_id,
-session_token,
-session_timestamp
-)
-VALUES
-(2, 'jwttoken124124', '2018-09-30 18:40:17.701184')
