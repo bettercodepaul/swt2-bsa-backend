5376d9afd248c14a84ce0341f4001ed31a1347a1
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-10-03 12:07:08 +0200

    BSAPP-14: Salt and hash value generation

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/businessactivity/PasswordHashingBA.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/businessactivity/PasswordHashingBA.java
@@ -1,16 +1,83 @@
 package de.bogenliga.application.business.user.impl.businessactivity;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.UUID;
 import org.springframework.stereotype.Component;
+import de.bogenliga.application.common.errorhandling.ErrorCode;
+import de.bogenliga.application.common.errorhandling.exception.TechnicalException;
 
 /**
  * TODO [AL] class documentation
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ *
+ * @see <a href="https://crackstation.net/hashing-security.htm">Salted Password Hashing - Doing it Right</a>
  */
 @Component
 public class PasswordHashingBA {
 
-    public String calculateHash(final String password, final String salt) {
-        return password;
+
+    private static final String SHA_512 = "SHA-512";
+
+
+    private String convertBytesToHex(byte[] bytes) {
+        String generatedPassword;
+        final StringBuilder sb = new StringBuilder();
+        for (final byte aByte : bytes) {
+            sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+        }
+        generatedPassword = sb.toString();
+        return generatedPassword;
+    }
+
+
+    /**
+     * Generate a hashed password.
+     *
+     * @param password plaintext password
+     * @param salt     user specific salt
+     *
+     * @return sha-512 hash value
+     */
+    String calculateHash(final String password, final String salt) {
+
+        try {
+            final MessageDigest md = MessageDigest.getInstance(SHA_512);
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            final byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            return convertBytesToHex(bytes);
+        } catch (final NoSuchAlgorithmException e) {
+            throw new TechnicalException(ErrorCode.UNEXPECTED_ERROR, e);
+        }
+    }
+
+
+    /**
+     * Generate a salt fo a new user.
+     * <p>
+     * Use a random and a unique part.
+     *
+     * @return sha-512 hash value
+     */
+    String generateSalt() {
+        try {
+            final SecureRandom random = new SecureRandom();
+            final byte[] randomSeed = new byte[100];
+            random.nextBytes(randomSeed);
+
+            byte[] uniqueSeed = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+
+            final MessageDigest md = MessageDigest.getInstance(SHA_512);
+            md.update(randomSeed);
+            final byte[] bytes = md.digest(uniqueSeed);
+
+            return convertBytesToHex(bytes);
+
+        } catch (final NoSuchAlgorithmException e) {
+            throw new TechnicalException(ErrorCode.UNEXPECTED_ERROR, e);
+        }
     }
 }
---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/user/impl/businessactivity/PasswordHashingBATest.java
@@ -0,0 +1,72 @@
+package de.bogenliga.application.business.user.impl.businessactivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+@SuppressWarnings({"pmd-unit-tests:JUnitTestsShouldIncludeAssert", "squid:S2187"})
+public class PasswordHashingBATest {
+
+    private static final String HASH_VALUE = "2908d2c28dfc047741fc590a026ffade237ab2ba7e1266f010fe49bde548b5987a534a86655a0d17f336588e540cd66f67234b152bbb645b4bb85758a1325d64";
+    private static final String PASSWORD = "password";
+    private static final String SALT = "salt";
+    private static final int EXPECTED_SIZE = 128;
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @InjectMocks
+    private PasswordHashingBA underTest;
+
+
+    @Test
+    public void calculateHash() {
+        final String actual = underTest.calculateHash("swt2",
+                "7e287c3b7f9f8a7193f007347d7fc784a647536892bd35fc7daee318917aa7544dff21b6454e56786cf8b9263df89811c3bf14309e04025d6b8e369240f61746");
+
+        assertThat(actual)
+                .hasSize(EXPECTED_SIZE)
+                .isEqualTo(HASH_VALUE);
+    }
+
+
+    @Test
+    public void calculateHash_withDifferentSalt() {
+        final String actual = underTest.calculateHash(PASSWORD, "other");
+
+        assertThat(actual)
+                .hasSize(EXPECTED_SIZE)
+                .isNotEqualTo(HASH_VALUE);
+    }
+
+
+    @Test
+    public void calculateHash_withDifferentPassword() {
+        final String actual = underTest.calculateHash("other", SALT);
+
+        assertThat(actual)
+                .hasSize(EXPECTED_SIZE)
+                .isNotEqualTo(HASH_VALUE);
+    }
+
+
+    @Test
+    public void generateSalt() {
+        final String actual = underTest.generateSalt();
+
+        assertThat(actual).hasSize(EXPECTED_SIZE).isEqualTo("");
+
+        final String other = underTest.generateSalt();
+
+        assertThat(other)
+                .hasSize(EXPECTED_SIZE)
+                .isNotEqualTo(actual);
+
+    }
+}
