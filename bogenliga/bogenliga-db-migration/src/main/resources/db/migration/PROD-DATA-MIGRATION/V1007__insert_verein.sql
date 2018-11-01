41c752ff2dea90c7b8ee43ddc3821bbe389e4f05
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2018-11-01 17:29:37 +0100

    versuch doppelte vereinsname (tailing number) zu unterdrücken...

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1007__insert_verein.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1007__insert_verein.sql
@@ -1,7 +1,12 @@
-INSERT INTO public.verein (verein_name, verein_dsb_identifier)
-SELECT Verein,
-       vnr,
+
+INSERT INTO public.verein (verein_name, verein_dsb_identifier, verein_region_id)
+SELECT SUBSTRING ("vereine"."Verein",'([A-Z,a-z, ,-]{1,200})'),
+       "vereine"."VNR",
        1
-FROM prod_data_migration.Vereine
-WHERE vnr not in ('36WT919999', '36WT929999', '36WT939999', '36WT949999')
+FROM prod_data_migration."vereine"
+WHERE "VNR" not in ('36WT919999', '36WT929999', '36WT939999', '36WT949999')
+AND "aktiviert" = '1'
+AND SUBSTRING ("vereine"."Verein",'([0-9]{1,1})')=''
 ;
+
+
