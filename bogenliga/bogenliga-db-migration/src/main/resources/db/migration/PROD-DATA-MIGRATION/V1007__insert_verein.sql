aae1f5a1b4fa85dd5e882fccd3ce06335f2bd5a6
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2018-11-01 18:06:37 +0100

    alter table vereine um Mannschaft und Vereinsname zu trennen

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1007__insert_verein.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1007__insert_verein.sql
@@ -1,12 +1,38 @@
+--leider mischt die alter Datenstruktur die Informationen Vereinsname und Mannschaftsnummer
+--dass müssen wir für die Migration trennen
+--dazu fügen wir eine Spalte Mannschaft hinzu und füllen für alle den Default 0
+--danach schneiden wir aus dem Vereinsnamen evtl. vorhandenen Nummern aus
+--und über schreiben die 0
+
+-- spalte hinzufügen
+ALTER TABLE prod_data_migration."vereine"
+  ADD COLUMN "Mannschaft" text DEFAULT '0'
+;
+Commit;
+
+
+Update prod_data_migration."vereine"
+SET "Mannschaft" = SUBSTRING ("Verein",'([1-9]{1,1})')
+WHERE SUBSTRING ("vereine"."Verein",'([0-9]{1,1})')<>''
+;
+
+
+
+Update prod_data_migration."vereine"
+SET "Verein" = SUBSTRING ("Verein",'([A-Z,a-z,ä,ö,ü,Ä,Ü,Ö,ß, ,-]{1,200})')
+;
+
+Delete from prod_data_migration."vereine"
+where "VNR" in ('36WT919999', '36WT929999', '36WT939999', '36WT949999')
+;
 
 INSERT INTO public.verein (verein_name, verein_dsb_identifier, verein_region_id)
-SELECT SUBSTRING ("vereine"."Verein",'([A-Z,a-z, ,-]{1,200})'),
+SELECT "vereine"."Verein",
        "vereine"."VNR",
        1
 FROM prod_data_migration."vereine"
-WHERE "VNR" not in ('36WT919999', '36WT929999', '36WT939999', '36WT949999')
-AND "aktiviert" = '1'
-AND SUBSTRING ("vereine"."Verein",'([0-9]{1,1})')=''
+WHERE "aktiviert" = '1'
+GROUP BY "vereine"."Verein"
 ;
 
 
