3483e8dd99a3fa799449f0dd5320d51a578cef78
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2018-11-02 07:04:52 +0100

    aufräumen, regionen um landesverbände ergänzt

---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1006__delete_waste_tables.sql
@@ -0,0 +1,24 @@
+
+-- löschen aller Tabellen, die keinen Inhalt haben, bzw. als temp. Tabellen
+-- für Import/Export vom Altsystem genutzt werden
+--
+DROP TABLE prod_data_migration."a_schgegauswahl";
+DROP TABLE prod_data_migration."a_schheimauswahl";
+DROP TABLE prod_data_migration."a_schzaehlerauswahl";
+DROP TABLE prod_data_migration."a_tabelleauswahl";
+DROP TABLE prod_data_migration."a_wkplkurzauswahl";
+DROP TABLE prod_data_migration."aktuelleliga";
+DROP TABLE prod_data_migration."aktuellerwettkampf";
+DROP TABLE prod_data_migration."exportschuetzenliste";
+DROP TABLE prod_data_migration."exporttabelle";
+DROP TABLE prod_data_migration."Exportwettkampfplan";
+DROP TABLE prod_data_migration."Finale";
+DROP TABLE prod_data_migration."Finalendtabelle";
+DROP TABLE prod_data_migration."Finalmitarbeiter";
+DROP TABLE prod_data_migration."Finalwettkampfplan";
+
+
+
+
+
+
