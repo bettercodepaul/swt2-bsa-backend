a077fd5fcecee8580cfaf6f15cca0970a1d84283
fylip97 <phillip.dengler@student.reutlingen-university.de>
2018-11-08 16:21:53 +0100

    klassen angelegt

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/mapper/DsbMannschaftDTOMapper.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/mapper/DsbMannschaftDTOMapper.java
@@ -1,9 +1,68 @@
 package de.bogenliga.application.services.v1.dsbmannschaft.mapper;
 
+import java.util.function.Function;
+import de.bogenliga.application.business.dsbmitglied.api.types.DsbMitgliedDO;
+import de.bogenliga.application.common.service.mapping.DataTransferObjectMapper;
+import de.bogenliga.application.services.v1.dsbmitglied.model.DsbMitgliedDTO;
+
 /**
- * TODO [AL] class documentation
+
+ * I map the {@link DsbMannschaftDO} and {@link DsbMannschaftDTO} objects
  *
- * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ * @author Philip Dengler
  */
-public class DsbMannschaftDTOMapper {
+public class DsbMannschaftDTOMapper implements DataTransferObjectMapper {
+
+    public static final Function<DsbMannschaftDO, DsbMannschaftDTO> toDTO = dsbMannschaftdDO -> {
+
+
+        final Long dsbMannschaftId = dsbMannschaftDo.getId();
+        final Long dsbMannschaftVereinId = dsbMannschaftDo.getVereinId();
+        final Long dsbMannschaftNummer = dsbMannschaftDo.getNummer();
+        final Long dsbMannschaftBenutzerId = dsbMannschaftDo.getBenutzerId();
+        final Long dsbMannschaftVeranstalungId = dsbMannschaftDO.getVeranstalungId();
+
+        return new dsbMannschftDTO(dsbMannschaftId,
+                dsbMannschaftVereinId,
+                dsbMannschaftNummer,
+                dsbMannschaftBenutzerId,
+                dsbMannschaftVeranstalungId);
+
+
+
+    };
+
+    /**
+     * I map the {@link DsbMannschaftDTO} object to the {@link DsbMannschaftDO} object
+     */
+
+
+    public static final Function<DsbMannschaftdDTO, DsbMannschaftDO> toDO = dto -> {
+
+
+
+        final Long dsbMannschaftId = dto.getId();
+        final Long dsbMannschaftVereinId = dto.getVereinId();
+        final Long dsbMannschaftNummer = dto.getNummer();
+        final Long dsbMannschaftBenutzerId = dto.getBenutzerId();
+        final Long dsbMannschaftVeranstalungId = dto.getVeranstalungId();
+
+        return new dsbMannschaftDO(dsbMannschaftId,
+                dsbMannschaftVereinId,
+                dsbMannschaftNummer,
+                dsbMannschaftBenutzerId,
+                dsbMannschaftVeranstalungId);
+
+    };
+
+
+
+    /**
+     * Constructor
+     */
+
+    private DsbMannschaftDTOMapper() {
+        // empty private constructor
+    }
+
 }
