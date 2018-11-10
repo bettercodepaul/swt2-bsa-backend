d41569282a0676058edc894adb88c0c2443aecb7
fylip97 <phillip.dengler@student.reutlingen-university.de>
2018-11-10 16:54:47 +0100

    test mannschaft application klasse angelegt

---
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/dsbmannschaft/service/DsbMannschaftServiceTest.java
@@ -0,0 +1,14 @@
+package de.bogenliga.application.services.v1.dsbmannschaft.service;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Philip dengler
+ */
+public class DsbMannschaftServiceTest {
+
+    private static final long User = 0;
+    private static final long ID = 1893;
+
+
+}
