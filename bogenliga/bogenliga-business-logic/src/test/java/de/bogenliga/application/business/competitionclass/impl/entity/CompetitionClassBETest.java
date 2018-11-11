ad541886475d23f89011081e17ac2c6a5e0420f5
LTaraman <leila.taraman@student.reutlingen-university.de>
2018-11-11 12:19:14 +0100

    CompetitionClass Tests for CompClass-DAO-BE & Mapper

---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/competitionclass/impl/entity/CompetitionClassBETest.java
@@ -0,0 +1,52 @@
+package de.bogenliga.application.business.competitionclass.impl.entity;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Leila Taraman, Leila.Taraman@Student.Reutlingen-University.DE
+ */
+public class CompetitionClassBETest {
+
+    private static final long USER = 0;
+
+    private static final long ID = 42;
+    private static final String KLASSENAME = "Herren";
+    private static final long KLASSEALTERMIN = 10;
+    private static final long KLASSEALTERMAX = 50;
+    private static final long KLASSENR = 1337;
+
+
+    @Test
+    public void assertToString(){
+        final CompetitionClassBE underTest = getCompetitionClassBE();
+        underTest.setKlasseId(ID);
+        underTest.setKlasseName(KLASSENAME);
+
+        final String actual = underTest.toString();
+
+        assertThat(actual)
+                .isNotEmpty()
+                .contains(Long.toString(ID))
+                .contains(KLASSENAME);
+
+    }
+
+    @Test
+    public void assertToStringWithOutKlasseName(){
+        final CompetitionClassBE underTest = new CompetitionClassBE();
+        underTest.setKlasseId(ID);
+        underTest.setKlasseName(null);
+
+        final String actual = underTest.toString();
+
+        assertThat(actual)
+                .isNotEmpty()
+                .contains(Long.toString(ID))
+                .contains("null");
+    }
+
+}
---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/competitionclass/impl/mapper/CompetitionClassMapperTest.java
@@ -0,0 +1,48 @@
+package de.bogenliga.application.business.competitionclass.impl.mapper;
+
+import org.junit.Test;
+import de.bogenliga.application.business.competitionclass.api.types.CompetitionClassDO;
+import de.bogenliga.application.business.competitionclass.impl.entity.CompetitionClassBE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Leila Taraman, Leila.Taraman@Student.Reutlingen-University.DE
+ */
+public class CompetitionClassMapperTest {
+
+    private static final long USER = 0;
+
+    private static final long ID = 42;
+    private static final String KLASSENAME = "Herren";
+    private static final long KLASSEALTERMIN = 10;
+    private static final long KLASSEALTERMAX = 50;
+    private static final long KLASSENR = 1337;
+
+    @Test
+    public void toVO() throws Exception{
+
+        final CompetitionClassBE competitionClassBE = getCompetitionClassBE();
+
+        final CompetitionClassDO actual = CompetitionClassMapper.toCompetitionClassDO.apply(competitionClassBE);
+
+        assertThat(actual.getId()).isEqualTo(ID);
+        assertThat(actual.getKlasseName()).isEqualTo(KLASSENAME);
+
+    }
+
+    @Test
+    public void toBE() throws Exception{
+
+        final CompetitionClassDO competitionClassDO = getCompetitionClassDO();
+
+        final CompetitionClassBE actual = CompetitionClassMapper.toCompetitionClassBE.apply(competitionClassDO);
+
+        assertThat(actual.getKlasseId()).isEqualTo(ID);
+        assertThat(actual.getKlasseName()).isEqualTo(KLASSENAME);
+
+    }
+
+}
