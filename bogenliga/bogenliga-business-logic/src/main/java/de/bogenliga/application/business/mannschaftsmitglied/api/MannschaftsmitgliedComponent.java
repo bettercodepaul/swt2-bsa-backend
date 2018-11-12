c984b95a5daf44cde46ec62f3b776ded7e43a157
fylip97 <phillip.dengler@student.reutlingen-university.de>
2018-11-12 14:28:25 +0100

    klassen für mannschaftsmitglied in business logik angelegt

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/api/MannschaftsmitgliedComponent.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.mannschaftsmitglied.api;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public interface MannschaftsmitgliedComponent {
+}
---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/impl/dao/MannschaftsmitgliedDAO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.mannschaftsmitglied.impl.dao;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class MannschaftsmitgliedDAO {
+}
