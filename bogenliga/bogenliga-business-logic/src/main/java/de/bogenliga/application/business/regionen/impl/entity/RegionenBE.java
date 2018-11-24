409e8e36957aa7804b8c0aacc903a09452077e64
DennisGoe <dennis.goericke@student.reutlingen-university.de>
2018-11-24 13:07:12 +0100

    setup Folder structure for businessLayer Regionen

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/regionen/impl/entity/RegionenBE.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.regionen.impl.entity;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Dennis Goericke, dennis.goericke@student.reutlingen-university.de
+ */
+public class RegionenBE {
+}
