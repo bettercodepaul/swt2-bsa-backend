fc2ac4e2567ba61362af3538bf745497b31d69f5
DennisGoe <dennis.goericke@student.reutlingen-university.de>
2018-11-24 13:07:12 +0100

    setup Folder structure for businessLayer Regionen

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/regionen/impl/business/RegionenComponentImpl.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.regionen.impl.business;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Dennis Goericke, dennis.goericke@student.reutlingen-university.de
+ */
+public class RegionenComponentImpl {
+}
---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/regionen/impl/dao/RegionenDAO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.regionen.impl.dao;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Dennis Goericke, dennis.goericke@student.reutlingen-university.de
+ */
+public class RegionenDAO {
+}
