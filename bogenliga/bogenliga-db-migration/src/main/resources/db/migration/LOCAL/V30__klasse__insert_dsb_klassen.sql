930fe679d2b1c1dc7782cc8dc0a44386ec68eb2c
LTaraman <leila.taraman@student.reutlingen-university.de>
2018-12-03 20:23:32 +0100

    BSAPP-186 edit database testdata

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V30__klasse__insert_dsb_klassen.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V30__klasse__insert_dsb_klassen.sql
@@ -1,20 +1,20 @@
 INSERT INTO klasse (klasse_id, klasse_name, klasse_jahrgang_min, klasse_jahrgang_max, klasse_nr)
 VALUES
 
-(0,'Herren' ,21, 49 ,10),
-(1,'Damen' ,21 ,49 ,11),
-(2,'Schüler A männl.' ,13 ,14 ,20),
-(3,'Schüler A weibl.' ,13 ,14 ,21),
-(4,'Schüler B männl.' ,11 ,12 ,22),
-(5,'Schüler B weibl.' ,11 ,12 ,23),
-(6,'Jugend männl.' ,15 ,17 ,30),
-(7,'Jugend weibl.' ,15 ,17 ,31),
-(8,'Junioren' ,18 ,20 ,40),
-(9,'Junioren weibl.' ,18 ,20 ,41),
-(10,'Master m' ,50 ,65 ,12),
-(11,'Master w' ,50 ,65 ,13),
-(12,'Senioren' ,66 ,99 ,14),
-(13,'Senioren weibl.' ,66 ,99 ,15),
-(14,'unbestimmt' ,0 ,99 ,16)
+(0,'Herren' ,1921, 1949 ,10),
+(1,'Damen' ,1921 ,1949 ,11),
+(2,'Schüler A männl.' ,1913 ,1914 ,20),
+(3,'Schüler A weibl.' ,1913 ,1914 ,21),
+(4,'Schüler B männl.' ,1911 ,1912 ,22),
+(5,'Schüler B weibl.' ,1911 ,1912 ,23),
+(6,'Jugend männl.' ,1915 ,1917 ,30),
+(7,'Jugend weibl.' ,1915 ,1917 ,31),
+(8,'Junioren' ,1918 ,1920 ,40),
+(9,'Junioren weibl.' ,1918 ,1920 ,41),
+(10,'Master m' ,1950 ,1965 ,12),
+(11,'Master w' ,1950 ,1965 ,13),
+(12,'Senioren' ,1966 ,1999 ,14),
+(13,'Senioren weibl.' ,1966 ,1999 ,15),
+(14,'unbestimmt' ,1900 ,1999 ,16)
 ;
 
