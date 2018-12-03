46be67b8b48c324bba0abc0ed9c5b19255a9dbcc
Yann Philippczyk <yann.philippczyk@exxcellent.de>
2018-12-03 11:01:20 +0100

    WIP: Userprofil

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/business/UserProfileComponentImpl.java
@@ -0,0 +1,75 @@
+package de.bogenliga.application.business.user.impl.business;
+
+import de.bogenliga.application.business.dsbmitglied.impl.dao.DsbMitgliedDAO;
+import de.bogenliga.application.business.dsbmitglied.impl.entity.DsbMitgliedBE;
+import de.bogenliga.application.business.user.api.UserComponent;
+import de.bogenliga.application.business.user.api.UserProfileComponent;
+import de.bogenliga.application.business.user.api.types.UserProfileDO;
+import de.bogenliga.application.business.user.impl.dao.UserDAO;
+import de.bogenliga.application.business.user.impl.entity.UserBE;
+import de.bogenliga.application.business.user.impl.mapper.UserMapper;
+import de.bogenliga.application.common.errorhandling.ErrorCode;
+import de.bogenliga.application.common.errorhandling.exception.BusinessException;
+import de.bogenliga.application.common.validation.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link UserComponent}
+ */
+@Component
+public class UserProfileComponentImpl implements UserProfileComponent {
+
+    private static final String PRECONDITION_MSG_USER_ID = "UserDO ID must not be negative";
+    private final UserDAO userDAO;
+    private final DsbMitgliedDAO dsbMitgliedDAO;
+
+
+    /**
+     * Constructor
+     * <p>
+     * dependency injection with {@link Autowired}
+     *
+     * @param userDAO         to access the database and return user representations
+     * @param signInBA        to sign in users
+     * @param technicalUserBA to handle all technical user operations
+     */
+    @Autowired
+    public UserProfileComponentImpl(final UserDAO userDAO,
+                                    final DsbMitgliedDAO dsbMitgliedDAO) {
+        this.userDAO = userDAO;
+        this.dsbMitgliedDAO = dsbMitgliedDAO;
+    }
+
+
+    @Override
+    public UserProfileDO findById(final Long id) {
+        Preconditions.checkNotNull(id, PRECONDITION_MSG_USER_ID);
+        Preconditions.checkArgument(id >= 0, PRECONDITION_MSG_USER_ID);
+
+        //TODO
+        //if (id == 0) {
+        //    // 0 identifies the SYSTEM user
+        //    return technicalUserBA.getSystemUser();
+        //}
+
+        UserProfileDO userProfileDO = new UserProfileDO();
+
+        final UserBE userBE = userDAO.findById(id);
+        final DsbMitgliedBE dsbMitgliedBE = dsbMitgliedDAO.findByUserId(id);
+
+        if (userBE == null) {
+            throw new BusinessException(ErrorCode.ENTITY_NOT_FOUND_ERROR,
+                    String.format("No result found for ID '%s'", id));
+        }
+        if (dsbMitgliedBE == null) {
+            userProfileDO.setEmail(userBE.getUserEmail());
+        } else {
+            userProfileDO = UserMapper.toUserProfileDO.apply(userBE, dsbMitgliedBE);
+        }
+
+
+        return userProfileDO;
+    }
+
+}
