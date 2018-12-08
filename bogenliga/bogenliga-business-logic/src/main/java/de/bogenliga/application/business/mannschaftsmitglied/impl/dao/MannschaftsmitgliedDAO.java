0ef7abbd4047fff0e3944dcdb0fb081dcc63a50f
Kaywan <kaywan.barzani@student.reutlingen-university.de>
2018-12-08 18:45:37 +0100

    implemented update and delete in mannschaftsmitglied

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/impl/dao/MannschaftsmitgliedDAO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/impl/dao/MannschaftsmitgliedDAO.java
@@ -123,14 +123,14 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
         basicDao.setModificationAttributes(mannschaftsmitgliedBE, currentMemberId);
 
 
-      //  return basicDao.updateEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE, MANNSCHAFTSMITGLIED_BE_TEAM_ID,MANNSCHAFTSMITGLIED_BE_USER_ID);
-        return null;
+     return basicDao.updateEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE, MANNSCHAFTSMITGLIED_BE_USER_ID);
+
     }
 
     public void delete(final MannschaftsmitgliedBE mannschaftsmitgliedBE, final long currentMemberId) {
         basicDao.setModificationAttributes(mannschaftsmitgliedBE, currentMemberId);
 
-        basicDao.deleteEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE,MANNSCHAFTSMITGLIED_BE_TEAM_ID);
+        basicDao.deleteEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE,MANNSCHAFTSMITGLIED_BE_USER_ID);
 
     }
 
