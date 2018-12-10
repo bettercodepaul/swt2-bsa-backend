46fc927089fca9787d221bcf54b845d047d13d51
Bruno Michael Cunha Teixeira <brunomichaelt@gmail.com>
2018-12-10 18:10:19 +0100

    BSAPP-217: Delted liga Deleted Liga because it was pushed to the wrong branch

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/business/LigaComponentImpl.java
+++
@@ -1,9 +0,0 @@
-package de.bogenliga.application.business.liga.impl.business;
-
-/**
- * TODO [AL] class documentation
- *
- * @author Bruno Michael Cunha Teixeira, Bruno_Michael.Cunha_teixeira@Student.Reutlingen-University.de
- */
-public class LigaComponentImpl {
-}
