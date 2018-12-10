d5771830d2ddd2b1fcfe42521b12bb0c4f9285a2
BrunoCunhaTeixeira <brunomichaelt@gmail.com>
2018-12-10 12:49:01 +0100

    BSAPP-217: Created Classes for BSAPP-217

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/api/LigaComponent.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.liga.api;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Bruno Michael Cunha Teixeira, Bruno_Michael.Cunha_teixeira@Student.Reutlingen-University.de
+ */
+public interface LigaComponent {
+}
---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/entity/LigaBE.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.liga.impl.entity;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Bruno Michael Cunha Teixeira, Bruno_Michael.Cunha_teixeira@Student.Reutlingen-University.de
+ */
+public class LigaBE {
+}
