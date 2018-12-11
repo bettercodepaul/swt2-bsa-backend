32cfc2495c2a109d7ff7af054a459b53b95744d7
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-12-11 10:27:06 +0100

    Merge master branch; Add table scripts

---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V32__wettkampftyp__insert_dsb_wettkampftypen.sql
@@ -0,0 +1,10 @@
+INSERT INTO wettkampftyp (wettkampftyp_id, wettkampftyp_name)
+VALUES (0, 'Liga kummutativ'),
+       (1, 'Liga Satzsystem'),
+       (2, 'WA im Freien'),
+       (3, 'WA in der Halle'),
+       (4, 'WA Stern Turnier'),
+       (5, 'Feldbogen'),
+       (6, '3D Turnier')
+;
+
