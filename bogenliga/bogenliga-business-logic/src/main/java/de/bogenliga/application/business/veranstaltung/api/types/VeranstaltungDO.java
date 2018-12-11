3361f1cd39d5aea382b80f85b500f11704a70de9
holmma <Marvin.Holm@Student.Reutlingen-University.de>
2018-12-11 15:11:31 +0100

    veranstaltungsklassen angelegt

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/veranstaltung/api/types/VeranstaltungDO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.veranstaltung.api.types;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */swt2
+public class VeranstaltungDO {
+}
