b2ecfd5f4712186a1dc44c936f0c7d0fc26dcd99
BrunoCunhaTeixeira <brunomichaelt@gmail.com>
2018-12-11 19:27:54 +0100

    BSAPP-217: LigaDAO and LigaBE implemented Comment in LigaDO Implemented interface in LigaComponentImpl

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/dao/LigaDAO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/dao/LigaDAO.java
@@ -1,4 +1,99 @@
 package de.bogenliga.application.business.liga.impl.dao;
 
-public class LigaDAO {
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import de.bogenliga.application.business.liga.impl.entity.LigaBE;
+import de.bogenliga.application.common.component.dao.BasicDAO;
+import de.bogenliga.application.common.component.dao.BusinessEntityConfiguration;
+import de.bogenliga.application.common.component.dao.DataAccessObject;
+
+/**
+ *
+ * DataAccessObject for the liga entity in the database.
+ *
+ * Use a {@link BusinessEntityConfiguration} for each entity to configure the generic {@link BasicDAO} methods
+ *
+ * @author Bruno Michael Cunha Teixeira, Bruno_Michael.Cunha_teixeira@Student.Reutlingen-University.de
+ */
+public class LigaDAO implements DataAccessObject {
+
+    // define the logger context
+    private static final Logger LOGGER = LoggerFactory.getLogger(LigaDAO.class);
+
+    // table name in the database
+    private static final String TABLE = "liga";
+
+    //business entity parameter names
+    private static final String LIGA_BE_ID="ligaId";
+    private static final String LIGA_BE_NAME="ligaName";
+    private static final String REGION_BE_ID="ligaRegionId";
+    private static final String REGION_BE_NAME="ligaRegionName";
+    private static final String LIGA_BE_UEBERGEORDNET_ID="ligaUebergeordnetId";
+    private static final String LIGA_BE_UEBERGEORDNET_NAME="ligaUebergeordnetName";
+    private static final String LIGA_BE_VERANTWORTLICH_ID="ligaVerantwortlichId";
+    private static final String LIGA_BE_VERANTWORTLICH_MAIL="ligaVerantwortlichMail";
+    private static final String LIGA_BE_USER_ID="ligaUserId";
+
+    private static final String LIGA_TABLE_ID="liga_id";
+    private static final String LIGA_TABLE_NAME="liga_name";
+    private static final String REGION_TABLE_ID="liga_region_id";
+    private static final String REGION_TABLE_NAME="liga_region_name";
+    private static final String LIGA_TABLE_UEBERGEORDNET_ID="liga_uebergeordnet_id";
+    private static final String LIGA_TABLE_UEBERGEORDNET_NAME="liga_uebergeordnet_name";
+    private static final String LIGA_TABLE_VERANTWORTLICH_ID="liga_verantwortlich_id";
+    private static final String LIGA_TABLE_VERANTWORTLICH_MAIL="liga_verantwortlich_mail";
+    private static final String LIGA_TABLE_USER_ID="liga_user_id";
+
+    //wrap all specific config parameters
+    private static final BusinessEntityConfiguration<LigaBE> DSBMITGLIED = new BusinessEntityConfiguration<>(
+            LigaBE.class, TABLE, getColumnsToFieldsMap(), LOGGER);
+
+    /*
+     * SQL queries
+     */
+    private static final String FIND_ALL =
+            "SELECT * "
+                    + " FROM liga"
+                    + " ORDER BY liga_id";
+
+    private static final String FIND_BY_ID =
+            "SELECT * "
+                    + " FROM liga "
+                    + " WHERE liga_id = ?";
+
+    private final BasicDAO basicDao;
+
+    /**
+     * Initialize the transaction manager to provide a database connection
+     *
+     * @param basicDao to handle the commonly used database operations
+     */
+    @Autowired
+    public LigaDAO(final BasicDAO basicDao) {
+        this.basicDao = basicDao;
+    }
+
+
+    // table column label mapping to the business entity parameter names
+    private static Map<String, String> getColumnsToFieldsMap() {
+        final Map<String, String> columnsToFieldsMap = new HashMap<>();
+
+        columnsToFieldsMap.put(LIGA_TABLE_ID, LIGA_BE_ID);
+        columnsToFieldsMap.put(LIGA_TABLE_NAME, LIGA_BE_NAME);
+        columnsToFieldsMap.put(REGION_TABLE_ID, REGION_BE_ID);
+        columnsToFieldsMap.put(REGION_TABLE_NAME, REGION_BE_NAME);
+        columnsToFieldsMap.put(LIGA_TABLE_UEBERGEORDNET_ID, LIGA_BE_UEBERGEORDNET_ID);
+        columnsToFieldsMap.put(LIGA_TABLE_UEBERGEORDNET_NAME, LIGA_BE_UEBERGEORDNET_NAME);
+        columnsToFieldsMap.put(LIGA_TABLE_VERANTWORTLICH_ID, LIGA_BE_VERANTWORTLICH_ID);
+        columnsToFieldsMap.put(LIGA_TABLE_VERANTWORTLICH_MAIL, LIGA_BE_VERANTWORTLICH_MAIL);
+        columnsToFieldsMap.put(LIGA_TABLE_USER_ID,LIGA_BE_USER_ID);
+
+        // add technical columns
+        columnsToFieldsMap.putAll(BasicDAO.getTechnicalColumnsToFieldsMap());
+
+        return columnsToFieldsMap;
+    }
 }
--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/entity/LigaBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/entity/LigaBE.java
@@ -1,4 +1,136 @@
 package de.bogenliga.application.business.liga.impl.entity;
 
-public class LigaBE {
+import de.bogenliga.application.common.component.entity.BusinessEntity;
+import de.bogenliga.application.common.component.entity.CommonBusinessEntity;
+
+/**
+ * I represent the liga business entity.
+ * The {@link CommonBusinessEntity} contains the technical parameter. Business entities commonly use these parameters to
+ * control their lifecycle.
+ *
+ * @see CommonBusinessEntity
+ */
+public class LigaBE extends CommonBusinessEntity implements BusinessEntity {
+
+    private Long ligaId;
+    private String ligaName;
+    private Long ligaRegionId;
+    private String ligaRegionName;
+    private Long ligaUebergeordnetId;
+    private String ligaUebergeordnetName;
+    private Long ligaVerantwortlichId;
+    private String ligaVerantwortlichMail;
+    private Long ligaUserId;
+
+    public LigaBE(){
+        // empty constructor
+    }
+
+
+    public Long getLigaId() {
+        return ligaId;
+    }
+
+
+    public void setLigaId(Long ligaId) {
+        this.ligaId = ligaId;
+    }
+
+
+    public String getLigaName() {
+        return ligaName;
+    }
+
+
+    public void setLigaName(String ligaName) {
+        this.ligaName = ligaName;
+    }
+
+
+    public Long getLigaRegionId() {
+        return ligaRegionId;
+    }
+
+
+    public void setLigaRegionId(Long ligaRegionId) {
+        this.ligaRegionId = ligaRegionId;
+    }
+
+
+    public String getLigaRegionName() {
+        return ligaRegionName;
+    }
+
+
+    public void setLigaRegionName(String ligaRegionName) {
+        this.ligaRegionName = ligaRegionName;
+    }
+
+
+    public Long getLigaUebergeordnetId() {
+        return ligaUebergeordnetId;
+    }
+
+
+    public void setLigaUebergeordnetId(Long ligaUebergeordnetId) {
+        this.ligaUebergeordnetId = ligaUebergeordnetId;
+    }
+
+
+    public String getLigaUebergeordnetName() {
+        return ligaUebergeordnetName;
+    }
+
+
+    public void setLigaUebergeordnetName(String ligaUebergeordnetName) {
+        this.ligaUebergeordnetName = ligaUebergeordnetName;
+    }
+
+
+    public Long getLigaVerantwortlichId() {
+        return ligaVerantwortlichId;
+    }
+
+
+    public void setLigaVerantwortlichId(Long ligaVerantwortlichId) {
+        this.ligaVerantwortlichId = ligaVerantwortlichId;
+    }
+
+
+    public String getLigaVerantwortlichMail() {
+        return ligaVerantwortlichMail;
+    }
+
+
+    public void setLigaVerantwortlichMail(String ligaVerantwortlichMail) {
+        this.ligaVerantwortlichMail = ligaVerantwortlichMail;
+    }
+
+
+    public Long getLigaUserId() {
+        return ligaUserId;
+    }
+
+
+    public void setLigaUserId(Long ligaUserId) {
+        this.ligaUserId = ligaUserId;
+    }
+
+    @Override
+    public String toString(){
+        return "LigaBE{" +
+                "ligaId=" + ligaId +
+                ", ligaName='" + ligaName + '\'' +
+                ", ligaRegionId='" + ligaRegionId + '\'' +
+                ", ligaRegionName='" + ligaRegionName + '\'' +
+                ", ligaUebergeordnetId='" + ligaUebergeordnetId + '\'' +
+                ", ligaUebergeordnetName='" + ligaUebergeordnetName + '\'' +
+                ", ligaVerantwortlichId=" + ligaVerantwortlichId + '\'' +
+                ", ligaVerantwortlichMail=" + ligaVerantwortlichMail + '\'' +
+                ", ligaUserId=" + ligaId+
+                '}';
+    }
+
+
+
 }
