6941e3b7d78f18d3fa9314e7ed37124c14fb817d
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2018-12-11 10:27:06 +0100

    Merge master branch; Add table scripts

---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V31__disziplin__insert_dsb_disziplinen.sql
@@ -0,0 +1,8 @@
+INSERT INTO disziplin (disziplin_id, disziplin_name)
+VALUES (0, 'Recurve'),
+       (1, 'Compound'),
+       (2, 'Blankbogen'),
+       (3, 'Langbogen'),
+       (4, 'Instinktiv')
+;
+
---
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/PROD-DATA-MIGRATION/V33__liga__insert_liga_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet)
+VALUES (1, 0, 'Bundesliga', NULL),
+       (2, 1, 'Württembergliga Recurve', 1),
+       (3, 1, 'Landesliga Nord Recurve', 2),
+       (4, 1, 'Landesliga Süd Recurve', 2),
+       (5, 3, 'Bezirksoberliga', 3),
+       (6, 3, 'Bezirksliga A', 5),
+       (7, 3, 'Bezirksliga B', 6)
