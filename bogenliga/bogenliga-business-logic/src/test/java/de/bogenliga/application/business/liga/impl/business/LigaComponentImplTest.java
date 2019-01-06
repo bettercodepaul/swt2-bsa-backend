2b969d578d2be414feebedca801022945f0ed8e6
Ge2theZ <giuseppe.ferrera@student.reutlingen-university.de>
2019-01-06 01:50:49 +0100

    BSAPP-216: fixed sonar issues

--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/liga/impl/business/LigaComponentImplTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/liga/impl/business/LigaComponentImplTest.java
@@ -24,7 +24,6 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 /**
