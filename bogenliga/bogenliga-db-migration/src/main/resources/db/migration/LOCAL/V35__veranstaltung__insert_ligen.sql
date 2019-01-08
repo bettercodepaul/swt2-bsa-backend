66f8695fde210b3170ee28eadeaeb0f0b55f2397
LTaraman <leila.taraman@student.reutlingen-university.de>
2019-01-08 21:20:01 +0100

    BSAPP-233: Took "Kampfrichter_anzahl" out of the database

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
@@ -5,19 +5,19 @@ INSERT INTO veranstaltung (-- Recurve Ligen
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
                            veranstaltung_meldedeadline,
-                           veranstaltung_kampfrichter_anzahl /**,
+                           /** veranstaltung_kampfrichter_anzahl **/ /**,
                            veranstaltung_hoehere */)
 VALUES
-(0, 1, 'Würtembergliga', 2018, 2, '2017-10-31', 5),
-(1, 1, 'Landesliga Nord', 2018, 2, '2017-10-31', 5),
+(0, 1, 'Würtembergliga', 2018, 2, '2017-10-31'),
+(1, 1, 'Landesliga Nord', 2018, 2, '2017-10-31'),
 (2, 1, 'Landesliga Süd', 2018, 2, '2017-10-31', 5),
-(3, 1, 'Relegation Landesliga Nord', 2018, 2, '2017-10-31', 5),
-(4, 1, 'Relegation Landesliga Süd', 2018, 2, '2017-10-31', 5),
-(5, 1, 'Bezirksoberliga Neckar', 2018, 2, '2017-10-31', 5),
-(6, 1, 'Bezirksliga A Neckar', 2018, 2, '2017-10-31', 5),
-(7, 1, 'Bezirksoberliga Oberschwaben', 2018, 2, '2017-10-31', 5),
-(8, 1, 'Bezirksliga A Oberschwaben', 2018, 2, '2017-10-31', 5),
-(9, 1, 'Bezirksliga B Oberschwaben', 2018, 2, '2017-10-31', 5)
+(3, 1, 'Relegation Landesliga Nord', 2018, 2, '2017-10-31'),
+(4, 1, 'Relegation Landesliga Süd', 2018, 2, '2017-10-31'),
+(5, 1, 'Bezirksoberliga Neckar', 2018, 2, '2017-10-31'),
+(6, 1, 'Bezirksliga A Neckar', 2018, 2, '2017-10-31'),
+(7, 1, 'Bezirksoberliga Oberschwaben', 2018, 2, '2017-10-31'),
+(8, 1, 'Bezirksliga A Oberschwaben', 2018, 2, '2017-10-31'),
+(9, 1, 'Bezirksliga B Oberschwaben', 2018, 2, '2017-10-31')
 ;
 
 INSERT INTO veranstaltung (-- Compound Ligen
@@ -27,12 +27,12 @@ INSERT INTO veranstaltung (-- Compound Ligen
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
                            veranstaltung_meldedeadline,
-                           veranstaltung_kampfrichter_anzahl /**,
+                          /** veranstaltung_kampfrichter_anzahl **/ /**,
                            veranstaltung_hoehere **/)
 VALUES
-(20, 1, 'Bawü Compound Finale', 2018, 2, '2017-10-31', 5),
-(21, 1, 'Württembergliga Compound', 2018, 2, '2017-10-31', 5),
-(22, 1, 'Landesliga A Compound', 2018, 2, '2017-10-31', 5),
-(23, 1, 'Landesliga B Compound', 2018, 2, '2017-10-31', 5)
+(20, 1, 'Bawü Compound Finale', 2018, 2, '2017-10-31'),
+(21, 1, 'Württembergliga Compound', 2018, 2, '2017-10-31'),
+(22, 1, 'Landesliga A Compound', 2018, 2, '2017-10-31'),
+(23, 1, 'Landesliga B Compound', 2018, 2, '2017-10-31')
 ;
 
