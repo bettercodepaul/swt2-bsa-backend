c604a4a715dc65e2dd9197ac5f983bdc18bb2375
LTaraman <leila.taraman@student.reutlingen-university.de>
2019-01-08 20:40:42 +0100

    BSAPP-232: took "Veranstaltung_hoehere" out of the database

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
@@ -5,19 +5,19 @@ INSERT INTO veranstaltung (-- Recurve Ligen
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
                            veranstaltung_meldedeadline,
-                           veranstaltung_kampfrichter_anzahl,
-                           veranstaltung_hoehere)
+                           veranstaltung_kampfrichter_anzahl /**,
+                           veranstaltung_hoehere */)
 VALUES
-(0, 1, 'Würtembergliga', 2018, 2, '2017-10-31', 5, NULL),
-(1, 1, 'Landesliga Nord', 2018, 2, '2017-10-31', 5, 0),
-(2, 1, 'Landesliga Süd', 2018, 2, '2017-10-31', 5, 0),
-(3, 1, 'Relegation Landesliga Nord', 2018, 2, '2017-10-31', 5, 1),
-(4, 1, 'Relegation Landesliga Süd', 2018, 2, '2017-10-31', 5, 2),
-(5, 1, 'Bezirksoberliga Neckar', 2018, 2, '2017-10-31', 5, 4),
-(6, 1, 'Bezirksliga A Neckar', 2018, 2, '2017-10-31', 5, 5),
-(7, 1, 'Bezirksoberliga Oberschwaben', 2018, 2, '2017-10-31', 5, 4),
-(8, 1, 'Bezirksliga A Oberschwaben', 2018, 2, '2017-10-31', 5, 7),
-(9, 1, 'Bezirksliga B Oberschwaben', 2018, 2, '2017-10-31', 5, 8)
+(0, 1, 'Würtembergliga', 2018, 2, '2017-10-31', 5),
+(1, 1, 'Landesliga Nord', 2018, 2, '2017-10-31', 5),
+(2, 1, 'Landesliga Süd', 2018, 2, '2017-10-31', 5),
+(3, 1, 'Relegation Landesliga Nord', 2018, 2, '2017-10-31', 5),
+(4, 1, 'Relegation Landesliga Süd', 2018, 2, '2017-10-31', 5),
+(5, 1, 'Bezirksoberliga Neckar', 2018, 2, '2017-10-31', 5),
+(6, 1, 'Bezirksliga A Neckar', 2018, 2, '2017-10-31', 5),
+(7, 1, 'Bezirksoberliga Oberschwaben', 2018, 2, '2017-10-31', 5),
+(8, 1, 'Bezirksliga A Oberschwaben', 2018, 2, '2017-10-31', 5),
+(9, 1, 'Bezirksliga B Oberschwaben', 2018, 2, '2017-10-31', 5)
 ;
 
 INSERT INTO veranstaltung (-- Compound Ligen
@@ -27,12 +27,12 @@ INSERT INTO veranstaltung (-- Compound Ligen
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
                            veranstaltung_meldedeadline,
-                           veranstaltung_kampfrichter_anzahl,
-                           veranstaltung_hoehere)
+                           veranstaltung_kampfrichter_anzahl /**,
+                           veranstaltung_hoehere **/)
 VALUES
-(20, 1, 'Bawü Compound Finale', 2018, 2, '2017-10-31', 5, NULL),
-(21, 1, 'Württembergliga Compound', 2018, 2, '2017-10-31', 5, 20),
-(22, 1, 'Landesliga A Compound', 2018, 2, '2017-10-31', 5, 21),
-(23, 1, 'Landesliga B Compound', 2018, 2, '2017-10-31', 5, 22)
+(20, 1, 'Bawü Compound Finale', 2018, 2, '2017-10-31', 5),
+(21, 1, 'Württembergliga Compound', 2018, 2, '2017-10-31', 5),
+(22, 1, 'Landesliga A Compound', 2018, 2, '2017-10-31', 5),
+(23, 1, 'Landesliga B Compound', 2018, 2, '2017-10-31', 5)
 ;
 
