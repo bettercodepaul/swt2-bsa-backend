8be4cb00fc00fcd5c675dfe87b80e65be28a20dd
LTaraman <leila.taraman@student.reutlingen-university.de>
2019-01-09 21:45:14 +0100

    BSAPP-231: implementation of entity class VeranstaltungBE

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/veranstaltung/impl/entity/VeranstaltungBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/veranstaltung/impl/entity/VeranstaltungBE.java
@@ -5,4 +5,80 @@ import de.bogenliga.application.common.component.entity.CommonBusinessEntity;
 
 public class VeranstaltungBE extends CommonBusinessEntity implements BusinessEntity {
 
+    private Long id;
+    private Long wettkampftyp;
+    private String name;
+    private Long jahr;
+    private Long deadline;
+    private Long ligaleiterId;
+
+    public VeranstaltungBE(){
+        //empty constructor
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public  Long getWettkampftyp() {
+        return  wettkampftyp;
+    }
+
+    public void setWettkampftyp(Long wettkampftyp) {
+        this.wettkampftyp = wettkampftyp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Long getJahr() {
+        return jahr;
+    }
+
+
+    public void setJahr(Long jahr) {
+        this.jahr = jahr;
+    }
+
+
+    public Long getDeadline() {
+        return deadline;
+    }
+
+
+    public void setDeadline(Long deadline) {
+        this.deadline = deadline;
+    }
+
+
+    public Long getLigaleiterId() {
+        return ligaleiterId;
+    }
+
+
+    public void setLigaleiterId(Long ligaleiterId) {
+        this.ligaleiterId = ligaleiterId;
+    }
+
+    @Override
+    public String toString(){
+
+        return "VeranstaltungBE {" +
+                "id = " + id + '\'' +
+                ", wettkampftyp = '" + wettkampftyp + '\'' +
+                ", name = '" + name + '\'' +
+                ", jahr = '" + jahr + '\'' +
+                ", deadline = '" + deadline +
+                ", ligaleiterId = '" + ligaleiterId +
+                '}';
+    }
 }
