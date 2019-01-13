a9e1fd010267cee3698c74942c63b1f412a5a1ed
Lehnert Andre; EXXCELLENT SOLUTIONS <Andre.Lehnert@partner.bmw.de>
2019-01-13 18:09:46 +0100

    DOWNLOAD: Add download example service, client and component

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/hello/HelloDownloadServiceV1.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/hello/HelloDownloadServiceV1.java
@@ -20,24 +20,30 @@ import de.bogenliga.application.springconfiguration.security.permissions.Require
 import de.bogenliga.application.springconfiguration.security.types.UserPermission;
 
 /**
- * TODO [AL] class documentation
+ * Dummy file download service
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ * @deprecated Remove REST service version dummy
  */
+@Deprecated
 @RestController
 @CrossOrigin
+@RequestMapping("/v1/hello-world/download")
 public class HelloDownloadServiceV1 implements ServiceFacade {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloDownloadServiceV1.class);
 
 
+    /**
+     * Provide PDF
+     */
     @RequestMapping(method = RequestMethod.GET,
-            path = "/v1/hello-world/download/pdf",
+            path = "pdf",
             produces = MediaType.APPLICATION_PDF_VALUE)
-    @RequiresPermission(UserPermission.CAN_READ_SYSTEMDATEN)
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
     public ResponseEntity<InputStreamResource> downloadPdf() {
         LOG.debug("Download static PDF file...");
-        final Resource resource = new ClassPathResource("SWT2_PDF_Beispiel.pdf");
+        final Resource resource = new ClassPathResource("SWT2_Beispiel.pdf");
         long r = 0;
         InputStream is = null;
 
@@ -51,6 +57,114 @@ public class HelloDownloadServiceV1 implements ServiceFacade {
         } catch (final IOException e) {
             e.printStackTrace();
             throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "PDF download failed", e);
+        }
+    }
+
+
+    /**
+     * Provide DOC
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            path = "doc",
+            produces = "application/msword")
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
+    public ResponseEntity<InputStreamResource> downloadWord() {
+        LOG.debug("Download static WORD file...");
+        final Resource resource = new ClassPathResource("SWT2_Beispiel.doc");
+        long r = 0;
+        InputStream is = null;
+
+        try {
+            is = resource.getInputStream();
+            r = resource.contentLength();
+
+            return ResponseEntity.ok()
+                    .contentLength(r)
+                    .body(new InputStreamResource(is));
+        } catch (final IOException e) {
+            e.printStackTrace();
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "WORD download failed", e);
+        }
+    }
+
+
+    /**
+     * Provide XLS
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            path = "xls",
+            produces = "application/vnd.ms-excel")
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
+    public ResponseEntity<InputStreamResource> downloadExcel() {
+        LOG.debug("Download static EXCEL file...");
+        final Resource resource = new ClassPathResource("SWT2_Beispiel.xls");
+        long r = 0;
+        InputStream is = null;
+
+        try {
+            is = resource.getInputStream();
+            r = resource.contentLength();
+
+            return ResponseEntity.ok()
+                    .contentLength(r)
+                    .body(new InputStreamResource(is));
+        } catch (final IOException e) {
+            e.printStackTrace();
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "EXCEL download failed", e);
+        }
+    }
+
+
+    /**
+     * Provide CSV
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            path = "csv",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
+    public ResponseEntity<InputStreamResource> downloadCSV() {
+        LOG.debug("Download static CSV file...");
+        final Resource resource = new ClassPathResource("SWT2_Beispiel.csv");
+        long r = 0;
+        InputStream is = null;
+
+        try {
+            is = resource.getInputStream();
+            r = resource.contentLength();
+
+            return ResponseEntity.ok()
+                    .contentLength(r)
+                    .body(new InputStreamResource(is));
+        } catch (final IOException e) {
+            e.printStackTrace();
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "CSV download failed", e);
+        }
+    }
+
+
+    /**
+     * Provide MDB (MS Access)
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            path = "mdb",
+            produces = "application/vnd.ms-access")
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
+    public ResponseEntity<InputStreamResource> downloadMDB() {
+        LOG.debug("Download static MDB file...");
+        final Resource resource = new ClassPathResource("SWT2_Beispiel.mdb");
+        long r = 0;
+        InputStream is = null;
+
+        try {
+            is = resource.getInputStream();
+            r = resource.contentLength();
+
+            return ResponseEntity.ok()
+                    .contentLength(r)
+                    .body(new InputStreamResource(is));
+        } catch (final IOException e) {
+            e.printStackTrace();
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "MDB download failed", e);
         }
     }
 }
