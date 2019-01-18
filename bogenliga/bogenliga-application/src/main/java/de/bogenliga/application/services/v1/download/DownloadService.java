73e271de9d4d7e9752a99f8e1daeda7aa3624c18
Dennis Eitle <denniseitle@aol.com>
2019-01-18 14:18:10 +0100

    BSAPP-105: Add generic Download-Service with Setzliste-Download Support

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/download/DownloadService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/download/DownloadService.java
@@ -1,6 +1,8 @@
-package de.bogenliga.application.services.v1.setzliste.service;
+package de.bogenliga.application.services.v1.download;
 
 import de.bogenliga.application.business.Setzliste.api.SetzlisteComponent;
+import de.bogenliga.application.common.errorhandling.ErrorCode;
+import de.bogenliga.application.common.errorhandling.exception.TechnicalException;
 import de.bogenliga.application.common.service.ServiceFacade;
 import de.bogenliga.application.springconfiguration.security.permissions.RequiresPermission;
 import de.bogenliga.application.springconfiguration.security.types.UserPermission;
@@ -19,7 +21,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * I´m a REST resource and handle setzliste pdf requests over the HTTP protocol.
+ * I´m a REST resource and handle download requests over the HTTP protocol.
  *
  * @author Dennis Eitle
  * @see <a href="https://en.wikipedia.org/wiki/Create,_read,_update_and_delete">Wikipedia - CRUD</a>
@@ -34,10 +36,10 @@ import java.io.InputStream;
  */
 @RestController
 @CrossOrigin(maxAge = 0)
-@RequestMapping("v1/setzliste")
-public class SetzlisteService implements ServiceFacade {
+@RequestMapping("v1/download/")
+public class DownloadService implements ServiceFacade {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SetzlisteService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DownloadService.class);
 
     /*
      * Business components
@@ -52,8 +54,34 @@ public class SetzlisteService implements ServiceFacade {
      *
      */
     @Autowired
-    public SetzlisteService(final SetzlisteComponent setzlisteComponent) {
+    public DownloadService(final SetzlisteComponent setzlisteComponent) {
         this.setzlisteComponent = setzlisteComponent;
+    }
+
+    /**
+     * Provide Setzliste PDF
+     */
+    @RequestMapping(method = RequestMethod.GET,
+            path = "pdf/setzliste",
+            produces = MediaType.APPLICATION_PDF_VALUE)
+    @RequiresPermission(UserPermission.CAN_READ_STAMMDATEN)
+    public ResponseEntity<InputStreamResource> downloadSetzlistePdf() {
+        LOG.debug("Download static PDF file...");
+        //TODO: Aktuell werden Parameter noch nicht übergeben, da die Wettkampftabelle im Frontend noch statisch ist. Sobald das erledigt ist, können Parameter aus dem Frontend wie bei "getTableByVarsV2()" übergeben werden
+        final String fileName = setzlisteComponent.getTable(30, 1);
+        final Resource resource = new ClassPathResource(fileName);
+        long r = 0;
+        InputStream is = null;
+        try {
+            is = resource.getInputStream();
+            r = resource.contentLength();
+            return ResponseEntity.ok()
+                    .contentLength(r)
+                    .body(new InputStreamResource(is));
+        } catch (final IOException e) {
+            e.printStackTrace();
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR, "PDF download failed", e);
+        }
     }
 
 
