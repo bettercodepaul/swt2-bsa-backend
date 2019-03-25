b9fc47c585f925875bb7d4859766dcc6e71a334f
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-03-25 15:08:23 +0100

    Bugfix in Insert Script "Veranstaltungen" - DB-Mig läuft wieder

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V35__veranstaltung__insert_ligen.sql
@@ -4,13 +4,11 @@ INSERT INTO veranstaltung (-- Recurve Ligen
                            veranstaltung_name,
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
-                           veranstaltung_meldedeadline,
-                           /** veranstaltung_kampfrichter_anzahl **/ /**,
-                           veranstaltung_hoehere */)
+                           veranstaltung_meldedeadline)
 VALUES
 (0, 1, 'Würtembergliga', 2018, 2, '2017-10-31'),
 (1, 1, 'Landesliga Nord', 2018, 2, '2017-10-31'),
-(2, 1, 'Landesliga Süd', 2018, 2, '2017-10-31', 5),
+(2, 1, 'Landesliga Süd', 2018, 2, '2017-10-31'),
 (3, 1, 'Relegation Landesliga Nord', 2018, 2, '2017-10-31'),
 (4, 1, 'Relegation Landesliga Süd', 2018, 2, '2017-10-31'),
 (5, 1, 'Bezirksoberliga Neckar', 2018, 2, '2017-10-31'),
@@ -26,9 +24,7 @@ INSERT INTO veranstaltung (-- Compound Ligen
                            veranstaltung_name,
                            veranstaltung_sportjahr,
                            veranstaltung_ligaleiter_id,
-                           veranstaltung_meldedeadline,
-                          /** veranstaltung_kampfrichter_anzahl **/ /**,
-                           veranstaltung_hoehere **/)
+                           veranstaltung_meldedeadline)
 VALUES
 (20, 1, 'Bawü Compound Finale', 2018, 2, '2017-10-31'),
 (21, 1, 'Württembergliga Compound', 2018, 2, '2017-10-31'),
