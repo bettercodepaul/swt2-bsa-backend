feaa6b1615e67834df6fca688e3a3b3118b26b71
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-03-28 17:08:22 +0100

    drei neue Rolle mit Rechten: Sportleiter, Kampfrichter, Ligaleiter

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V44__rolle_recht__insert_test_data.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V44__rolle_recht__insert_test_data.sql
@@ -10,13 +10,35 @@ VALUES (1, 1), -- admin = all permissions (technical and business)
        (1, 6),
        (1, 7),
        (1, 8),
+       (1, 9),
+       (1, 10),
+       (1, 11),
        (2, 1), -- moderator = all business read/ write permissions
        (2, 2),
        (2, 3),
        (2, 4),
        (2, 5),
        (2, 6),
+       (2, 9),
+       (2, 10),
        (3, 1), -- user = all business read permissions
        (3, 3),
-       (3, 5)
-;
+       (3, 5),
+       (3, 9),
+       (4, 1), -- SPORTLEITER = all business read permissions & modify his club  (verein)
+       (4, 3),
+       (4, 5),
+       (4, 9),
+       (4, 10),
+       (5, 1), -- KAMPFRICHTER = all business read permissions & modify his event  (wettkampf)
+       (5, 3),
+       (5, 4),
+       (5, 5),
+       (5, 9),
+       (6, 1), -- LIGALEITER = all business read permissions & modify events and all clubs  (verein, wettkampf)
+       (6, 3),
+       (6, 4),
+       (6, 5),
+       (6, 9),
+       (6, 11)
+ ;
