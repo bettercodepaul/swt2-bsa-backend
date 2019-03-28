8d954d932d9f6a7fdf6914d551dca007ece47b0b
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-03-28 17:08:22 +0100

    drei neue Rolle mit Rechten: Sportleiter, Kampfrichter, Ligaleiter

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V43__rolle__insert_test_data.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V43__rolle__insert_test_data.sql
@@ -4,5 +4,8 @@ rolle_name
 )
 VALUES (1, 'ADMIN'),
        (2, 'MODERATOR'),
-       (3, 'USER')
+       (3, 'USER'),
+       (4, 'SPORTLEITER'),
+       (5, 'KAMPFRICHTER'),
+       (6, 'LIGALEITER')
 ;
