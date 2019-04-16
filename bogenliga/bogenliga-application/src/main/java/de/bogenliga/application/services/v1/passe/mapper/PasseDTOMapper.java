348303fbdf7a6867fa3a825daf770f382ea4c50f
Yannik Prigl <yannik.prigl@student.reutlingen-university>
2019-04-16 21:02:06 +0200

    added DTO->DO and Do->DTO mapping

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/passe/mapper/PasseDTOMapper.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/passe/mapper/PasseDTOMapper.java
@@ -16,12 +16,42 @@ public class PasseDTOMapper implements DataTransferObjectMapper {
     public PasseDTOMapper() {
     }
 
+
+    /**
+     * Map {@link PasseDO} to {@link PasseDTO}
+     */
     public static final Function<PasseDO, PasseDTO> toDTO = passeDO -> {
+        final long id = passeDO.getId();
         final long mannschaftsId = passeDO.getPasseMannschaftId();
         final long wettkampfId = passeDO.getPasseWettkampfId();
         final long matchnr = passeDO.getPasseMatchNr();
         final long lfdnr = passeDO.getPasseLfdnr();
         final long dsbMitgliedId = passeDO.getPasseDsbMitgliedId();
-        final int[] ringzahl = 
-    }
+        final int[] ringzahl = {passeDO.getPfeil1(), passeDO.getPfeil2(), passeDO.getPfeil3(),
+                passeDO.getPfeil4(), passeDO.getPfeil5(), passeDO.getPfeil6()};
+        return new PasseDTO(id, mannschaftsId, wettkampfId, matchnr,
+                lfdnr, dsbMitgliedId, ringzahl);
+    };
+
+    /**
+     * Map {@Link PasseDTO} to {@Link PasseDO}
+     */
+    public static final Function<PasseDTO, PasseDO> toDO = passeDTO -> {
+        final long id = passeDTO.getId();
+        final long mannschaftsId = passeDTO.getMannschaft_id();
+        final long wettkampfId = passeDTO.getWettkampf_id();
+        final long matchnr = passeDTO.getMatch_nr();
+        final long lfdnr = passeDTO.getLfdnr();
+        final long dsbMitgliedId = passeDTO.getDsb_mitglied_nr();
+        final int pfeil1 = passeDTO.getRingzahl()[0];
+        final int pfeil2 = passeDTO.getRingzahl()[1];
+        final int pfeil3 = passeDTO.getRingzahl()[2];
+        final int pfeil4 = passeDTO.getRingzahl()[3];
+        final int pfeil5 = passeDTO.getRingzahl()[4];
+        final int pfeil6 = passeDTO.getRingzahl()[5];
+
+        return new PasseDO(id,mannschaftsId,wettkampfId,matchnr,
+                lfdnr,dsbMitgliedId,pfeil1,pfeil2,pfeil3,pfeil4,pfeil5,pfeil6);
+    };
 }
+
--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/passe/model/PasseDTO.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/passe/model/PasseDTO.java
@@ -11,16 +11,20 @@ public class PasseDTO implements DataTransferObject {
 
 
     private static final long serialVersionUID = -3582493648923927523L;
-    private int mannschaft_id;
-    private int wettkampf_id;
-    private int match_nr;
-    private int lfdnr;
-    private int dsb_mitglied_nr;
+
+    private long id;
+    private long mannschaft_id;
+    private long wettkampf_id;
+    private long match_nr;
+    private long lfdnr;
+    private long dsb_mitglied_nr;
     private int[] ringzahl;
 
     public PasseDTO(){}
 
-    public PasseDTO(int mannschaft_id, int wettkampf_id, int match_nr, int lfdnr, int dsb_mitglied_nr, int[] ringzahl){
+    public PasseDTO(long id,long mannschaft_id, long wettkampf_id, long match_nr,
+                    long lfdnr, long dsb_mitglied_nr, int[] ringzahl){
+        this.id = id;
         this.mannschaft_id = mannschaft_id;
         this.wettkampf_id = wettkampf_id;
         this.match_nr = match_nr;
@@ -29,53 +33,62 @@ public class PasseDTO implements DataTransferObject {
         this.ringzahl = ringzahl;
     }
 
+    public long getId() {
+        return id;
+    }
 
-    public int getMannschaft_id() {
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public long getMannschaft_id() {
         return mannschaft_id;
     }
 
 
-    public void setMannschaft_id(int mannschaft_id) {
+    public void setMannschaft_id(long mannschaft_id) {
         this.mannschaft_id = mannschaft_id;
     }
 
 
-    public int getWettkampf_id() {
+    public long getWettkampf_id() {
         return wettkampf_id;
     }
 
 
-    public void setWettkampf_id(int wettkampf_id) {
+    public void setWettkampf_id(long wettkampf_id) {
         this.wettkampf_id = wettkampf_id;
     }
 
 
-    public int getMatch_nr() {
+    public long getMatch_nr() {
         return match_nr;
     }
 
 
-    public void setMatch_nr(int match_nr) {
+    public void setMatch_nr(long match_nr) {
         this.match_nr = match_nr;
     }
 
 
-    public int getLfdnr() {
+    public long getLfdnr() {
         return lfdnr;
     }
 
 
-    public void setLfdnr(int lfdnr) {
+    public void setLfdnr(long lfdnr) {
         this.lfdnr = lfdnr;
     }
 
 
-    public int getDsb_mitglied_nr() {
+    public long getDsb_mitglied_nr() {
         return dsb_mitglied_nr;
     }
 
 
-    public void setDsb_mitglied_nr(int dsb_mitglied_nr) {
+    public void setDsb_mitglied_nr(long dsb_mitglied_nr) {
         this.dsb_mitglied_nr = dsb_mitglied_nr;
     }
 
