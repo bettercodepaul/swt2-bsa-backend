d1e41a02c211ab09ae4f8968fd289cf8fa6cca1a
KayStudentReutlingen <kay_arian_ranko.scheerer@student.reutlingen-university.de>
2019-04-16 20:08:03 +0200

    added PasseDO and DAO, DAO not complete

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Passe/api/types/PasseDO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.Passe.api.types;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class PasseDO {
+}
