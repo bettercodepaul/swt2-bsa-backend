725730e2d36bc024ca4828eee01d47e43a06dfb5
KaiStahl <Kai.Stahl@student.reutlingen-university.de>
2019-04-19 12:41:25 +0200

    Kalsse: ErrorTesting, Testkommentar -> löschen

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/errortesting/ErrorTestingService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/errortesting/ErrorTestingService.java
@@ -12,8 +12,6 @@ import de.bogenliga.application.common.errorhandling.exception.BusinessException
 import de.bogenliga.application.common.errorhandling.exception.TechnicalException;
 import de.bogenliga.application.common.service.ServiceFacade;
 
-//Scrumicorns_Kai_Test
-
 /**
  * I´m a REST resource and produce errors for the error handling tests
  *
