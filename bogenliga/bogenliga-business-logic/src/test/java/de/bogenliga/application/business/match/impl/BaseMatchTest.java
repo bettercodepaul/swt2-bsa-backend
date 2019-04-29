e6e41a6c02c8b3ed163b8c951712b83985553d61
Dominik Halle <dominik_jonathan.halle@student.reutlingen-university.de>
2019-04-29 18:04:17 +0200

    add tests for MatchComponent and MatchDAO

---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/match/impl/BaseMatchTest.java
@@ -0,0 +1,42 @@
+package de.bogenliga.application.business.match.impl;
+
+import java.util.List;
+import org.junit.Rule;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import de.bogenliga.application.business.match.impl.entity.MatchBE;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Dominik Halle, HSRT MKI SS19 - SWT2
+ */
+public abstract class BaseMatchTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    protected static final Long MATCH_ID = 1L;
+    protected static final Long MATCH_NR = 1L;
+    protected static final Long MATCH_BEGEGNUNG = 1L;
+    protected static final Long MATCH_WETTKAMPF_ID = 1L;
+    protected static final Long MATCH_MANNSCHAFT_ID = 1L;
+    protected static final Long MATCH_SCHEIBENNUMMER = 3L;
+    protected static final Long MATCH_MATCHPUNKTE = 6L;
+    protected static final Long MATCH_SATZPUNKTE = 3L;
+    protected static final Long CURRENT_USER_ID = 1L;
+
+
+    protected MatchBE getMatchBE() {
+        MatchBE matchBE = new MatchBE();
+        matchBE.setId(MATCH_ID);
+        matchBE.setNr(MATCH_NR);
+        matchBE.setBegegnung(MATCH_BEGEGNUNG);
+        matchBE.setMannschaftId(MATCH_MANNSCHAFT_ID);
+        matchBE.setWettkampfId(MATCH_WETTKAMPF_ID);
+        matchBE.setMatchpunkte(MATCH_MATCHPUNKTE);
+        matchBE.setScheibenNummer(MATCH_SCHEIBENNUMMER);
+        matchBE.setSatzpunkte(MATCH_SATZPUNKTE);
+        return matchBE;
+    }
+}
---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/match/impl/business/MatchComponentImplTest.java
@@ -0,0 +1,182 @@
+package de.bogenliga.application.business.match.impl.business;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import de.bogenliga.application.business.match.api.types.MatchDO;
+import de.bogenliga.application.business.match.impl.BaseMatchTest;
+import de.bogenliga.application.business.match.impl.dao.MatchDAO;
+import de.bogenliga.application.business.match.impl.entity.MatchBE;
+import de.bogenliga.application.business.match.impl.mapper.MatchMapper;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Dominik Halle, HSRT MKI SS19 - SWT2
+ */
+public class MatchComponentImplTest extends BaseMatchTest {
+
+    @Mock
+    private MatchDAO matchDAO;
+
+    @InjectMocks
+    private MatchComponentImpl underTest;
+
+
+    private void validateObjectList (List<MatchDO> actual) {
+        assertThat(actual)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(1);
+
+        assertThat(actual.get(0)).isNotNull();
+    }
+
+
+    private void assertValid(MatchBE expectedMatchBE, MatchDO actual) {
+        assertThat(actual).isNotNull();
+        assertThat(actual.getId()).isEqualTo(expectedMatchBE.getId()).isEqualTo(MATCH_ID);
+        assertThat(actual.getBegegnung()).isEqualTo(expectedMatchBE.getBegegnung()).isEqualTo(MATCH_BEGEGNUNG);
+        assertThat(actual.getMannschaftId()).isEqualTo(expectedMatchBE.getMannschaftId()).isEqualTo(
+                MATCH_MANNSCHAFT_ID);
+        assertThat(actual.getWettkampfId()).isEqualTo(expectedMatchBE.getWettkampfId()).isEqualTo(MATCH_WETTKAMPF_ID);
+        assertThat(actual.getMatchpunkte()).isEqualTo(expectedMatchBE.getMatchpunkte()).isEqualTo(MATCH_MATCHPUNKTE);
+        assertThat(actual.getSatzpunkte()).isEqualTo(expectedMatchBE.getSatzpunkte()).isEqualTo(MATCH_SATZPUNKTE);
+        assertThat(actual.getNr()).isEqualTo(expectedMatchBE.getNr()).isEqualTo(MATCH_NR);
+    }
+
+
+    @Test
+    public void findAll() {
+        MatchBE expectedMatchBE = getMatchBE();
+
+        final List<MatchBE> expectedBEList = Collections.singletonList(expectedMatchBE);
+
+        // configure mocks
+        when(matchDAO.findAll()).thenReturn(expectedBEList);
+
+        // call test method
+        final List<MatchDO> actual = underTest.findAll();
+
+        // assert result
+        validateObjectList(actual);
+    }
+
+
+    @Test
+    public void findById() {
+        MatchBE expectedMatchBE = getMatchBE();
+
+        // configure mocks to return the expectedMatchBE when findById is called
+        when(matchDAO.findById(anyLong())).thenReturn(expectedMatchBE);
+
+        final MatchDO actual = underTest.findById(MATCH_ID);
+
+        // assert result
+        assertValid(expectedMatchBE, actual);
+
+        verify(matchDAO).findById(MATCH_ID);
+    }
+
+
+    @Test
+    public void findByPk() {
+        MatchBE expectedMatchBE = getMatchBE();
+
+        // configure mocks to return the expectedMatchBE when findById is called
+        when(matchDAO.findByPk(
+                anyLong(), anyLong(), anyLong(), anyLong(), anyLong()
+        )).thenReturn(expectedMatchBE);
+
+        final MatchDO actual = underTest.findByPk(
+                MATCH_NR, MATCH_WETTKAMPF_ID,
+                MATCH_MANNSCHAFT_ID, MATCH_BEGEGNUNG,
+                MATCH_SCHEIBENNUMMER
+        );
+
+        // assert result
+        assertValid(expectedMatchBE, actual);
+
+        verify(matchDAO).findByPk(
+                MATCH_NR, MATCH_WETTKAMPF_ID,
+                MATCH_MANNSCHAFT_ID, MATCH_BEGEGNUNG,
+                MATCH_SCHEIBENNUMMER
+        );
+    }
+
+
+    @Test
+    public void findByWettkampfId() {
+        MatchBE expectedMatchBE = getMatchBE();
+
+        final List<MatchBE> expectedBEList = Collections.singletonList(expectedMatchBE);
+
+        // configure mocks
+        when(matchDAO.findByWettkampfId(anyLong())).thenReturn(expectedBEList);
+
+        // call test method
+        final List<MatchDO> actual = underTest.findByWettkampfId(MATCH_WETTKAMPF_ID);
+
+        // assert result
+        validateObjectList(actual);
+        verify(matchDAO).findByWettkampfId(MATCH_WETTKAMPF_ID);
+    }
+
+
+    @Test
+    public void findByMannschaftId() {
+        MatchBE expectedMatchBE = getMatchBE();
+
+        final List<MatchBE> expectedBEList = Collections.singletonList(expectedMatchBE);
+
+        // configure mocks
+        when(matchDAO.findByMannschaftId(anyLong())).thenReturn(expectedBEList);
+
+        // call test method
+        final List<MatchDO> actual = underTest.findByMannschaftId(MATCH_MANNSCHAFT_ID);
+
+        // assert result
+        validateObjectList(actual);
+        verify(matchDAO).findByMannschaftId(MATCH_MANNSCHAFT_ID);
+    }
+
+
+    @Test
+    public void create() {
+        MatchBE expectedMatchBE = getMatchBE();
+        when(matchDAO.create(any(MatchBE.class), anyLong())).thenReturn(expectedMatchBE);
+        MatchDO matchDO = MatchMapper.toMatchDO.apply(expectedMatchBE);
+        MatchDO actual = underTest.create(matchDO, CURRENT_USER_ID);
+
+        // assert result
+        assertValid(expectedMatchBE, actual);
+
+        // cannot verify invocation as BE is different in component impl method (from mapper)
+    }
+
+
+    @Test
+    public void update() {
+        MatchBE expectedMatchBE = getMatchBE();
+        // configure mocks to return the expectedMatchBE when findById is called
+        when(matchDAO.findById(anyLong())).thenReturn(expectedMatchBE);
+        when(matchDAO.update(any(MatchBE.class), anyLong())).thenReturn(expectedMatchBE);
+        MatchDO matchDO = MatchMapper.toMatchDO.apply(expectedMatchBE);
+        MatchDO actual = underTest.update(matchDO, CURRENT_USER_ID);
+
+        // assert result
+        assertValid(expectedMatchBE, actual);
+
+        // cannot verify invocation as BE is different in component impl method (from mapper)
+    }
+
+
+    @Test
+    public void delete() {
+        MatchBE expectedMatchBE = getMatchBE();
+        MatchDO matchDO = MatchMapper.toMatchDO.apply(expectedMatchBE);
+        underTest.delete(matchDO, CURRENT_USER_ID);
+    }
+}
