faa6077b6749d8f18e8f5169979193fef89a3761
HesseM <Michael_Maximilian.Hesse@Student.Reutlingen-University.DE>
2019-05-03 12:46:36 +0200

    creating Tests

---
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/download/DownloadServiceTest.java
@@ -0,0 +1,16 @@
+package de.bogenliga.application.services.v1.download;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class DownloadServiceTest {
+
+    @Test
+    public void downloadSetzlistePdf() {
+    }
+}
