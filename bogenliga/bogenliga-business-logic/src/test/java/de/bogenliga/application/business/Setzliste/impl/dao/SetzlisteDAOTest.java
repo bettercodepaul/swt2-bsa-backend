3e96ccc72ea59cd4c8eb67e917350f64e27887ef
HesseM <Michael_Maximilian.Hesse@Student.Reutlingen-University.DE>
2019-05-03 12:46:36 +0200

    creating Tests

---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/Setzliste/impl/dao/SetzlisteDAOTest.java
@@ -0,0 +1,16 @@
+package de.bogenliga.application.business.Setzliste.impl.dao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class SetzlisteDAOTest {
+
+    @Test
+    public void getTable() {
+    }
+}
