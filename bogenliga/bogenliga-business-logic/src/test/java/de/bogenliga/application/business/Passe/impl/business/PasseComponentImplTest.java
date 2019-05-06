01a0375ccec7957cc4968f88fba58f84b5a7dd50
KayStudentReutlingen <kay_arian_ranko.scheerer@student.reutlingen-university.de>
2019-05-06 16:15:37 +0200

    Implemented automatic test for components, added MatchID to Passe TABLE

--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/Passe/impl/business/PasseComponentImplTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/Passe/impl/business/PasseComponentImplTest.java
@@ -1,21 +1,20 @@
 package de.bogenliga.application.business.Passe.impl.business;
 
-import java.time.OffsetDateTime;
+import java.lang.reflect.InvocationTargetException;
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import de.bogenliga.application.business.Passe.api.types.PasseDO;
 import de.bogenliga.application.business.Passe.impl.dao.PasseDAO;
 import de.bogenliga.application.business.Passe.impl.entity.PasseBE;
-import de.bogenliga.application.business.dsbmannschaft.api.types.DsbMannschaftDO;
+import de.bogenliga.application.business.baseClass.impl.BasicTest;
 import de.bogenliga.application.business.dsbmannschaft.impl.dao.DsbMannschaftDAO;
 import de.bogenliga.application.business.dsbmitglied.impl.dao.DsbMitgliedDAO;
-import de.bogenliga.application.business.dsbmitglied.impl.entity.DsbMitgliedBE;
-import static org.junit.Assert.*;
 
 /**
  * TODO [AL] class documentation
@@ -30,7 +29,6 @@ public class PasseComponentImplTest {
     @Mock
     private PasseDAO passeDAO;
 
-
     @Mock
     private DsbMannschaftDAO dsbMannschaftDAO;
 
@@ -40,52 +38,28 @@ public class PasseComponentImplTest {
     @InjectMocks
     private PasseComponentImpl underTest;
 
-    @Captor
-    private ArgumentCaptor<PasseBE> passeBEArgumentCaptor;
 
-    private static final long PASSE_ID = 8;
-    private static final long PASSE_MANNSCHAFT_ID = 1;
-    private static final long PASSE_WETTKAMPF_ID = 1337;
-    private static final long PASSE_LFDR_NR = 2;
-    private static final long PASSE_DSB_MITGLIED_ID = 98;
-    private static final long PASSE_MATCH_NR = 4;
-    private static final int PASSE_PFEIL_1 = 10;
-    private static final int PASSE_PFEIL_2 = 5;
-    private static final OffsetDateTime offsetDateTime = null;
-    private static final long USER = 1;
-    private static final long VERSION = 2;
+    private BasicTest<PasseComponentImpl> basicTest;
 
 
-    /***
-     * Utility methods for creating business entities/data objects.
-     * Also used by other test classes.
-     */
+    @Before
+    public void testSetup() {
+        basicTest = new BasicTest<>();
+        basicTest.setEntity(underTest);
 
-    public static PasseBE getPasseBE() {
-        final PasseBE expectedPasseBE = new PasseBE();
-        expectedPasseBE.setId(PASSE_ID);
-        expectedPasseBE.setPasseDsbMitgliedId(PASSE_DSB_MITGLIED_ID);
-        expectedPasseBE.setPasseWettkampfId(PASSE_WETTKAMPF_ID);
-        expectedPasseBE.setPasseMannschaftId(PASSE_MANNSCHAFT_ID);
-        expectedPasseBE.setPasseMatchNr(PASSE_MATCH_NR);
-        expectedPasseBE.setPasseLfdnr(PASSE_LFDR_NR);
-        expectedPasseBE.setPfeil1(PASSE_PFEIL_1);
-        expectedPasseBE.setPfeil2(PASSE_PFEIL_2);
-
-        expectedPasseBE.setLastModifiedByUserId(USER);
-
-        return expectedPasseBE;
     }
 
 
-    public static PasseDO getPasseDO() {
-        final PasseDO expectedPasseDO = new PasseDO(PASSE_ID,
-                PASSE_MANNSCHAFT_ID, PASSE_WETTKAMPF_ID, PASSE_MATCH_NR, PASSE_LFDR_NR,
-                PASSE_DSB_MITGLIED_ID, PASSE_PFEIL_1, PASSE_PFEIL_2, 0, 0, 0, 0, offsetDateTime,
-                USER, offsetDateTime, USER, VERSION);
+    @Test(expected = Test.None.class /* no exception expected */)
+    public void test() {
+        try {
+            basicTest.assertException();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
-        return expectedPasseDO;
     }
-
 
 }
