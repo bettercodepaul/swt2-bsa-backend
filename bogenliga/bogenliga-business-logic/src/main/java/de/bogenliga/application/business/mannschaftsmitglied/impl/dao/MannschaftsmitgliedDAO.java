e566f8f663abb7d5b7a4dec60a5fabeea7539853
Dominik Halle <dominik_jonathan.halle@student.reutlingen-university.de>
2019-05-08 17:07:11 +0200

    use mannschaftsmitlgied impl and query builder state from query-builder-reafctor branch due to unrealiable raw sql strings and more syntax issues...

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/impl/dao/MannschaftsmitgliedDAO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/mannschaftsmitglied/impl/dao/MannschaftsmitgliedDAO.java
@@ -1,23 +1,20 @@
 package de.bogenliga.application.business.mannschaftsmitglied.impl.dao;
 
-import de.bogenliga.application.business.dsbmitglied.impl.entity.DsbMitgliedBE;
-import de.bogenliga.application.business.mannschaftsmitglied.impl.entity.MannschaftsmitgliedBE;
-import de.bogenliga.application.common.component.dao.BasicDAO;
-import de.bogenliga.application.common.component.dao.BusinessEntityConfiguration;
-import de.bogenliga.application.common.component.dao.DataAccessObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import de.bogenliga.application.business.mannschaftsmitglied.impl.entity.MannschaftsmitgliedBE;
+import de.bogenliga.application.common.component.dao.BasicDAO;
+import de.bogenliga.application.common.component.dao.BusinessEntityConfiguration;
+import de.bogenliga.application.common.component.dao.DataAccessObject;
+import de.bogenliga.application.common.database.queries.QueryBuilder;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-//
 /**
- * TODO [AL] class documentation
- *
- * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ * @author Dominik Halle, HSRT MKI SS19 - SWT2
  */
 @Repository
 public class MannschaftsmitgliedDAO implements DataAccessObject {
@@ -28,6 +25,11 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
 
     // table name in the database
     private static final String TABLE = "mannschaftsmitglied";
+    private static final String TABLE_ALIAS = "m";
+
+    private static final String DSB_MITGLIED_TABLE = "dsb_mitglied";
+    private static final String DSB_MITGLIED_TABLE_ALIAS = "d";
+    private static final String DSB_MITGLIED_TABLE_MIGLIED_ID = "dsb_mitglied_id";
     // business entity parameter names
 
     private static final String MANNSCHAFTSMITGLIED_BE_ID = "id";
@@ -39,7 +41,6 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
     private static final String DSBMITGLIED_BE_FORENAME = "dsbMitgliedVorname";
     private static final String DSBMITGLIED_BE_SURNAME = "dsbMitgliedNachname";
 
-
     private static final String MANNSCHAFTSMITGLIED_TABLE_ID = "mannschaftsmitglied_id";
     private static final String MANNSCHAFTSMITGLIED_TABLE_TEAM_ID = "mannschaftsmitglied_mannschaft_id";
     private static final String MANNSCHAFTSMITGLIED_TABLE_USER_ID = "mannschaftsmitglied_dsb_mitglied_id";
@@ -49,36 +50,53 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
     private static final String DSBMITGLIED_TABLE_FORENAME = "dsb_mitglied_vorname";
     private static final String DSBMITGLIED_TABLE_SURNAME = "dsb_mitglied_nachname";
 
+    private static final String[] selectedFields = {
+            MANNSCHAFTSMITGLIED_TABLE_ID, MANNSCHAFTSMITGLIED_TABLE_TEAM_ID, MANNSCHAFTSMITGLIED_TABLE_USER_ID,
+            MANNSCHAFTSMITGLIED_TABLE_INSERT, DSBMITGLIED_TABLE_FORENAME, DSBMITGLIED_TABLE_SURNAME
+    };
+
+    private static final String FIND_ALL = new QueryBuilder()
+            .selectFields(selectedFields)
+            .from(TABLE, TABLE_ALIAS)
+            .join(DSB_MITGLIED_TABLE, DSB_MITGLIED_TABLE_ALIAS)
+            .on(TABLE_ALIAS, MANNSCHAFTSMITGLIED_TABLE_USER_ID, DSB_MITGLIED_TABLE_ALIAS, DSB_MITGLIED_TABLE_MIGLIED_ID)
+            .orderBy(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID)
+            .compose().toString();
+
+    private static final String FIND_BY_TEAM_ID = new QueryBuilder()
+            .selectFields(selectedFields)
+            .from(TABLE, TABLE_ALIAS)
+            .join(DSB_MITGLIED_TABLE, DSB_MITGLIED_TABLE_ALIAS)
+            .on(TABLE_ALIAS, MANNSCHAFTSMITGLIED_TABLE_USER_ID, DSB_MITGLIED_TABLE_ALIAS, DSB_MITGLIED_TABLE_MIGLIED_ID)
+            .whereEquals(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID)
+            .compose().toString();
+
+    private static final String FIND_BY_MEMBER_AND_TEAM_ID = new QueryBuilder()
+            .selectFields(selectedFields)
+            .from(TABLE, TABLE_ALIAS)
+            .join(DSB_MITGLIED_TABLE, DSB_MITGLIED_TABLE_ALIAS)
+            .on(TABLE_ALIAS, MANNSCHAFTSMITGLIED_TABLE_USER_ID, DSB_MITGLIED_TABLE_ALIAS, DSB_MITGLIED_TABLE_MIGLIED_ID)
+            .whereEquals(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID)
+            .andEquals(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID)
+            .compose().toString();
+
+    private static final String FIND_ALL_SCHUETZE_TEAM = new QueryBuilder()
+            .selectFields(selectedFields)
+            .from(TABLE, TABLE_ALIAS)
+            .join(DSB_MITGLIED_TABLE, DSB_MITGLIED_TABLE_ALIAS)
+            .on(TABLE_ALIAS, MANNSCHAFTSMITGLIED_TABLE_USER_ID, DSB_MITGLIED_TABLE_ALIAS, DSB_MITGLIED_TABLE_MIGLIED_ID)
+            .whereEqualsRaw(MANNSCHAFTSMITGLIED_TABLE_INSERT, "1") // true means 1 and vv.
+            .andEquals(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID)
+            .orderBy(MANNSCHAFTSMITGLIED_TABLE_ID)
+            .compose().toString();
+
     // wrap all specific config parameters
     private static final BusinessEntityConfiguration<MannschaftsmitgliedBE> MANNSCHAFTSMITGLIED = new BusinessEntityConfiguration<>(
             MannschaftsmitgliedBE.class, TABLE, getColumnsToFieldsMap(), LOGGER);
 
 
-    private static final String FIND_ALL =
-            "SELECT mannschaftsmitglied_id, mannschaftsmitglied_mannschaft_id, mannschaftsmitglied_dsb_mitglied_id, mannschaftsmitglied_dsb_mitglied_eingesetzt, dsb_mitglied_vorname, dsb_mitglied_nachname "
-                    + " FROM mannschaftsmitglied m join dsb_mitglied d on m.mannschaftsmitglied_dsb_mitglied_id = d.dsb_mitglied_id"
-                    + " ORDER BY mannschaftsmitglied_mannschaft_id;";
-
-    private static final String FIND_BY_TEAM_ID =
-            "SELECT mannschaftsmitglied_id, mannschaftsmitglied_mannschaft_id, mannschaftsmitglied_dsb_mitglied_id, mannschaftsmitglied_dsb_mitglied_eingesetzt, dsb_mitglied_vorname, dsb_mitglied_nachname "
-                    + " FROM mannschaftsmitglied m join dsb_mitglied d on m.mannschaftsmitglied_dsb_mitglied_id = d.dsb_mitglied_id"
-                    + " WHERE mannschaftsmitglied_mannschaft_id = ?;";
-
-    private static final String FIND_BY_MEMBER_AND_TEAM_ID =
-            "SELECT mannschaftsmitglied_id, mannschaftsmitglied_mannschaft_id, mannschaftsmitglied_dsb_mitglied_id, mannschaftsmitglied_dsb_mitglied_eingesetzt, dsb_mitglied_vorname, dsb_mitglied_nachname "
-                    + "FROM mannschaftsmitglied m join dsb_mitglied d on m.mannschaftsmitglied_dsb_mitglied_id = d.dsb_mitglied_id"
-                    +" WHERE mannschaftsmitglied_mannschaft_id =? AND mannschaftsmitglied_dsb_mitglied_id=?;";
-
-    private static final String FIND_ALL_SCHUETZE_TEAM =
-            "SELECT mannschaftsmitglied_id, mannschaftsmitglied_mannschaft_id, mannschaftsmitglied_dsb_mitglied_id, mannschaftsmitglied_dsb_mitglied_eingesetzt, dsb_mitglied_vorname, dsb_mitglied_nachname "
-                    + "FROM mannschaftsmitglied m join dsb_mitglied d on m.mannschaftsmitglied_dsb_mitglied_id = d.dsb_mitglied_id"
-                    +" WHERE   mannschaftsmitglied_mannschaft_id =? AND mannschaftsmitglied_dsb_mitglied_eingesetzt=1"
-                    + " ORDER BY mannschaftsmitglied_id;";
-
-
-
-
     private final BasicDAO basicDao;
+
 
     @Autowired
     public MannschaftsmitgliedDAO(final BasicDAO basicDao) {
@@ -92,13 +110,13 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
 
         columnsToFieldsMap.put(MANNSCHAFTSMITGLIED_TABLE_ID, MANNSCHAFTSMITGLIED_BE_ID);
         columnsToFieldsMap.put(MANNSCHAFTSMITGLIED_TABLE_TEAM_ID, MANNSCHAFTSMITGLIED_BE_TEAM_ID);
-        columnsToFieldsMap.put(MANNSCHAFTSMITGLIED_TABLE_USER_ID,MANNSCHAFTSMITGLIED_BE_USER_ID);
+        columnsToFieldsMap.put(MANNSCHAFTSMITGLIED_TABLE_USER_ID, MANNSCHAFTSMITGLIED_BE_USER_ID);
         columnsToFieldsMap.put(MANNSCHAFTSMITGLIED_TABLE_INSERT, MANNSCHAFTSMITGLIED_BE_INSERT);
 
         System.out.println("test");
-        //new: important for the join with dsb_mitglied
-        columnsToFieldsMap.put(DSBMITGLIED_TABLE_FORENAME,DSBMITGLIED_BE_FORENAME);
-        columnsToFieldsMap.put(DSBMITGLIED_TABLE_SURNAME,DSBMITGLIED_BE_SURNAME);
+        // new: important for the join with dsb_mitglied
+        columnsToFieldsMap.put(DSBMITGLIED_TABLE_FORENAME, DSBMITGLIED_BE_FORENAME);
+        columnsToFieldsMap.put(DSBMITGLIED_TABLE_SURNAME, DSBMITGLIED_BE_SURNAME);
 
 
         // add technical columns
@@ -107,17 +125,20 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
         return columnsToFieldsMap;
     }
 
+
     /**
      * Return all mannschaftsmitglied entries
      */
-     public List<MannschaftsmitgliedBE> findAll() {
-
+    public List<MannschaftsmitgliedBE> findAll() {
         return basicDao.selectEntityList(MANNSCHAFTSMITGLIED, FIND_ALL);
     }
 
+
     /**
      * return all members in the team
+     *
      * @param id
+     *
      * @return
      */
     public List<MannschaftsmitgliedBE> findByTeamId(final long id) {
@@ -125,42 +146,40 @@ public class MannschaftsmitgliedDAO implements DataAccessObject {
     }
 
 
-    public List<MannschaftsmitgliedBE> findAllSchuetzeInTeam(final long id){
-        return basicDao.selectEntityList(MANNSCHAFTSMITGLIED, FIND_ALL_SCHUETZE_TEAM,id);
+    public List<MannschaftsmitgliedBE> findAllSchuetzeInTeam(final long id) {
+        return basicDao.selectEntityList(MANNSCHAFTSMITGLIED, FIND_ALL_SCHUETZE_TEAM, id);
     }
 
 
-    public MannschaftsmitgliedBE findByMemberAndTeamId(long teamId, final long memberId){
+    public MannschaftsmitgliedBE findByMemberAndTeamId(long teamId, final long memberId) {
         MannschaftsmitgliedBE test;
-        test = basicDao.selectSingleEntity(MANNSCHAFTSMITGLIED, FIND_BY_MEMBER_AND_TEAM_ID, teamId,memberId);
+        test = basicDao.selectSingleEntity(MANNSCHAFTSMITGLIED, FIND_BY_MEMBER_AND_TEAM_ID, teamId, memberId);
         return test;
-
     }
+
 
     public MannschaftsmitgliedBE create(final MannschaftsmitgliedBE mannschaftsmitgliedBE, final long currentMemberId) {
-
         basicDao.setCreationAttributes(mannschaftsmitgliedBE, currentMemberId);
 
         return basicDao.insertEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE);
     }
 
 
-    public MannschaftsmitgliedBE update(final MannschaftsmitgliedBE mannschaftsmitgliedBE,final long currentMemberId) {
+    public MannschaftsmitgliedBE update(final MannschaftsmitgliedBE mannschaftsmitgliedBE, final long currentMemberId) {
         basicDao.setModificationAttributes(mannschaftsmitgliedBE, currentMemberId);
 
-
-     return basicDao.updateEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE, MANNSCHAFTSMITGLIED_BE_USER_ID);
-
+        return basicDao.updateEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE, MANNSCHAFTSMITGLIED_BE_USER_ID);
     }
+
 
     public void delete(final MannschaftsmitgliedBE mannschaftsmitgliedBE, final long currentMemberId) {
         basicDao.setModificationAttributes(mannschaftsmitgliedBE, currentMemberId);
 
-        basicDao.deleteEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE,MANNSCHAFTSMITGLIED_BE_USER_ID);
-
+        basicDao.deleteEntity(MANNSCHAFTSMITGLIED, mannschaftsmitgliedBE, MANNSCHAFTSMITGLIED_BE_USER_ID);
     }
 
-    public boolean checkExistingSchuetze(long teamId, final long memberId){
-        return findByMemberAndTeamId(teamId,memberId).isDsbMitgliedEingesetzt();
+
+    public boolean checkExistingSchuetze(long teamId, final long memberId) {
+        return findByMemberAndTeamId(teamId, memberId).isDsbMitgliedEingesetzt();
     }
 }
