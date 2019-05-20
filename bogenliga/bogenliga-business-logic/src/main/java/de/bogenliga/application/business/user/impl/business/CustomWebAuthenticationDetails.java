2e7ba636c2119f767dd6a6d2ef2e022e417a66f5
KayStudentReutlingen <kay_arian_ranko.scheerer@student.reutlingen-university.de>
2019-05-20 15:53:04 +0200

    2FA

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/business/CustomWebAuthenticationDetails.java
@@ -0,0 +1,19 @@
+package de.bogenliga.application.business.user.impl.business;
+
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+public class CustomWebAuthenticationDetails extends WebAuthenticationDetails {
+
+    private String verificationCode;
+
+    public CustomWebAuthenticationDetails(HttpServletRequest request) {
+        super(request);
+        verificationCode = request.getParameter("code");
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+}
