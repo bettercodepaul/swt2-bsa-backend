def08d51844d2b011b2a3e98f6bfc74cab57a23c
SebEckl <sebastian_alois.eckl@student.reutlingen-university.de>
2019-05-25 11:40:47 +0200

    adjusted ArrayList in generateSetzliste (SetzlisteService)

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/setzliste/service/SetzlisteService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/setzliste/service/SetzlisteService.java
@@ -1,6 +1,6 @@
 package de.bogenliga.application.services.v1.setzliste.service;
 
-import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -8,9 +8,7 @@ import java.util.ListIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,12 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import de.bogenliga.application.business.Setzliste.api.SetzlisteComponent;
 import de.bogenliga.application.business.match.api.types.MatchDO;
 import de.bogenliga.application.common.service.ServiceFacade;
-import de.bogenliga.application.common.service.UserProvider;
 import de.bogenliga.application.common.validation.Preconditions;
 import de.bogenliga.application.services.v1.match.mapper.MatchDTOMapper;
 import de.bogenliga.application.services.v1.match.model.MatchDTO;
-import de.bogenliga.application.springconfiguration.security.permissions.RequiresPermission;
-import de.bogenliga.application.springconfiguration.security.types.UserPermission;
 
 /**
  * This is a rest resource that generates the matches.
@@ -70,144 +65,7 @@ public class SetzlisteService implements ServiceFacade {
         Preconditions.checkNotNull(wettkampfid, "wettkampfid must not be null");
 
         List<MatchDO>  matchDOList = this.setzlisteComponent.generateMatchesBySetzliste(wettkampfid);
-        List<MatchDTO> matchDTOList = new List<MatchDTO>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-
-            @Override
-            public Iterator<MatchDTO> iterator() {
-                return null;
-            }
-
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-
-            @Override
-            public boolean add(MatchDTO matchDTO) {
-                return false;
-            }
-
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-
-            @Override
-            public boolean addAll(Collection<? extends MatchDTO> c) {
-                return false;
-            }
-
-
-            @Override
-            public boolean addAll(int index, Collection<? extends MatchDTO> c) {
-                return false;
-            }
-
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-
-            @Override
-            public void clear() {
-
-            }
-
-
-            @Override
-            public MatchDTO get(int index) {
-                return null;
-            }
-
-
-            @Override
-            public MatchDTO set(int index, MatchDTO element) {
-                return null;
-            }
-
-
-            @Override
-            public void add(int index, MatchDTO element) {
-
-            }
-
-
-            @Override
-            public MatchDTO remove(int index) {
-                return null;
-            }
-
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-
-            @Override
-            public ListIterator<MatchDTO> listIterator() {
-                return null;
-            }
-
-
-            @Override
-            public ListIterator<MatchDTO> listIterator(int index) {
-                return null;
-            }
-
-
-            @Override
-            public List<MatchDTO> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
+        ArrayList<MatchDTO> matchDTOList = new ArrayList<>();
         for (MatchDO matchDO : matchDOList) {
             matchDTOList.add(MatchDTOMapper.toDTO.apply(matchDO));
         }
