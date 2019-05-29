60145fdc5f92c9db5442bef44747452454a8a2f9
Fannni0142 <fanni_tamara.marosi@student.reutlingen-university.de>
2019-05-29 12:33:50 +0200

    merged section_31 master and userstory
    Signed-off-by: Fannni0142 <fanni_tamara.marosi@student.reutlingen-university.de>

---
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/veranstaltung/service.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.veranstaltung;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class service {
+}
