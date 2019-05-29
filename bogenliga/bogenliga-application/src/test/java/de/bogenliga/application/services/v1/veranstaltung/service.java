2531a5c79e7f15c18c64bf9a925c9280a91d362b
Fannni0142 <fanni_tamara.marosi@student.reutlingen-university.de>
2019-05-29 16:05:43 +0200

    VeranstaltungServiceTest test
    Signed-off-by: Fannni0142 <fanni_tamara.marosi@student.reutlingen-university.de>

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/veranstaltung/service.java
+++
@@ -1,9 +0,0 @@
-package de.bogenliga.application.services.v1.veranstaltung;
-
-/**
- * TODO [AL] class documentation
- *
- * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
- */
-public class service {
-}
