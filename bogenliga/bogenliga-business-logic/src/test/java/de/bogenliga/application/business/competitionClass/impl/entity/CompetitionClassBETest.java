0e8a5701de58078a0fb058e0992563e2fb1d1656
Dominik Halle <dominik_jonathan.halle@student.reutlingen-university.de>
2019-06-07 10:38:20 +0200

    add missing (?) files after switching to ignorecase=false in git config

---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/competitionClass/impl/entity/CompetitionClassBETest.java
@@ -0,0 +1,52 @@
+package de.bogenliga.application.business.competitionClass.impl.entity;
+
+import org.junit.Test;
+import de.bogenliga.application.business.competitionclass.impl.entity.CompetitionClassBE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static de.bogenliga.application.business.competitionClass.impl.business.CompetitionClassComponentImplTest.getCompetitionClassBE;
+import static org.junit.Assert.*;
+
+/**
+ * @author Leila Taraman, Leila.Taraman@Student.Reutlingen-University.DE
+ */
+public class CompetitionClassBETest {
+
+    private static final long USER = 0L;
+
+    private static final long ID = 42L;
+    private static final String KLASSENAME = "Herren";
+    private static final long KLASSEJAHRGANGMIN = 10L;
+    private static final long KLASSEJAHRGANGMAX = 50L;
+    private static final long KLASSENR = 1337L;
+
+
+    @Test
+    public void assertToString(){
+        final CompetitionClassBE underTest = getCompetitionClassBE();
+        underTest.setKlasseId(ID);
+        underTest.setKlasseName(KLASSENAME);
+
+        final String actual = underTest.toString();
+
+        assertThat(actual)
+                .isNotEmpty()
+                .contains(Long.toString(ID))
+                .contains(KLASSENAME);
+
+    }
+
+    @Test
+    public void assertToStringWithOutKlasseName(){
+        final CompetitionClassBE underTest = new CompetitionClassBE();
+        underTest.setKlasseId(ID);
+        underTest.setKlasseName(null);
+
+        final String actual = underTest.toString();
+
+        assertThat(actual)
+                .isNotEmpty()
+                .contains(Long.toString(ID))
+                .contains("null");
+    }
+
+}
