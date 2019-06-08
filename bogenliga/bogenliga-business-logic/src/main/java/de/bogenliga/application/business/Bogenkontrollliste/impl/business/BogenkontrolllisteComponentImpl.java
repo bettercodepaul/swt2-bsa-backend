92bf8bdae044ae8be4129b942e912c2fcce6b002
HesseM <Michael_Maximilian.Hesse@Student.Reutlingen-University.DE>
2019-06-08 11:28:29 +0200

    adding Components to Bogenkontrollliste

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Bogenkontrollliste/impl/business/BogenkontrolllisteComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Bogenkontrollliste/impl/business/BogenkontrolllisteComponentImpl.java
@@ -1,15 +1,41 @@
 package de.bogenliga.application.business.Bogenkontrollliste.impl.business;
 
-import com.itextpdf.layout.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import de.bogenliga.application.business.Bogenkontrollliste.api.BogenkontrolllisteComponent;
-import de.bogenliga.application.business.match.api.types.MatchDO;
+import de.bogenliga.application.business.dsbmannschaft.api.DsbMannschaftComponent;
+import de.bogenliga.application.business.veranstaltung.api.VeranstaltungComponent;
+import de.bogenliga.application.business.vereine.api.VereinComponent;
+import de.bogenliga.application.business.wettkampf.api.WettkampfComponent;
+import com.itextpdf.layout.Document;
 
 /**
  * TODO [AL] class documentation
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
+@Component
 public class BogenkontrolllisteComponentImpl implements BogenkontrolllisteComponent {
+
+
+    private static final String PRECONDITION_WETTKAMPFID = "wettkampfid cannot be negative";
+
+    private final DsbMannschaftComponent dsbMannschaftComponent;
+    private final VereinComponent vereinComponent;
+    private final WettkampfComponent wettkampfComponent;
+    private final VeranstaltungComponent veranstaltungComponent;
+
+    @Autowired
+    public BogenkontrolllisteComponentImpl(final DsbMannschaftComponent dsbMannschaftComponent,
+                                           final VereinComponent vereinComponent,
+                                           final WettkampfComponent wettkampfComponent,
+                                           final VeranstaltungComponent veranstaltungComponent) {
+        this.dsbMannschaftComponent = dsbMannschaftComponent;
+        this.vereinComponent = vereinComponent;
+        this.wettkampfComponent = wettkampfComponent;
+        this.veranstaltungComponent = veranstaltungComponent;
+    }
+
     @Override
     public byte[] getBogenkontrolllistePDFasByteArray(long wettkampfid) {
         return new byte[0];
@@ -20,5 +46,5 @@ public class BogenkontrolllisteComponentImpl implements BogenkontrolllisteCompon
      * </p>
      * @param doc document to write
      */
-    private void generateBogenkontrolllistePage(Document doc, MatchDO[] matchDOs) {}
+    private void generateBogenkontrolllistePage(Document doc) {}
 }
