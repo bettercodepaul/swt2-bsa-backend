27e76b1d71abe15a2009b2009155a2632e998b51
neumannmarcel <marcelneumann166@googlemail.com>
2019-06-08 12:08:07 +0200

    Started MeldezettelComponentImpl

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Meldezettel/impl/business/MeldezettelComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Meldezettel/impl/business/MeldezettelComponentImpl.java
@@ -1,6 +1,24 @@
 package de.bogenliga.application.business.Meldezettel.impl.business;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.itextpdf.kernel.geom.PageSize;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
 import de.bogenliga.application.business.Meldezettel.api.MeldezettelComponent;
+import de.bogenliga.application.business.dsbmannschaft.api.DsbMannschaftComponent;
+import de.bogenliga.application.business.match.api.MatchComponent;
+import de.bogenliga.application.business.match.api.types.MatchDO;
+import de.bogenliga.application.business.vereine.api.VereinComponent;
+import de.bogenliga.application.business.wettkampf.api.WettkampfComponent;
+import de.bogenliga.application.common.errorhandling.ErrorCode;
+import de.bogenliga.application.common.errorhandling.exception.BusinessException;
+import de.bogenliga.application.common.errorhandling.exception.TechnicalException;
+import de.bogenliga.application.common.validation.Preconditions;
 
 /**
  * TODO [AL] class documentation
@@ -8,8 +26,72 @@ import de.bogenliga.application.business.Meldezettel.api.MeldezettelComponent;
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
 public class MeldezettelComponentImpl implements MeldezettelComponent {
+
+    private final DsbMannschaftComponent dsbMannschaftComponent;
+    private final VereinComponent vereinComponent;
+    private final WettkampfComponent wettkampfComponent;
+
+    @Autowired
+    public MeldezettelComponentImpl(final DsbMannschaftComponent dsbMannschaftComponent,
+                                     final VereinComponent vereinComponent,
+                                     final WettkampfComponent wettkampfComponent) {
+        this.dsbMannschaftComponent = dsbMannschaftComponent;
+        this.vereinComponent = vereinComponent;
+        this.wettkampfComponent = wettkampfComponent;
+    }
     @Override
     public byte[] getMeldezettelPDFasByteArray(long wettkampfid) {
-        return new byte[0];
+        Preconditions.checkArgument(wettkampfid >= 0, PRECONDITION_WETTKAMPFID);
+
+        List<MatchDO> wettkampfDOList =
+        List<MatchDO> vereinDOList =
+
+        byte[] bResult;
+        if (matchDOList.size() != 0) {
+            bResult = generateDoc(matchDOList).toByteArray();
+        }else{
+            throw new BusinessException(ErrorCode.UNEXPECTED_ERROR, "Matches für den Wettkampf noch nicht erzeugt");
+        }
+        return bResult;
+    }
+
+    /**
+     * <p>writes a Meldezettel document for the Wettkampf
+     * </p>
+     */
+    private ByteArrayOutputStream generateDoc(List<MatchDO> matchDOList) {
+        ByteArrayOutputStream ret;
+        try (final ByteArrayOutputStream result = new ByteArrayOutputStream();
+             final PdfWriter writer = new PdfWriter(result);
+             final PdfDocument pdfDocument = new PdfDocument(writer);
+             final Document doc = new Document(pdfDocument, PageSize.A4)) {
+
+            //iterate through matches
+            for (long i = 1; i<=7; i++){
+                //iterate through begegnungen
+                for(long k = 1; k<=4; k++){
+                    MatchDO[] matchesBegegnung = getMatchDOsForPage(matchDOList , i, k);
+                    if(matchesBegegnung[0] != null && matchesBegegnung[1] != null) {
+                        generateSchusszettelPage(doc, matchesBegegnung);
+                        if(i != 7){
+                            doc.add(new AreaBreak());
+                        }
+                    }
+                }
+            }
+            doc.close();
+            ret = result;
+
+        } catch (final IOException e) {
+            throw new TechnicalException(ErrorCode.INTERNAL_ERROR,
+                    "PDF Dokument konnte nicht erstellt werden: " + e);
+        }
+        return ret;
+
+    }
+
+    private void generateMeldezettelPage(Document doc, MatchDO[] matchDOs)
+    {
+        Long wettkampfTag = wettkampfComponent.findById(matchDOs[0].getWettkampfId()).getWettkampfTag();
     }
 }
