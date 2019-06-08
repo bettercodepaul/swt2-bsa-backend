f2e1a79d8bfe0df21f4d4b55258c1cfc1e043920
neumannmarcel <marcelneumann166@googlemail.com>
2019-06-08 10:58:31 +0200

    Added backend files to Meldezettel

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/Meldezettel/impl/business/MeldezettelComponentImpl.java
@@ -0,0 +1,15 @@
+package de.bogenliga.application.business.Meldezettel.impl.business;
+
+import de.bogenliga.application.business.Meldezettel.api.MeldezettelComponent;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class MeldezettelComponentImpl implements MeldezettelComponent {
+    @Override
+    public byte[] getMeldezettelPDFasByteArray(long wettkampfid) {
+        return new byte[0];
+    }
+}
