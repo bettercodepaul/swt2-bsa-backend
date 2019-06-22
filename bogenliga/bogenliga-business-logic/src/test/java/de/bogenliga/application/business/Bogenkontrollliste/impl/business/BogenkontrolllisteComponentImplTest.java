111fcae664e4976482b8545547fb7d29bdcd1453
HesseM <Michael_Maximilian.Hesse@Student.Reutlingen-University.DE>
2019-06-22 09:54:18 +0200

    add Test class

---
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/Bogenkontrollliste/impl/business/BogenkontrolllisteComponentImplTest.java
@@ -0,0 +1,16 @@
+package de.bogenliga.application.business.Bogenkontrollliste.impl.business;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class BogenkontrolllisteComponentImplTest {
+
+    @Test
+    public void getBogenkontrolllistePDFasByteArray() {
+    }
+}
