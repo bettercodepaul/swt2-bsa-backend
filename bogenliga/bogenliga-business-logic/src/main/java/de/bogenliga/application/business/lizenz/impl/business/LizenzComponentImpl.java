5dccd32fecadfaf5af455744a4e3974d71e5e905
mki4 <franke-michelle@gmx.de>
2019-07-02 18:44:28 +0200

    BSAPP-117 PDF Datei angepasst, Fehlerbehandlung hinzugefügt

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/lizenz/impl/business/LizenzComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/lizenz/impl/business/LizenzComponentImpl.java
@@ -34,6 +34,7 @@ import de.bogenliga.application.business.lizenz.impl.entity.LizenzBE;
 import de.bogenliga.application.business.lizenz.impl.mapper.LizenzMapper;
 import de.bogenliga.application.business.mannschaftsmitglied.api.MannschaftsmitgliedComponent;
 import de.bogenliga.application.business.mannschaftsmitglied.api.types.MannschaftsmitgliedDO;
+import de.bogenliga.application.business.mannschaftsmitglied.impl.business.MannschaftsmitgliedComponentImpl;
 import de.bogenliga.application.business.mannschaftsmitglied.impl.dao.MannschaftsmitgliedDAO;
 import de.bogenliga.application.business.mannschaftsmitglied.impl.entity.MannschaftsmitgliedBE;
 import de.bogenliga.application.business.mannschaftsmitglied.impl.mapper.MannschaftsmitgliedMapper;
