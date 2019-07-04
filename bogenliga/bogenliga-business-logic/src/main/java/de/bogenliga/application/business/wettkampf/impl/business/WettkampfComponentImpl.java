c60a5ca4f205c44243930ea926150d2aac3b971f
mki4 <Annea.Skupnjak@student.reutlingen-university.de>
2019-07-04 13:23:28 +0200

    BSAPP-111 Passe Service in Backend and Frontend implemented + constraint for 'delete member in team, if results exist' added

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/impl/business/WettkampfComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/impl/business/WettkampfComponentImpl.java
@@ -73,6 +73,13 @@ public class WettkampfComponentImpl implements WettkampfComponent {
         return wettkampfBEList.stream().map(WettkampfMapper.toWettkampfDO).collect(Collectors.toList());
     }
 
+    @Override
+    public List<WettkampfDO> findAllByVeranstaltungId(long veranstaltungId) {
+        Preconditions.checkArgument(veranstaltungId >= 0, PRECONDITION_MSG_WETTKAMPF_VERANSTALTUNGS_ID);
+        final List<WettkampfBE> wettkampfBEList = this.wettkampfDAO.findAllByVeranstaltungId(veranstaltungId);
+        return wettkampfBEList.stream().map(WettkampfMapper.toWettkampfDO).collect(Collectors.toList());
+    }
+
 
     @Override
     public WettkampfDO create(final WettkampfDO wettkampfDO, final long currentWettkampfID) {
