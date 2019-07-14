eac565dce54788632dbadc2b152c69b516c13231
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-07-14 06:48:56 +0200

    ligatabelle - auf Basis view BE/DAO - ungetesteter Zwischenstand

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligatabelle/impl/dao/LigatabelleDAO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligatabelle/impl/dao/LigatabelleDAO.java
@@ -1,7 +1,6 @@
 package de.bogenliga.application.business.ligatabelle.impl.dao;
 
 import de.bogenliga.application.business.ligatabelle.impl.entity.LigatabelleBE;
-import de.bogenliga.application.business.role.impl.entity.RoleBE;
 import de.bogenliga.application.common.component.dao.BasicDAO;
 import de.bogenliga.application.common.component.dao.BusinessEntityConfiguration;
 import de.bogenliga.application.common.component.dao.DataAccessObject;
@@ -32,70 +31,128 @@ public class LigatabelleDAO implements DataAccessObject {
     // business entity parameter names
 
     private static final String VERANSTALTUNGID_BE = "veranstaltungId";
+    private static final String VERANSTALTUNGNAME_BE = "veranstaltungName";
+    private static final String WETTKAMPFID_BE = "wettkampfId";
     private static final String WETTKAMPFTAG_BE = "wettkampfTag";
     private static final String MANNSCHAFTID_BE = "mannschaftId";
     private static final String MANNSCHAFTNUMMER_BE = "mannschaftNummer";
-    private static final String TABELLENPLATZ_BE = "tabellenplatz";
+    private static final String VEREINID_BE = "vereinId";
+    private static final String VEREINNAME_BE = "vereinName";
     private static final String MATCHPKT_BE = "matchpkt";
     private static final String MATCHPKTGEGEN_BE = "matchpkt_gegen";
     private static final String SATZPKT_BE = "satzpkt";
     private static final String SATZPKTGEGEN_BE = "satzpkt_gegen";
-    private static final String VEREINID_BE = "verein_id";
-    private static final String VEREINNAME_BE = "verein_name";
+    private static final String SATZPKTDIFFERENZ_BE = "satzpkt_differenz";
+    private static final String SORTIERUNG_BE = "sortierung";
+    private static final String TABELLENPLATZ_BE = "tabellenplatz";
 
-    private static final String VERANSTALTUNGID_TABLE = "ligatabelle_verantsaltung_id";
+    private static final String VERANSTALTUNGID_TABLE = "ligatabelle_veranstaltung_id";
+    private static final String VERANSTALTUNGNAME_TABLE = "ligatabelle_veranstaltung_name";
+    private static final String WETTKAMPFID_TABLE = "ligatabelle_wettkampf_id";
     private static final String WETTKAMPFTAG_TABLE = "ligatabelle_wettkampf_tag";
     private static final String MANNSCHAFTID_TABLE = "ligatabelle_mannschaft_id";
     private static final String MANNSCHAFTNUMMER_TABLE = "mannschaft_Nummer";
+    private static final String VEREINID_TABLE = "ligatabelle_verein_id";
+    private static final String VEREINNAME_TABLE = "ligatabelle_verein_name";
+    private static final String MATCHPKT_TABLE = "ligatabelle_matchpkt";
+    private static final String MATCHPKTGEGEN_TABLE = "ligatabelle_matchpkt_gegen";
+    private static final String SATZPKT_TABLE = "ligatabelle_satzpkt";
+    private static final String SATZPKTGEGEN_TABLE = "ligatabelle_satzpkt_gegen";
+    private static final String SATZPKTDIFFERENZ_TABLE = "ligatabelle_satzpkt_differenz";
+    private static final String SORTIERUNG_TABLE = "ligatabelle_sortierung";
     private static final String TABELLENPLATZ_TABLE = "tabellenplatz";
-    private static final String MATCHPKT_TABLE = "matchpkt";
-    private static final String MATCHPKTGEGEN_TABLE = "matchpkt_gegen";
-    private static final String SATZPKT_TABLE = "satzpkt";
-    private static final String SATZPKTGEGEN_TABLE = "satzpkt_gegen";
-    private static final String VEREINID_TABLE = "verein_id";
-    private static final String VEREINNAME_TABLE = "verein_name";
 
     /*
      * SQL queries
      */
+
+    /* der Select liefert die aktuelle Ligatabelle zur Veranstaltung -
+    * es wird immer der "höchste Wettkampftag ermittelt un die Tabellenreihenfolge
+    * automatisch erzeugt durch die Sortierkriterien beim generieren der Row-Number
+    * ggf. mpüssen wir für die verschiedenen Liga-Formen andere Selects hinterlegen
+    * hier jetzt erst mal der Select für Match-Punkte vor Satzpunkt-Differenz
+    * sollte für Liga-Satzsystem passen
+     */
     private static final String GET_LIGATABELLE =
-            "SELECT lt.ligatabelle_tabellenplatz, " +
-                    "lt.ligatabelle_wettkampftag ," +
-                    "lt.ligatabelle_mannschaft_id, " +
-                    "ms.mannschaft_nummer, " +
-                    "lt.ligatabelle_tabellenplatz, " +
-                    "lt.ligatabelle_matchpkt, " +
-                    "lt.ligatabelle_matchpkt_gegen, " +
-                    "lt.ligatabelle_satzpkt, " +
-                    "lt.ligatabelle_satzpkt_gegen, " +
-                    "v.verein_id, " +
-                    "v.verein_name"
-                    + " FROM ligatabelle as lt"
-                    + " INNER JOIN mannschaft AS ms ON lt.veranstaltung_id = ms.mannschaft_veranstaltung_id"
-                    + " INNER JOIN verein AS v ON v.verein_id = ms.mannschaft_verein_id"
-                    + " INNER JOIN ligatabelle lt ON ms.mannschaft_id = lt.ligatabelle_mannschaft_id"
-                    + " INNER JOIN wettkampf AS wk ON vs.veranstaltung_id = wk.wettkampf_veranstaltung_id"
-                    + " WHERE wk.wettkampf_id = ?"
-                    + " AND lt.ligatabelle_mannschaft_id = ms.mannschaft_id"
-                    + " AND lt.ligatabelle_veranstaltung_id = wk.wettkampf_veranstaltung_id"
-                    + " ORDER BY lt.ligatabelle_tabellenplatz";
+        "SELECT ligatabelle_veranstaltung_id, "+
+           "ligatabelle_veranstaltung_name," +
+           "ligatabelle_wettkampf_id," +
+           "ligatabelle_wettkampf_tag," +
+           "ligatabelle_mannschaft_id," +
+           "ligatabelle_mannschaft_nummer," +
+           "ligatabelle_verein_id," +
+           "ligatabelle_verein_name," +
+           "ligatabelle_matchpkt," +
+           "ligatabelle_matchpkt_gegen," +
+           "ligatabelle_satzpkt," +
+           "ligatabelle_satzpkt_gegen," +
+           "ligatabelle_satzpkt_differenz," +
+           "ligatabelle_sortierung," +
+           "row_number()  over (" +
+             "order by ligatabelle_matchpkt desc ," +
+               "ligatabelle_matchpkt_gegen," +
+               "ligatabelle_satzpkt_differenz desc," +
+               "ligatabelle_satzpkt desc," +
+               "ligatabelle_satzpkt_gegen," +
+               "ligatabelle_sortierung," +
+               "ligatabelle_veranstaltung_id," +
+               "ligatabelle_veranstaltung_name," +
+               "ligatabelle_wettkampf_id," +
+               "ligatabelle_wettkampf_tag," +
+               "ligatabelle_mannschaft_id," +
+               "ligatabelle_mannschaft_nummer," +
+               "ligatabelle_verein_id," +
+               "ligatabelle_verein_name" +
+             ")as tabellenplatz" +
+        "from ligatabelle" +
+        "where ligatabelle_veranstaltung_id = ?" +
+        "and ligatabelle_wettkampf_tag = (" +
+            "select max(ligatabelle_wettkampf_tag) " +
+            "from ligatabelle where ligatabelle_veranstaltung_id = ?)";
 
-
+    /* der Select liefert die aktuelle Ligatabelle zur Wettkampf-ID
+     * ggf. mpüssen wir für die verschiedenen Liga-Formen andere Selects hinterlegen
+     * hier jetzt erst mal der Select für Match-Punkte vor Satzpunkt-Differenz
+     * sollte für Liga-Satzsystem passen
+     */
+    private static final String GET_LIGATABELLE_WETTKAMPF =
+            "SELECT ligatabelle_veranstaltung_id, "+
+                    "ligatabelle_veranstaltung_name," +
+                    "ligatabelle_wettkampf_id," +
+                    "ligatabelle_wettkampf_tag," +
+                    "ligatabelle_mannschaft_id," +
+                    "ligatabelle_mannschaft_nummer," +
+                    "ligatabelle_verein_id," +
+                    "ligatabelle_verein_name," +
+                    "ligatabelle_matchpkt," +
+                    "ligatabelle_matchpkt_gegen," +
+                    "ligatabelle_satzpkt," +
+                    "ligatabelle_satzpkt_gegen," +
+                    "ligatabelle_satzpkt_differenz," +
+                    "ligatabelle_sortierung," +
+                    "row_number()  over (" +
+                        "order by ligatabelle_matchpkt desc ," +
+                        "ligatabelle_matchpkt_gegen," +
+                        "ligatabelle_satzpkt_differenz desc," +
+                        "ligatabelle_satzpkt desc," +
+                        "ligatabelle_satzpkt_gegen," +
+                        "ligatabelle_sortierung," +
+                        "ligatabelle_veranstaltung_id," +
+                        "ligatabelle_veranstaltung_name," +
+                        "ligatabelle_wettkampf_id," +
+                        "ligatabelle_wettkampf_tag," +
+                        "ligatabelle_mannschaft_id," +
+                        "ligatabelle_mannschaft_nummer," +
+                        "ligatabelle_verein_id," +
+                        "ligatabelle_verein_name" +
+                        ")as tabellenplatz" +
+                    "from ligatabelle" +
+                    "where ligatabelle_wettkampf_id = ?" ;
 
 
     // wrap all specific config parameters
     private static final BusinessEntityConfiguration<LigatabelleBE> LIGATABELLE = new BusinessEntityConfiguration<>(
             LigatabelleBE.class, TABLE, getColumnsToFieldsMap(), LOG);
-
-
-    /*
-     * SQL queries
-     */
-
-    private static final String FIND_BY_WETTKAMPF_ID =
-            "SELECT * "
-                    + " FROM rolle "
-                    + " WHERE upper(rolle_name) = upper(?)";
 
 
 
@@ -117,28 +174,39 @@ public class LigatabelleDAO implements DataAccessObject {
     private static Map<String, String> getColumnsToFieldsMap() {
         final Map<String, String> columnsToFieldsMap = new HashMap<>();
 
-        columnsToFieldsMap.put(ROLE_TABLE_ID, ROLE_BE_ID);
-        columnsToFieldsMap.put(ROLE_TABLE_NAME, ROLE_NAME);
+
+        columnsToFieldsMap.put(VERANSTALTUNGID_TABLE, VERANSTALTUNGID_BE);
+        columnsToFieldsMap.put(VERANSTALTUNGNAME_TABLE, VERANSTALTUNGNAME_BE);
+        columnsToFieldsMap.put(WETTKAMPFID_TABLE, WETTKAMPFID_BE);
+        columnsToFieldsMap.put(WETTKAMPFTAG_TABLE, WETTKAMPFTAG_BE);
+        columnsToFieldsMap.put(MANNSCHAFTID_TABLE, MANNSCHAFTID_BE);
+        columnsToFieldsMap.put(MANNSCHAFTNUMMER_TABLE, MANNSCHAFTNUMMER_BE);
+        columnsToFieldsMap.put(VEREINID_TABLE, VEREINID_BE);
+        columnsToFieldsMap.put(VEREINNAME_TABLE, VEREINNAME_BE);
+        columnsToFieldsMap.put(MATCHPKT_TABLE, MATCHPKT_BE);
+        columnsToFieldsMap.put(MATCHPKTGEGEN_TABLE, MATCHPKTGEGEN_BE);
+        columnsToFieldsMap.put(SATZPKT_TABLE, SATZPKT_BE);
+        columnsToFieldsMap.put(SATZPKTGEGEN_TABLE, SATZPKTGEGEN_BE);
+        columnsToFieldsMap.put(SATZPKTDIFFERENZ_TABLE, SATZPKTDIFFERENZ_BE);
+        columnsToFieldsMap.put(SORTIERUNG_TABLE, SORTIERUNG_BE);
+        columnsToFieldsMap.put(TABELLENPLATZ_TABLE, TABELLENPLATZ_BE);
 
         return columnsToFieldsMap;
     }
 
     /**
-     * Return all role entries
+     * Lesen der aktuellen Liga-Tabelle zur Veranstaltung
      */
-    public List<RoleBE> findAll() {
-        return basicDao.selectEntityList(ROLE, FIND_ALL);
+    public List<LigatabelleBE> getLigatabelleVeranstaltung(final long id) {
+        return basicDao.selectEntityList(LIGATABELLE, GET_LIGATABELLE);
     }
 
     /**
-     * Return user entry with specific email adress
-     *
-     * @param roleName - role name to search
+     * Lesen der aktuellen Liga-Tabelle zum Wettkampf (ID)
      */
-    public RoleBE findByName(final String roleName) {
-        return basicDao.selectSingleEntity(ROLE, FIND_BY_NAME, roleName);
+    public List<LigatabelleBE> getLigatabelleWettkampf(final long id) {
+        return basicDao.selectEntityList(LIGATABELLE, GET_LIGATABELLE_WETTKAMPF);
     }
-
 
 
 
