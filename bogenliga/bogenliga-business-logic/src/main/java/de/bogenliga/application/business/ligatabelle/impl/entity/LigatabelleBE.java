899512298cba7f92ebd33a1e347af9ae69d8746c
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2019-07-14 06:48:56 +0200

    ligatabelle - auf Basis view BE/DAO - ungetesteter Zwischenstand

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligatabelle/impl/entity/LigatabelleBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligatabelle/impl/entity/LigatabelleBE.java
@@ -13,17 +13,21 @@ import de.bogenliga.application.common.component.entity.CommonBusinessEntity;
 public class LigatabelleBE extends CommonBusinessEntity implements BusinessEntity {
     private static final long serialVersionUID = -7930719922483666804L;
 
-    private long veranstaltungId;
+    private Long veranstaltungId;
+    private String veranstaltungName;
+    private Long wettkampfId;
     private int wettkampfTag;
-    private long mannschaftId;
+    private Long mannschaftId;
     private int mannschaftNummer;
-    private int tabellenplatz;
+    private Long vereinId;
+    private String vereinName;
     private int matchpkt;
     private int matchpkt_gegen;
     private int satzpkt;
     private int satzpkt_gegen;
-    private long vereinId;
-    private String vereinName;
+    private int satzpkt_differenz;
+    private int sortierung;
+    private int tabellenplatz;
 
 
 
@@ -34,20 +38,47 @@ public class LigatabelleBE extends CommonBusinessEntity implements BusinessEntit
         // empty
     }
 
+    /** hier der select um alle übergeordneten Ligen zu einer Liga zu finden:
+     * with recursive cte AS (
+     * 	select liga_id, liga_uebergeordnet, 1 as level
+     * 	from liga
+     *
+     * 	union all
+     * 	select t.liga_id, c.liga_uebergeordnet, c.level +1
+     * 	from cte  c
+     * 	join liga t on t.liga_uebergeordnet = c.liga_id
+     * 	)select * from cte where liga_uebergeordnet notnull;
+     *
+     * 	beim select am Ende noch einschränken auf die gesuchte liga_id
+     *
+     *
+     */
+
+    /*
+     *
+     *
+     * @return
+     */
+
+
     @Override
     public String toString() {
         return "LigatabelleBE{" +
                 "veranstaltungId=" + veranstaltungId +
+                ", veranstaltungName=" + veranstaltungId +
+                ", wettkampfId='" + wettkampfId +
                 ", wettkampfTag='" + wettkampfTag +
                 ", mannschaftId='" + mannschaftId +
                 ", mannschaftNummer='" + mannschaftNummer +
-                ", tabellenplatz='" + tabellenplatz +
+                ", vereinId='" + vereinId +
+                ", vereinName='" + vereinName +
                 ", matchpkt='" + matchpkt + '\'' +
                 ", matchpkt_gegen='" + matchpkt_gegen +
                 ", satzpkt='" + satzpkt +
                 ", satzpkt_gegen='" + satzpkt_gegen +
-                ", vereinId='" + vereinId +
-                ", vereinName='" + vereinName + '\'' +
+                ", satzpkt_differenz='" + satzpkt_differenz +
+                ", sortierung='" + sortierung +
+                ", tabellenplatz='" + tabellenplatz +
                 '}';
     }
 
