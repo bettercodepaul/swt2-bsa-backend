4e9325cd9fc2f8058511fd27f3f0ffb0cc1ac7f1
mpietschmann <michael.pietschmann@exxcellent.de>
2019-09-05 11:04:20 +0200

    Rechteprüfung: Ändern der Nummer der benutzer_rollen Tabelle.(Von 45 auf 58), da rolle_insert erst bei 56.

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V58__benutzer_rolle__insert_test_data.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V58__benutzer_rolle__insert_test_data.sql
@@ -13,7 +13,7 @@ INSERT INTO benutzer_rolle(
        (3, 5),
        (5, 5)
 ;*/
-VALUES (1, 101), -- admin
+/*VALUES (1, 101), -- admin
        (2, 106), -- ligaleiter
        (3, 103),
        (4, 106), -- ligaleiter
@@ -25,6 +25,22 @@ VALUES (1, 101), -- admin
        (6, 105),
        (7, 106), -- ligaleiter (Team)
        (8, 104), -- sportleiter (Team)
+       (9, 102) -- moderator (Team)
+;*/
+
+VALUES (1, 1), -- admin
+       (2, 2), -- ligaleiter
+       (3, 6),
+       (4, 2), -- ligaleiter
+       (4, 3),
+       (5, 4), -- ausrichter
+       (1, 4),
+       (2, 5),
+       (3, 5),
+       (5, 5),
+       (6, 8), -- default user
+       (7, 2), -- ligaleiter (Team)
+       (8, 5), -- sportleiter (Team)
        (9, 102) -- moderator (Team)
 ;
 
