ccd40d13a2c58545fa41320831fd22687faaf92c
mpietschmann <michael.pietschmann@exxcellent.de>
2019-10-24 11:32:55 +0200

    BSAPP_485: SearchPath entfernt aus der Mannschaftsmitglied.sql Datei. Number-Overflow des Lizenz-Sqls behoben.

--- bogenliga/bogenliga-application/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1041__insert_lizenz.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/PROD-DATA-MIGRATION/V1041__insert_lizenz.sql
@@ -1,8 +1,8 @@
 
 
-
 -- Migration Tabelle "lizenzneutabelle" -- macht keinen Sinn, da steht nichts drin
 -- wir gegenerieren hier eine Lizenz für alle Mannschaftsmitglieder
+-- 999999999999S = 5 Stellen für region_id, 6 Stellen für dsb_mitglied_id, 1 Stelle für die 0
 
 
 DELETE from lizenz;
@@ -15,16 +15,16 @@ insert INTO lizenz
     lizenz_disziplin_id
     )
 SELECT
-  to_number((verein_region_id || '0' || mannschaftsmitglied.mannschaftsmitglied_dsb_mitglied_id), '999D99S') ,
-  verein.verein_region_id,
-   mannschaftsmitglied.mannschaftsmitglied_dsb_mitglied_id,
-    'Liga',
-    '0'
-from
-  verein,
-  mannschaft,
-  mannschaftsmitglied
+       to_number(TRIM(LEADING '0' FROM (CAST(verein_region_id AS TEXT))) || '0' ||
+                 TRIM(LEADING '0' FROM (CAST(mannschaftsmitglied.mannschaftsmitglied_dsb_mitglied_id AS TEXT))),
+                 '999999999999S'),
+       verein.verein_region_id,
+       mannschaftsmitglied.mannschaftsmitglied_dsb_mitglied_id,
+       'Liga',
+       '0'
+from verein,
+     mannschaft,
+     mannschaftsmitglied
 where verein.verein_id = mannschaft.mannschaft_verein_id
-and mannschaftsmitglied.mannschaftsmitglied_mannschaft_id = mannschaft.mannschaft_id
+  and mannschaftsmitglied.mannschaftsmitglied_mannschaft_id = mannschaft.mannschaft_id
 ;
-
