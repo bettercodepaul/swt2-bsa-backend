4bb64bfe843ee7efc42f28d934e7e19f0857e187
Tobias Schuster <tobias.schuster@student.reutlingen-university.de>
2019-12-13 18:07:25 +0100

    Fixed Issue BSAPP-488
    Added Password Validity-Check on the server-side (as well as on the client-side). Throws an exception if the password is not valid.
    
    Created tests for the regular expression.

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/user/service/UserServiceTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/user/service/UserServiceTest.java
@@ -704,4 +704,44 @@ public class UserServiceTest {
 
     }
 
+    @Test
+    public void create_withInvalidPassword_shouldThrowException() {
+
+        // prepare test data identity user
+        final UserSignInDTO userSignInDTO = new UserSignInDTO();
+        userSignInDTO.setJwt(JWT);
+        userSignInDTO.setEmail(USERNAME);
+        userSignInDTO.setId(ID);
+
+        // configure mocks
+        when(requestWithHeader.getHeader(anyString())).thenReturn("Bearer " + JWT);
+        when(jwtTokenProvider.resolveUserSignInDTO(anyString())).thenReturn(userSignInDTO);
+        when(jwtTokenProvider.getUserId(any())).thenReturn(ID);
+
+        //prepare test data newUser
+
+        final UserCredentialsDTO userCredentialsDTO = new UserCredentialsDTO();
+        userCredentialsDTO.setUsername(USERNAME);
+
+        String[] invalidPasswords = new String[]{
+                "ABCabc0",
+                "ABCABC00",
+                "abcabc00",
+                "ABCabcABC",
+                "ABCABCABC",
+                "abcabcabc",
+                "        ",
+                "0123456789",
+        };
+
+        // Check different passwords for their validity
+        for (String invalidPassword : invalidPasswords) {
+            userCredentialsDTO.setPassword(invalidPassword);
+            assertThatExceptionOfType(BusinessException.class)
+                    .isThrownBy(() -> underTest.create(requestWithHeader, userCredentialsDTO))
+                    .withMessageContaining("This is not a valid Password")
+                    .withNoCause();
+        }
+    }
+
 }
