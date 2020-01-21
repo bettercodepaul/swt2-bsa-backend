4efa6e7de7532367b60a753bd0f07b5ee0766320
AnGsmn <annika.gessmann@student.reutlingen-university.de>
2020-01-21 12:58:49 +0100

    Backend Erweiterung bogenliga-application Spotter;

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/spotter/service/SpotterService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/spotter/service/SpotterService.java
@@ -1,9 +1,13 @@
 package de.bogenliga.application.services.v1.spotter.service;
 
+import de.bogenliga.application.common.service.ServiceFacade;
+
 /**
  * TODO [AL] class documentation
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
-public class SpotterService {
+public class SpotterService implements ServiceFacade {
+
+
 }
