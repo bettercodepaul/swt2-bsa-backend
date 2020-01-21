9b0e4d652fd1bd2fde05ac1a4d5a4d7c6a9e9e4f
AnGsmn <annika.gessmann@student.reutlingen-university.de>
2020-01-21 12:54:08 +0100

    Backend Erweiterung bogenliga-application Spotter (mapper, service, model)

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/spotter/model/SpotterDTO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.spotter.model;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class SpotterDTO {
+}
