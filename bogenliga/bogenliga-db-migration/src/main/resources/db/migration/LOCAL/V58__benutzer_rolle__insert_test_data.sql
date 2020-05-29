5a8cb5e769e9fa4d35d4fc904af3f96d3f148404
Bjoern Alpermann <bjoern_daniel.alpermann@student.reutlingen-university.de>
2020-05-29 13:00:03 +0200

    Trage korrekte Rollenzuweisung ein.

--- bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V58__benutzer_rolle__insert_test_data.sql
+++ bogenliga/bogenliga-db-migration/src/main/resources/db/migration/LOCAL/V58__benutzer_rolle__insert_test_data.sql
@@ -18,11 +18,11 @@ VALUES (1, 1), -- admin
        (3, 6),
        (4, 2), -- ligaleiter
        (4, 3),
-       (5, 5), -- ausrichter
+       (5, 4), -- ausrichter
        (1, 4),
        (2, 5),
        (3, 5),
-       (5, 6),
+       (5, 5),
        (6, 8), -- default user
        (7, 2), -- ligaleiter (Team)
        (8, 5), -- sportleiter (Team)
