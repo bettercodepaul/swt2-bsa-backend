a06d4981b37d5ed862f6169bb2d79b1dd2b32808
Angela-Schneider <Angela_Sophie_Margaret.Schneider@Student.Reutlingen-University.de>
2020-06-04 16:53:29 +0200

    ~MatchServiceTest.java, ~WettkampfServiceTest.java (Angepasst auf Aenderungen) ~WettkampfDTOMapper.java, WettkampfDTO.java (+Attribut KampfrichterID)

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/wettkampf/service/WettkampfServiceTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/wettkampf/service/WettkampfServiceTest.java
@@ -50,6 +50,7 @@ public class WettkampfServiceTest {
     private static final long mannschafts_id = 1;
     private static final OffsetDateTime created_At_Utc = OffsetDateTime.now();
     private static final long version = 1234;
+    private static final long wettkampf_kampfrichter_Id = 8;
 
 
     @Rule
@@ -83,6 +84,7 @@ public class WettkampfServiceTest {
         expectedBE.setWettkampfOrt(wettkampf_Ort);
         expectedBE.setWettkampfTypId(wettkampf_Wettkampftyp_Id);
         expectedBE.setWettkampfTag(wettkampf_Tag);
+        expectedBE.setKampfrichterId(wettkampf_kampfrichter_Id);
 
         return expectedBE;
     }
@@ -99,7 +101,8 @@ public class WettkampfServiceTest {
                 wettkampf_Wettkampftyp_Id,
                 created_At_Utc,
                 user_Id,
-                version
+                version,
+                wettkampf_kampfrichter_Id
                 );
     }
 
@@ -113,7 +116,8 @@ public class WettkampfServiceTest {
                 wettkampf_Tag,
                 wettkampf_Disziplin_Id,
                 wettkampf_Wettkampftyp_Id,
-                version
+                version,
+                wettkampf_kampfrichter_Id
 
         );
 
