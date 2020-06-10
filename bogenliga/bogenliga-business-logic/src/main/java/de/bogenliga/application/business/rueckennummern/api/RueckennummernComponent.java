70f061d1a9b0a6cf3c251ac8ef73e96380e2bcc5
JWinkler00 <Jonas.Winkler@Student.Reutlingen-University.de>
2020-06-10 21:09:35 +0200

    + RueckennummernComponent und RueckennummernComponentImpl -> Datensammlung und ByteArray Erstellung

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/rueckennummern/api/RueckennummernComponent.java
@@ -0,0 +1,20 @@
+package de.bogenliga.application.business.rueckennummern.api;
+
+import de.bogenliga.application.common.component.ComponentFacade;
+
+/**
+ * Responsible for the rueckennummern database requests.
+ *
+ * @author Jonas Winkler, jonas.winkler@student.reutlingen-university.de
+ */
+public interface RueckennummernComponent extends ComponentFacade {
+
+    /**
+     * Generates a pdf as binary document for one selected mannschaftsmitglied
+     * @param dsbMannschaftsId ID of the mannschaft
+     * @param dsbMitgliedId ID of the dsbMitglied
+     * @return pdf as binary document
+     */
+    byte[] getRueckennummernPDFasByteArray(long dsbMannschaftsId, long dsbMitgliedId);
+
+}
