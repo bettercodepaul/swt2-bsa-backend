867b80f9974c8192b5c08e1c8d2aa47d3a82ba02
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2020-06-28 08:10:46 +0200

    explizite Commits um Postgres SQL Locks in einer Transaktion zu vermeiden

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V308__benutzer_alter.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V308__benutzer_alter.sql
@@ -8,10 +8,13 @@ INSERT INTO dsb_mitglied (dsb_mitglied_vorname,
 VALUES ('Gero', 'Gras', '1990-03-16', 'D', 'WT1009', 0)
 ON CONFLICT DO NOTHING;
 
+COMMIT;
 
 /* neue Spalte in Tabelle einfügen mit Defaul-Wert */
 ALTER TABLE benutzer
     ADD COLUMN benutzer_dsb_mitglied_id DECIMAL(19,0) DEFAULT 0;
+
+COMMIT;
 
 /* Neue Spalte korrekt befüllen - im ersten Schritt sind alle existierenden Benutzer == Gero */
 UPDATE
@@ -28,6 +31,7 @@ WHERE
 ALTER TABLE dsb_mitglied
     DROP CONSTRAINT IF EXISTS fk_dsb_mitglied_benutzer_id;
 
+COMMIT;
 /*später muss  mal die alte Spalte in der DSB-Mitgliedstabelle bereinigt werden
   aktuell lasse ich sie stehen um den Umbauaufwand klein zu halten. */
 
