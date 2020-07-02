c2087d6a0d13c9db8812ed507fab940206d49b89
fipsi.schmidt@gmail.com <fdsawertP1@>
2020-07-02 15:31:37 +0200

    Sportjahr DO DTO und Mapper, SQL-Abfragen, REST-COntroler, Bearbeitung von Veranstaltung-COmponent

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/veranstaltung/api/VeranstaltungComponent.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/veranstaltung/api/VeranstaltungComponent.java
@@ -1,6 +1,7 @@
 package de.bogenliga.application.business.veranstaltung.api;
 
 import java.util.List;
+import de.bogenliga.application.business.sportjahr.SportjahrDO;
 import de.bogenliga.application.business.veranstaltung.api.types.VeranstaltungDO;
 import de.bogenliga.application.common.component.ComponentFacade;
 
@@ -45,7 +46,7 @@ public interface VeranstaltungComponent extends ComponentFacade {
      * @param currentDsbMitglied Id of the currently logged in user that sent create request
      *
      * @return returns that created Verein
-    */
+     */
 
     VeranstaltungDO create(VeranstaltungDO veranstaltungDO, long currentDsbMitglied);
 
@@ -57,7 +58,7 @@ public interface VeranstaltungComponent extends ComponentFacade {
      * @param currentDsbMitglied id of the currently logged in user that sent the update request
      *
      * @return returns the updated verein entry
-    */
+     */
 
     VeranstaltungDO update(VeranstaltungDO veranstaltungDO, long currentDsbMitglied);
 
@@ -66,9 +67,34 @@ public interface VeranstaltungComponent extends ComponentFacade {
      *
      * @param veranstaltungDO    Veranstaltung DataObject containing atleast an id
      * @param currentDsbMitglied id of the currently logged in user that sent the delete request
-    */
+     */
 
     void delete(VeranstaltungDO veranstaltungDO, long currentDsbMitglied);
+
+
+    /**
+     * Returns a "Veranstaltung" with the given id
+     *
+     * @param ligado ID of the liga to be queried from the database.
+     *
+     * @return returns the queried Veranstaltung
+     */
+
+    /**
+     *
+     * @param sportjahr
+     * @return a list with Veranstaltungen with the same Sportjahr
+     */
+    List<VeranstaltungDO> findBySportjahr(long sportjahr);
+
+    /**
+     *
+     * @return a list with all Sportjahre distinct
+     */
+
+    List<SportjahrDO> findAllSportjahreDestinct();
+
+
 
 
 
