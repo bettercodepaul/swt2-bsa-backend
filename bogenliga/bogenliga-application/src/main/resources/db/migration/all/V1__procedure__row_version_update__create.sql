638140f94b415192fc2ab53c7ea21a377059db1f
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-07-15 16:32:58 +0200

    Merge all database scripts to V0 and activate out-of-order-migration

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V1__procedure__row_version_update__create.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION update_row_version()
-  returns TRIGGER AS $update_row_version_trigger$
-BEGIN
-  new.version = old.version + 1;
-  return new;
-END
-$update_row_version_trigger$
-LANGUAGE plpgsql;
