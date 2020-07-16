72b7bd0e0fcfabbcd26d4e9cbe45143c03867266
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-07-16 09:18:24 +0200

    Use absolute path

--- check_sonar_quality_gate.sh
+++ check_sonar_quality_gate.sh
@@ -8,7 +8,7 @@ echo "==> Get quality gate for project $1 and branch $2"
 url=$(echo "https://sonarcloud.io/api/project_branches/list?project=$1")
 branch=$2
 
-command=$(python sonar_branch_result_parser.py $url $branch)
+command=$(python /home/travis/build/exxcellent/swt2-bsa-backend/sonar_branch_result_parser.py $url $branch)
 
 if echo "$command" | grep -q ERROR; then
     echo "==> Found ERROR! Quality gate failure"
