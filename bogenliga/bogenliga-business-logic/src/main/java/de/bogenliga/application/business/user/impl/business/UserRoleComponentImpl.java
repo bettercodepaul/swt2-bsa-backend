14d320b2dd85472a6f7c8246365b2d82989f7185
Fobiwan <fabio_silas.care@student.reutlingen-university.de>
2020-11-17 16:54:44 +0100

    Task 75 Subtask 698

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/business/UserRoleComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/user/impl/business/UserRoleComponentImpl.java
@@ -160,5 +160,52 @@ public class UserRoleComponentImpl implements UserRoleComponent {
     }
 
 
+    @Override
+    public void sendFeedback(final String text) {
+
+        List<UserRoleExtBE>  result = userRoleExtDAO.findAdminEmails();
+        String[] recipients = new String[result.size()];
+        for (int i = 0; i< result.size(); i++) {
+            recipients[i] = (result.get(i).getUserEmail());
+
+        }
+
+        final String username = "bogenligatest@gmail.com";
+        final String password = "mki4bogenliga";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("bogenligatest@gmail.com"));
+            for (String recipient : recipients) {
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse(recipient));
+            }
+            message.setSubject("Feedback");
+            message.setText(text);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
 
 }
