de78778e75d820525507b848531edf69f2510fe1
LarsBahnmueller <Lars_Herbert.Bahnmueller@Student.Reutlingen-University.DE>
2020-12-01 18:45:14 +0100

    BSAPP-722 -> 18:45 Uhr

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/api/EinstellungenComponent.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.einstellungen.api;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public interface EinstellungenComponent {
+}
---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/api/types/EinstellungenDO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.business.einstellungen.api.types;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class EinstellungenDO {
+}
