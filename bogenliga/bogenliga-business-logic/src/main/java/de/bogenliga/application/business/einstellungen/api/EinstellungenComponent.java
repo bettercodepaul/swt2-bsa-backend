7c0cdbaa27060128896650419814e25a7e96bf21
LarsBahnmueller <Lars_Herbert.Bahnmueller@Student.Reutlingen-University.DE>
2020-12-01 19:51:31 +0100

    BSAPP-722 -> 19:51 Uhr

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/api/EinstellungenComponent.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/api/EinstellungenComponent.java
@@ -1,9 +1,46 @@
 package de.bogenliga.application.business.einstellungen.api;
 
+import de.bogenliga.application.business.einstellungen.api.types.EinstellungenDO;
+import de.bogenliga.application.common.component.ComponentFacade;
+import java.util.List;
+
 /**
  * TODO [AL] class documentation
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
-public interface EinstellungenComponent {
+public interface EinstellungenComponent extends ComponentFacade {
+
+    /**
+     * Return all einstellungen entries
+     *
+     * @return list of all einstellungen in the database
+     * empty list, if no einstellungen is found
+     */
+    List<EinstellungenDO> findAll();
+
+
+    /**
+     * Update an existing Einstellungen. The Einstellungen is identified by the id.
+     *
+     * @param einstellungenDO existing EinstellungenDO to update
+     * @return persisted version of the Einstellungen
+     */
+    EinstellungenDO update(EinstellungenDO einstellungenDO, long currentEinstellungenId);
+
+    /**
+     * Create a new Einstellungen in the database.
+     *
+     * @param einstellungenDO new Einstellungen
+     * @return persisted version of the einstellungen
+     */
+    EinstellungenDO create(EinstellungenDO einstellungenDO, long currentEinstellungenId);
+
+    /**
+     * Delete an existing einstellungen. The einstellungen is identified by the id.
+     *
+     * @param einstellungenDO einstellungen to delete
+     */
+    void delete(EinstellungenDO einstellungenDO, long currentEinstellungenId);
+
 }
--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/impl/mapper/EinstellungenMapper.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/einstellungen/impl/mapper/EinstellungenMapper.java
@@ -1,9 +1,70 @@
 package de.bogenliga.application.business.einstellungen.impl.mapper;
 
+import java.sql.Time;
+import java.util.function.Function;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import de.bogenliga.application.business.einstellungen.api.types.EinstellungenDO;
+import de.bogenliga.application.business.einstellungen.impl.entity.EinstellungenBE;
+import de.bogenliga.application.common.component.mapping.ValueObjectMapper;
+import de.bogenliga.application.common.time.DateProvider;
+
 /**
- * TODO [AL] class documentation
+ * I convert the einstellungen DataObjects and BusinessEntities
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
-public class EinstellungenMapper {
+public class EinstellungenMapper implements ValueObjectMapper {
+
+    /**
+     * Converts a {@link EinstellungenBE} to a {@link EinstellungenDO}
+     *
+     */
+    public static final Function<EinstellungenBE, EinstellungenDO> toEinstellungenDO = be -> {
+
+        final Long id = be.getEinstellungenId();
+        final String value = be.getValue();
+        final String key = be.getKey();
+
+        Long createdByUserId = be.getCreatedByUserId();
+        Long lastModifiedByUserId = be.getLastModifiedByUserId();
+        Long version = be.getVersion();
+
+        OffsetDateTime createdAtUtc = DateProvider.convertTimestamp(be.getCreatedAtUtc());
+        OffsetDateTime lastModifiedAtUtc = DateProvider.convertTimestamp(be.getLastModifiedAtUtc());
+
+        return new EinstellungenDO(id, value, key);
+
+    };
+
+    /**
+     * Converts a {@link EinstellungenDO} to a {@link EinstellungenBE}
+     *
+     */
+    public static final Function<EinstellungenDO, EinstellungenBE> toEinstellungenBE = einstellungenDO -> {
+
+        Timestamp createdAtUtcTimestamp = DateProvider.convertOffsetDateTime(EinstellungenDO.getCreatedAtUtc());
+        Timestamp lastModifiedAtUtcTimestamp = DateProvider.convertOffsetDateTime((EinstellungenDO.getLastModifiedAtUtc());
+
+        EinstellungenBE einstellungenBE = new EinstellungenBE();
+        einstellungenBE.setId(einstellungenDO.getId());
+        einstellungenBE.setValue(einstellungenDO.getValue());
+        einstellungenBE.setKey(einstellungenDO.getKey());
+
+        einstellungenBE.setCreatedAtUtc(createdAtUtcTimestamp);
+        einstellungenBE.setCreatedByUserId(einstellungenDO.getCreatedByUserId());
+        einstellungenBE.setLastModifiedAtUtc(lastModifiedAtUtcTimestamp);
+        einstellungenBE.setLastModifiedByUserId(einstellungenDO.getLastModifiedByUserId());
+        einstellungenBE.setVersion(einstellungenDO.getVersion());
+
+        return einstellungenBE;
+    };
+
+    /**
+     * Private constructor
+     */
+    private EinstellungenMapper() {
+        //empty private constructor
+    }
 }
