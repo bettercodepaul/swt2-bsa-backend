dfaa80c89225f01ed55b655fb1480251778c58af
Lukas Woellhaf <lukas.woellhaf@googlemail.com>
2020-12-02 17:14:41 +0100

    BSAPP-741: Added SQL-Scripts for deleting all incorrect connections between user id's and user roles. Then inserting the correct assigned id's to the user roles

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V7_0__insert_into_benutzer_rolle_correct_roles.sql
@@ -0,0 +1,4 @@
+--insert correct user roles for the specific id's
+INSERT INTO benutzer_rolle
+    (benutzer_rolle_benutzer_id, benutzer_rolle_rolle_id)
+VALUES (1, 1), (2, 9), (3, 6), (4, 9), (5, 6), (6, 8), (7, 2), (8, 5), (9,9);
