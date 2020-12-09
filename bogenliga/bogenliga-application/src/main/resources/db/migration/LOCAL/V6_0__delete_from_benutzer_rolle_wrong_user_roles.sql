db2ac514e3ae6713f9abde9526b51094d8059759
Lukas Woellhaf <lukas.woellhaf@googlemail.com>
2020-12-09 12:02:46 +0100

    BSAPP-741: Added further comments and links to Jira Ticket

--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V6_0__delete_from_benutzer_rolle_wrong_user_roles.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V6_0__delete_from_benutzer_rolle_wrong_user_roles.sql
@@ -1,3 +1,4 @@
---temporary deletes all values to build them up later
--- because most id's are assigned to the wrong user-roles
+-- temporary deletes all values from table 'benutzer_rolle' to build them up later
+-- because most id's are assigned to the wrong user-roles or one user more than one user role
+-- see https://www.exxcellent.de/jira/browse/BSAPP-741 for more information
 DELETE FROM benutzer_rolle;
--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V7_0__insert_into_benutzer_rolle_correct_roles.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V7_0__insert_into_benutzer_rolle_correct_roles.sql
@@ -1,4 +1,5 @@
---insert correct user roles for the specific id's
+-- insert correct user roles for the specific id's
+-- see https://www.exxcellent.de/jira/browse/BSAPP-741 for more information
 INSERT INTO benutzer_rolle
     (benutzer_rolle_benutzer_id, benutzer_rolle_rolle_id)
 VALUES (1, 1), (2, 9), (3, 6), (4, 9), (5, 6), (6, 8), (7, 2), (8, 5), (9,9);
