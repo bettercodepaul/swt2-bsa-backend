87334184d1c595f5c2fb69098566025007fb3079
Fobiwan <fabio_silas.care@student.reutlingen-university.de>
2020-12-12 12:03:39 +0100

    BSAPP-724 new Script

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V10_0__update_configuration.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V10_0__update_configuration.sql
@@ -1,3 +1,22 @@
+ALTER TABLE configuration ADD COLUMN configuration_id INTEGER;
+DROP SEQUENCE IF EXISTS configuration_1;
+CREATE SEQUENCE configuration_1 START 1;
+ALTER TABLE configuration ALTER COLUMN configuration_id SET DEFAULT nextval('configuration_1');
+UPDATE configuration SET configuration_id = nextval('configuration_1');
+
+alter table configuration alter column configuration_id set not null;
+
+create unique index configuration_configuration_id_uindex
+    on configuration (configuration_id);
+
+alter table configuration drop constraint configuration_pk;
+
+alter table configuration
+    add constraint configuration_pk
+        primary key (configuration_id);
+
+
+
 ALTER TABLE IF EXISTS configuration
 ADD created_at_utc        TIMESTAMP        NOT NULL    DEFAULT (now() AT TIME ZONE 'utc');
 ALTER TABLE IF EXISTS configuration
