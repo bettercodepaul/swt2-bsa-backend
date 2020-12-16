bf0c0288b924395cc3a9b6bb64ba1edcd55da76f
yannikma <may.yan@web.de>
2020-12-16 15:42:11 +0100

    BSAPP-738: added a comment, which explains for what we need the v9-sql-script

--- bogenliga/bogenliga-application/src/main/resources/db/migration/DEV/V9_0__insert_adress_columns_wettkampf+delete_ort_column.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/DEV/V9_0__insert_adress_columns_wettkampf+delete_ort_column.sql
@@ -1,3 +1,9 @@
+/**
+ * This script is necessary, to store the adress of a "Wettkampftag",
+ * because we splitted the adress in 4 parts and deleted the single column "wettkampf_ort"
+ **/
+
+
 ALTER TABLE wettkampf
     DROP COLUMN wettkampf_ort,                     -- delete column wettkampf_ort
     ADD COLUMN wettkampf_strasse VARCHAR (30),     -- The street for the "Wettkampf Tag" will be stored in this column
