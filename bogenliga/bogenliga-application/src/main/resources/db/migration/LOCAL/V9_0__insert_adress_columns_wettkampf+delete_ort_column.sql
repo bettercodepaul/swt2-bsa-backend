0d85e44968807839e87442490d31da05908db4fb
DavinciLeonardo <davinci98@online.de>
2020-12-16 15:35:26 +0100

    Backend: rename SQL file + copy in DEV + All

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V9_0__insert_adress_columns_wettkampf+delete_ort_column.sql
@@ -0,0 +1,8 @@
+ALTER TABLE wettkampf
+    DROP COLUMN wettkampf_ort,                     -- delete column wettkampf_ort
+    ADD COLUMN wettkampf_strasse VARCHAR (30),     -- The street for the "Wettkampf Tag" will be stored in this column
+    ADD COLUMN wettkampf_plz VARCHAR (10),         -- The plz for the "Wettkampf Tag" will be stored in this column
+    ADD COLUMN wettkampf_ortsname VARCHAR (25),    -- The city name for the "Wettkampf Tag" will be stored in this column
+    ADD COLUMN wettkampf_ortsinfo VARCHAR (200);   -- Some additional adress information for the "Wettkampf Tag" will be stored in this column
+;
+
