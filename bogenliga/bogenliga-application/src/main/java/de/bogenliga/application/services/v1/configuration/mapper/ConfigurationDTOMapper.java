5a091e5b22774c39a73e4cbce8af42387e436241
Patrick762 <lutzpatti86@gmail.com>
2021-04-23 19:07:05 +0200

    added variables and methods from service/einstellungen to service/configuration

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/configuration/mapper/ConfigurationDTOMapper.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/configuration/mapper/ConfigurationDTOMapper.java
@@ -27,19 +27,21 @@ public final class ConfigurationDTOMapper implements DataTransferObjectMapper {
      * I map the {@link ConfigurationDO} object to the {@link ConfigurationDTO} object
      */
     public static final Function<ConfigurationDO, ConfigurationDTO> toDTO = vo -> {
+        final Long id = vo.getId();
         final String key = vo.getKey();
         final String value = vo.getValue();
 
-        return new ConfigurationDTO(key, value);
+        return new ConfigurationDTO(id, key, value);
     };
 
     /**
      * I map the {@link ConfigurationDTO} object to the {@link ConfigurationDO} object
      */
-    public static final Function<ConfigurationDTO, ConfigurationDO> toVO = dto -> {
+    public static final Function<ConfigurationDTO, ConfigurationDO> toDO = dto -> {
+        final Long id = dto.getId();
         final String key = dto.getKey();
         final String value = dto.getValue();
 
-        return new ConfigurationDO(key, value);
+        return new ConfigurationDO(id, key, value);
     };
 }
