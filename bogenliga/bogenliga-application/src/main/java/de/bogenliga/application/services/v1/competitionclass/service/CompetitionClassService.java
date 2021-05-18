4f4cc18742d0718e59ab2a68b4afa38db5320a85
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2021-05-18 16:01:10 +0200

    Logger gem. Sonar entfernt...

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/competitionclass/service/CompetitionClassService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/competitionclass/service/CompetitionClassService.java
@@ -3,8 +3,6 @@ package de.bogenliga.application.services.v1.competitionclass.service;
 import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +32,8 @@ public class CompetitionClassService implements ServiceFacade {
     private static final String PRECONDITION_MSG_KLASSE_JAHRGANG_MAX = "Max Age must not be negative";
     private static final String PRECONDITION_MSG_KLASSE_NR = "Something is wrong with the CompetitionClass Number";
     private static final String PRECONDITION_MSG_NAME = "The CompetitionClass must be given a name";
-    private static final  String steuerzeichen = "[\n\r\t]" ;
 
 
-    private final Logger logger = LoggerFactory.getLogger(CompetitionClassService.class);
 
     private final CompetitionClassComponent competitionClassComponent;
 
@@ -80,8 +76,6 @@ public class CompetitionClassService implements ServiceFacade {
     public CompetitionClassDTO findById(@PathVariable("id") final long id){
         Preconditions.checkArgument(id >= 0, PRECONDITION_MSG_KLASSE_ID);
 
-        logger.debug("Receive 'findById' request with ID '{}'", id);
-
         final CompetitionClassDO competitionClassDO = competitionClassComponent.findById(id);
 
         return CompetitionClassDTOMapper.toDTO.apply(competitionClassDO);
@@ -101,13 +95,6 @@ public class CompetitionClassService implements ServiceFacade {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @RequiresPermission(UserPermission.CAN_CREATE_SYSTEMDATEN)
     public CompetitionClassDTO create(@RequestBody final CompetitionClassDTO competitionClassDTO, final Principal principal) {
-        logger.debug(
-                "Receive 'create' request with klasseId '{}', klasseName '{}', klasseJahrgangMin '{}', klasseJahrgangMax '{}', klasseNr '{}' ",
-                competitionClassDTO.getId().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseName().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseJahrgangMin().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseJahrgangMax().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseNr().toString().replaceAll(steuerzeichen, "_"));
 
         checkPreconditions(competitionClassDTO);
 
@@ -131,13 +118,6 @@ public class CompetitionClassService implements ServiceFacade {
     public CompetitionClassDTO update(@RequestBody final CompetitionClassDTO competitionClassDTO,
                                       final Principal principal) {
 
-        logger.debug("Receive 'update' request with  id '{}', name '{}', jahrgang_Min '{}', jahrgang_Max '{}', klasseNr '{}'",
-
-                competitionClassDTO.getId().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseName().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseJahrgangMin().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseJahrgangMax().toString().replaceAll(steuerzeichen, "_"),
-                competitionClassDTO.getKlasseNr().toString().replaceAll(steuerzeichen, "_"));
 
         checkPreconditions(competitionClassDTO);
 
