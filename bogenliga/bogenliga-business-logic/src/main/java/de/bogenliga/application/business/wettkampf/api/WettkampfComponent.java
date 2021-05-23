9a3b889f0f2d314052d674eb0767a2214f3ab524
mki4 <nehemia_berhane.ghebremussie@student.reutlingen-university.de>
2021-05-23 21:16:49 +0200

    kleiner Fehler behoben

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/api/WettkampfComponent.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/api/WettkampfComponent.java
@@ -84,7 +84,7 @@ public interface WettkampfComponent extends ComponentFacade {
 
     /**
      * Generates a pdf as binary document
-     * @param wettkampfid ID for the competition
+     * @param veranstaltungsid ID for the competition
      * @return document
      */
     byte[] getEinzelstatistikPDFasByteArray(long veranstaltungsid,long manschaftsid,int jahr);
