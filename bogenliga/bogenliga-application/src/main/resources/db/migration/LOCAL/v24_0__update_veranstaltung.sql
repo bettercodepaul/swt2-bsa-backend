d3ce5e15d65bc4364435052f88ef5a906d46ece6
dura25 <kristian2507@gmail.com>
2021-05-28 10:30:12 +0200

    alle ligen in der tabelle hinzugefügt

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v24_0__update_veranstaltung.sql
@@ -0,0 +1,12 @@
+update veranstaltung
+
+SET veranstaltung_liga_id = 3
+where veranstaltung_id = 1,
+
+SET veranstaltung_liga_id =2
+
+where veranstaltung_id = 4,
+
+SET veranstaltung_liga_id =2
+where veranstaltung_id = 4,
+
