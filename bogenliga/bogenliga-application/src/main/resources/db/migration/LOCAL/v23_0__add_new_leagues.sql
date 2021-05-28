f7840c954c0de8798e37d7b4c9e7c2ba4683bd09
dura25 <kristian2507@gmail.com>
2021-05-28 10:30:12 +0200

    alle ligen in der tabelle hinzugefügt

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v23_0__add_new_leagues.sql
@@ -0,0 +1,24 @@
+INSERT INTO liga (liga_id, liga_region_id,
+liga_name, liga_uebergeordnet,
+liga_verantwortlich, created_at_utc,
+created_by, last_modified_at_utc,
+last_modified_by, version)
+
+values
+
+(8, null, "Regionalliga Südwest", 1, 1, null, 0, null, null, 0),
+(9, null, "Relegation Landesliga Nord", 3, 1, null, 0, null, null, 0),
+(10, null, "Relegation Landesliga Süd", 4, 1, null, 0, null, null, 0),
+(11, null, "Bezirksoberliga Neckar", 10, 1, null, 0, null, null, 0),
+(12, null, "Bezirksoberliga Oberschwaben", 10, 1, null, 0, null, null, 0),
+(13, null, "Bezirksliga A Neckar", 11, 1, null, 0, null, null, 0),
+(14, null, "Bezirksliga A Oberschwaben", 12, 1, null, 0, null, null, 0),
+(15, null, "Bezirksliga B Oberschwaben", 14, 1, null, 0, null, null, 0),
+(16, null, "Württembergliga Compound", 1, 1, null, 0, null, null, 0),
+(17, null, "Landesliga A Compound", 16, 1, null, 0, null, null, 0),
+(18, null, "Landesliga B Compound", 17, 1, null, 0, null, null, 0);
+
+
+
+
+
