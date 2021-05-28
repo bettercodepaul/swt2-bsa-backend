e2186514a5c541fb4d0fe359ec82fac01fe11e79
dura25 <kristian2507@gmail.com>
2021-05-28 10:52:57 +0200

    veranstaltungstabelle (sql) geupdatet

--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v24_0__update_veranstaltung.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v24_0__update_veranstaltung.sql
@@ -3,10 +3,41 @@ update veranstaltung
 SET veranstaltung_liga_id = 3
 where veranstaltung_id = 1,
 
-SET veranstaltung_liga_id =2
+SET veranstaltung_liga_id = 4
+where veranstaltung_id = 2,
 
+SET veranstaltung_liga_id = 9
+where veranstaltung_id = 3,
+
+SET veranstaltung_liga_id = 10
 where veranstaltung_id = 4,
 
-SET veranstaltung_liga_id =2
-where veranstaltung_id = 4,
+SET veranstaltung_liga_id = 11
+where veranstaltung_id = 5,
+
+SET veranstaltung_liga_id = 13
+where veranstaltung_id = 6,
+
+SET veranstaltung_liga_id = 12
+where veranstaltung_id = 7,
+
+SET veranstaltung_liga_id = 14
+where veranstaltung_id = 8,
+
+SET veranstaltung_liga_id = 15
+where veranstaltung_id = 9,
+
+SET veranstaltung_liga_id = 19
+where veranstaltung_id = 20,
+
+SET veranstaltung_liga_id = 16
+where veranstaltung_id = 21,
+
+SET veranstaltung_liga_id = 17
+where veranstaltung_id = 22,
+
+SET veranstaltung_liga_id = 18
+where veranstaltung_id = 23;
+
+
 
