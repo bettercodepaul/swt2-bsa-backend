300d9326c9e60a6316fddb2dc1f85e7e4991b3dc
anetteadam <anetteadam@gmx.de>
2021-06-01 18:04:22 +0200

    deleted unused import

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/veranstaltung/service/VeranstaltungService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/veranstaltung/service/VeranstaltungService.java
@@ -15,7 +15,6 @@ import de.bogenliga.application.business.veranstaltung.api.VeranstaltungComponen
 import de.bogenliga.application.common.service.ServiceFacade;
 import de.bogenliga.application.common.service.UserProvider;
 import de.bogenliga.application.common.validation.Preconditions;
-import de.bogenliga.application.services.v1.dsbmannschaft.service.DsbMannschaftService;
 import de.bogenliga.application.services.v1.sportjahr.SportjahrDTO;
 import de.bogenliga.application.services.v1.sportjahr.mapper.SportjahrDTOMapper;
 import de.bogenliga.application.services.v1.veranstaltung.mapper.VeranstaltungDTOMapper;
