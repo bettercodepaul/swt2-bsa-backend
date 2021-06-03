42f4da36ddbd57cae31e844c82fa5a97c8f62633
dura25 <kristian2507@gmail.com>
2021-06-03 12:01:24 +0200

    veranstaltungstabelle (sql) geupdatet

--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v26_0__update_veranstaltung.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v26_0__update_veranstaltung.sql
@@ -1,34 +1,54 @@
 update veranstaltung
 
 SET veranstaltung_liga_id = 3
-where veranstaltung_id = 1,
+where veranstaltung_id = 1;
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 4
-where veranstaltung_id = 2,
+where veranstaltung_id = 2;
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 9
-where veranstaltung_id = 3,
+where veranstaltung_id = 3;
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 10
 where veranstaltung_id = 4,
 
+update veranstaltung
+
 SET veranstaltung_liga_id = 11
 where veranstaltung_id = 5,
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 13
 where veranstaltung_id = 6,
 
+update veranstaltung
+
 SET veranstaltung_liga_id = 12
 where veranstaltung_id = 7,
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 14
 where veranstaltung_id = 8,
 
+update veranstaltung
+
 SET veranstaltung_liga_id = 15
 where veranstaltung_id = 9,
 
+update veranstaltung
+
 SET veranstaltung_liga_id = 19
 where veranstaltung_id = 20,
+
+update veranstaltung
 
 SET veranstaltung_liga_id = 16
 where veranstaltung_id = 21,
@@ -40,4 +60,8 @@ SET veranstaltung_liga_id = 18
 where veranstaltung_id = 23;
 
 
-
+delete
+from veranstaltung
+where veranstaltung_id = 1000
+and veranstaltung_id = 1001
+and veranstaltung_id = 1002;
--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/veranstaltung/impl/business/VeranstaltungComponentImplTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/veranstaltung/impl/business/VeranstaltungComponentImplTest.java
@@ -579,4 +579,6 @@ public class VeranstaltungComponentImplTest {
     }
 
 
+
+
 }
