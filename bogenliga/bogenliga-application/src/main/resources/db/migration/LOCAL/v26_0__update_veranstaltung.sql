cb9155e8ec690c01f40cc79c700131713006adde
ValerieSchneider <valerie_judith.schneider@student.reutlingen-university.de>
2021-06-03 14:59:48 +0200

    de.json und en.json Fehlermeldung Veranstaltung eingefügt SQL Skripte V25 und V26 verbessert Zeile 160-163 auskommentiert in VeranstaltungComponentImpl

--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v26_0__update_veranstaltung.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/v26_0__update_veranstaltung.sql
@@ -1,61 +1,54 @@
-update veranstaltung
+-- ckeck and improve test table "veranstaltung" with the correct "veranstaltung_liga_id"
 
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 3
 where veranstaltung_id = 1;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 4
 where veranstaltung_id = 2;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 9
 where veranstaltung_id = 3;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 10
-where veranstaltung_id = 4,
+where veranstaltung_id = 4;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 11
-where veranstaltung_id = 5,
+where veranstaltung_id = 5;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 13
-where veranstaltung_id = 6,
+where veranstaltung_id = 6;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 12
-where veranstaltung_id = 7,
+where veranstaltung_id = 7;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 14
-where veranstaltung_id = 8,
+where veranstaltung_id = 8;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 15
-where veranstaltung_id = 9,
+where veranstaltung_id = 9;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 19
-where veranstaltung_id = 20,
+where veranstaltung_id = 20;
 
-update veranstaltung
-
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 16
-where veranstaltung_id = 21,
+where veranstaltung_id = 21;
 
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 17
-where veranstaltung_id = 22,
+where veranstaltung_id = 22;
 
+UPDATE veranstaltung
 SET veranstaltung_liga_id = 18
 where veranstaltung_id = 23;
 
