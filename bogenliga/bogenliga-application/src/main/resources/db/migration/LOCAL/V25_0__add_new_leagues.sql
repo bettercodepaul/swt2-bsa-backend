5b5f53cf7ee37296bef97328c1ed809fc0664d4c
ValerieSchneider <valerie_judith.schneider@student.reutlingen-university.de>
2021-06-05 14:22:14 +0200

    SQL Fehler verbessert

--- bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V25_0__add_new_leagues.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/LOCAL/V25_0__add_new_leagues.sql
@@ -12,48 +12,48 @@ INSERT INTO liga (
                     last_modified_by,
                     version)
 VALUES
-(8, null, 'Regionalliga Südwest', 1, 1, null, 0, null, null, 0);
+(8, 1, 'Regionalliga Südwest', 1, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(9, null, 'Relegation Landesliga Nord', 3, 1, null, 0, null, null, 0);
+(9, 1, 'Relegation Landesliga Nord', 3, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(10, null, 'Relegation Landesliga Süd', 4, 1, null, 0, null, null, 0);
+(10, 1, 'Relegation Landesliga Süd', 4, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(11, null, 'Bezirksoberliga Neckar', 10, 1, null, 0, null, null, 0);
+(11, 3, 'Bezirksoberliga Neckar', 10, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(12, null, 'Bezirksoberliga Oberschwaben', 10, 1, null, 0, null, null, 0);
+(12, 3, 'Bezirksoberliga Oberschwaben', 10, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(13, null, 'Bezirksliga A Neckar', 11, 1, null, 0, null, null, 0);
+(13, 3, 'Bezirksliga A Neckar', 11, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(14, null, 'Bezirksliga A Oberschwaben', 12, 1, null, 0, null, null, 0);
+(14, 3, 'Bezirksliga A Oberschwaben', 12, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(15, null, 'Bezirksliga B Oberschwaben', 14, 1, null, 0, null, null, 0);
+(15, 3, 'Bezirksliga B Oberschwaben', 14, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(16, null, 'Württembergliga Compound', 1, 1, null, 0, null, null, 0);
+(16, 1, 'Württembergliga Compound', 1, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(17, null, 'Landesliga A Compound', 16, 1, null, 0, null, null, 0);
+(17, 1, 'Landesliga A Compound', 16, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(18, null, 'Landesliga B Compound', 17, 1, null, 0, null, null, 0);
+(18, 1, 'Landesliga B Compound', 17, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
 
 INSERT INTO liga (liga_id, liga_region_id, liga_name, liga_uebergeordnet, liga_verantwortlich, created_at_utc, created_by, last_modified_at_utc, last_modified_by,version)
 VALUES
-(19, null, 'Württembergliga Compound Finale', 1, 1, null, 0, null, null, 0);
+(19, 1, 'Württembergliga Compound Finale', 1, 1, '2021-01-28 20:49:06.337792', 0, null, null, 0);
