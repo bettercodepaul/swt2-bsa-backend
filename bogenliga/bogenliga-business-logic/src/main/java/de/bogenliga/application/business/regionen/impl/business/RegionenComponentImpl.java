8e348cae33d873a0f50af3eaa99322fec3839bd9
Emanuel-Inf <Emanuel.Petrinovic@Student.Reutlingen-University.DE>
2021-06-08 19:01:38 +0200

    -Fixed CodeSmell. Unused Import.

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/regionen/impl/business/RegionenComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/regionen/impl/business/RegionenComponentImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import de.bogenliga.application.business.liga.api.LigaComponent;
 import de.bogenliga.application.business.lizenz.api.LizenzComponent;
-import de.bogenliga.application.business.lizenz.impl.dao.LizenzDAO;
 import de.bogenliga.application.business.regionen.api.RegionenComponent;
 import de.bogenliga.application.business.regionen.api.types.RegionenDO;
 import de.bogenliga.application.business.regionen.impl.dao.RegionenDAO;
