b631fa20257afd814c25dbb106cdb5160ce54767
MaxWeise <maxfencing@web.de>
2021-07-02 22:31:09 +0200

    Cleaned up the code to and grouped the getters and setters.  - KampfrichterDTO    + Removed the no-args constructor
     - KampfrichterExtendedDTO
       + Removed comments
       + Added explanation to toString() modifications
       + Formatting
    
     - KampfrichterExtendedDTOTest
       + Added a test method for toString()

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/kampfrichter/model/KampfrichterExtendedDTO.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/kampfrichter/model/KampfrichterExtendedDTO.java
@@ -3,71 +3,30 @@ package de.bogenliga.application.services.v1.kampfrichter.model;
 import de.bogenliga.application.common.service.types.DataTransferObject;
 
 /**
- *
  * Extended KampfrichterDTO class to KampfrichterExtendedDTO. The difference are the Attributes: "kampfrichterVorname", "kampfrichterNachname"
  * and email.
  *
  */
 public class KampfrichterExtendedDTO extends KampfrichterDTO implements DataTransferObject {
     private static final Long serialVersionUID = 8559563978424033907L;
-    // private Long userID;
-    // private Long wettkampfID;
     private String kampfrichterVorname;
     private String kampfrichterNachname;
     private String email;
-    // private boolean leitend;
-
 
     // The parameter names have to be the exact same as the attribute names in kampfrichter-do.class.ts
     public KampfrichterExtendedDTO(final Long id, final String kampfrichterVorname, final String kampfrichterNachname,
                                    final String email, final Long wettkampfID, final boolean leitend) {
         super(id, wettkampfID, leitend);
-        //this.userID = id;
-        //this.wettkampfID = wettkampfID;
-        //this.leitend = leitend;
-
-        // Additional Attributes
+        // Attributes specific to this class
         this.kampfrichterVorname = kampfrichterVorname;
         this.kampfrichterNachname = kampfrichterNachname;
         this.email= email;
-
     }
-
-
-//    public Long getUserID() {
-//        return this.userID;
-//    }
-
-
-//    public void setUserId(final Long userId) {
-//        this.userID = userId;
-//    }
-
-
-//    public Long getWettkampfID() {
-//        return wettkampfID;
-//    }
-
-
-//    public void setWettkampfId(final Long wettkampfId) {
-//        this.wettkampfID = wettkampfId;
-//    }
-
-
-//    public boolean getLeitend() {
-//        return leitend;
-//    }
-
-
-//    public void setLeitend(final boolean leitend) {
-//        this.leitend = leitend;
-//    }
 
 
     public String getKampfrichterVorname() {
         return kampfrichterVorname;
     }
-
 
     public void setKampfrichterVorname(String kampfrichterVorname) {
         this.kampfrichterVorname = kampfrichterVorname;
@@ -78,7 +37,6 @@ public class KampfrichterExtendedDTO extends KampfrichterDTO implements DataTran
         return kampfrichterNachname;
     }
 
-
     public void setKampfrichterNachname(String kampfrichterNachname) {
         this.kampfrichterNachname = kampfrichterNachname;
     }
@@ -88,12 +46,13 @@ public class KampfrichterExtendedDTO extends KampfrichterDTO implements DataTran
         return email;
     }
 
-
     public void setEmail(String email) {
         this.email = email;
     }
 
 
+    // Getters are necessary because those are
+    // private attributes of the parent class
     @Override
     public String toString() {
         return "KampfrichterExtendedDTO{" +
