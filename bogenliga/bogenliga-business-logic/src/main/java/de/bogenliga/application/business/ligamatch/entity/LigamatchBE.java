4c75a0456f80701c6692dde386f2e23fa377daf9
groteskcici <c.luzzi@web.de>
2021-11-17 15:08:06 +0100

    Grundstruktur DAO & BE für neue Views ligamatch & und ligapasse

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligamatch/entity/LigamatchBE.java
@@ -0,0 +1,35 @@
+package de.bogenliga.application.business.ligamatch.entity;
+
+import de.bogenliga.application.common.component.entity.BusinessEntity;
+import de.bogenliga.application.common.component.entity.CommonBusinessEntity;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class LigamatchBE extends CommonBusinessEntity implements BusinessEntity {
+
+
+    private Long wettkampf_id;
+    private Long match_id;
+    private int match_nr;
+    private int scheibennummer;
+    private Long mannschaft_id;
+    private String mannschaft_name;
+    private String mannschaft_name_gegner;
+    private int scheibennummer_gegner;
+    private Long id_gegner;
+    private Long naechste_match_id;
+    private Long naechste_naechste_match_nr_match_id;
+    private int strafpunkte_satz_1;
+    private int strafpunkte_satz_2;
+    private int strafpunkte_satz_3;
+    private int strafpunkte_satz_4;
+    private int strafpunkte_satz_5;
+
+
+    public LigamatchBE() {
+        //empty Contructor
+    }
+}
