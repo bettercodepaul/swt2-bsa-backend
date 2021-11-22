9304cf1b5a8636ae02f380059c8662f1f77d3503
groteskcici <c.luzzi@web.de>
2021-11-22 10:39:04 +0100

    Did some CodeQuality improvement for some variable names. Added toString Method, UID and getter + setter to Ligapasse & Ligamatch BE. Both BE´s seems to be done now.

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligapasse/impl/entity/LigapasseBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligapasse/impl/entity/LigapasseBE.java
@@ -9,19 +9,68 @@ import de.bogenliga.application.common.component.entity.CommonBusinessEntity;
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
 public class LigapasseBE extends CommonBusinessEntity implements BusinessEntity {
+    private static final long serialVersionUID = -4448642082943788879L;
 
-    private Long wettkampf_id;
-    private Long match_id;
-    private Long passe_id;
-    private int passe_lfdnr;
-    private Long passe_manschaft_id;
-    private Long dsb_mitglied_id;
-    private String dsb_mitglied_name;
-    private int mannschaftsmitglied_rueckennummer;
-    private int passe_ringzahl_pfeil1;
-    private int passe_ringzahl_pfeil2;
+    private Long wettkampfId;
+    private Long matchId;
+    private Long passeId;
+    private int passeLfdnr;
+    private Long passeMannschaftId;
+    private Long dsbMitgliedId;
+    private String dsbMitgliedName;
+    private int mannschaftsmitgliedRueckennummer;
+    private int passeRingzahlPfeil1;
+    private int passeRingzahlPfeil2;
 
 
     public LigapasseBE() {
     }
+
+
+    @Override
+    public String toString() {
+        return "LigapasseBE{" +
+                "wettkampfId=" + wettkampfId +
+                ", matchId=" + matchId +
+                ", passeId=" + passeId +
+                ", passeLfdnr=" + passeLfdnr +
+                ", passeMannschaftId=" + passeMannschaftId +
+                ", dsbMitgliedId=" + dsbMitgliedId +
+                ", dsbMitgliedName='" + dsbMitgliedName + '\'' +
+                ", mannschaftsmitgliedRueckennummer=" + mannschaftsmitgliedRueckennummer +
+                ", passeRingzahlPfeil1=" + passeRingzahlPfeil1 +
+                ", passeRingzahlPfeil2=" + passeRingzahlPfeil2 +
+                '}';
+    }
+
+
+    public Long getWettkampfId() {return wettkampfId;}
+    public void setWettkampfId(Long wettkampfId) {this.wettkampfId = wettkampfId;}
+
+    public Long getMatchId() {return matchId;}
+    public void setMatch_id(Long matchId) {this.matchId = matchId;}
+
+    public Long getPasseId() {return passeId;}
+    public void setPasseId(Long passeId) {this.passeId = passeId;}
+
+    public int getPasseLfdnr() {return passeLfdnr;}
+    public void setPasseLfdnr(int passeLfdnr) {this.passeLfdnr = passeLfdnr;}
+
+    public Long getPasseMannschaftId() {return passeMannschaftId;}
+    public void setPasseMannschaftId(Long passeMannschaftId) {this.passeMannschaftId = passeMannschaftId;}
+
+    public Long getDsbMitgliedId() {return dsbMitgliedId;}
+    public void setDsbMitgliedId(Long dsbMitgliedId) {this.dsbMitgliedId = dsbMitgliedId;}
+
+    public String getDsbMitgliedName() {return dsbMitgliedName;}
+    public void setDsbMitgliedName(String dsbMitgliedName) {this.dsbMitgliedName = dsbMitgliedName;}
+
+    public int getMannschaftsmitgliedRueckennummer() {return mannschaftsmitgliedRueckennummer;}
+    public void setMannschaftsmitgliedRueckennummer(int mannschaftsmitgliedRueckennummer) {this.mannschaftsmitgliedRueckennummer = mannschaftsmitgliedRueckennummer;}
+
+    public int getPasseRingzahlPfeil1() {return passeRingzahlPfeil1;}
+    public void setPasseRingzahlPfeil1(int passeRingzahlPfeil1) {this.passeRingzahlPfeil1 = passeRingzahlPfeil1;}
+
+    public int getPasseRingzahlPfeil2() {return passeRingzahlPfeil2;}
+    public void setPasseRingzahlPfeil2(int passeRingzahlPfeil2) {this.passeRingzahlPfeil2 = passeRingzahlPfeil2;}
 }
