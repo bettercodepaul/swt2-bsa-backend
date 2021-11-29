a5b7b4872e62140e028402700ac0a980bde7d582
groteskcici <c.luzzi@web.de>
2021-11-29 09:25:44 +0100

    Finished the Selects and did some Code Qualityimprovement.

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligapasse/impl/dao/LigapasseDAO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/ligapasse/impl/dao/LigapasseDAO.java
@@ -1,15 +1,17 @@
 package de.bogenliga.application.business.ligapasse.impl.dao;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import de.bogenliga.application.business.ligatabelle.impl.entity.LigatabelleBE;
+import de.bogenliga.application.business.ligapasse.impl.entity.LigapasseBE;
 import de.bogenliga.application.common.component.dao.BasicDAO;
 import de.bogenliga.application.common.component.dao.BusinessEntityConfiguration;
 import de.bogenliga.application.common.component.dao.DataAccessObject;
+import de.bogenliga.application.common.database.queries.QueryBuilder;
 
 /**
  * TODO [AL] class documentation
@@ -26,16 +28,16 @@ public class LigapasseDAO implements DataAccessObject {
 
 
     //business entity parameters
-    private static final String MATCH_BE_WETTKAMPF_ID = "wettkampf_id";
-    private static final String MATCH_BE_MATCH_ID = "match_id";
-    private static final String MATCH_BE_PASSE_ID = "passe_id";
-    private static final String MATCH_BE_PASSE_LFDNR = "passe_lfdnr";
-    private static final String MATCH_BE_PASSE_MANSCHAFT_ID = "passe_manschaft_id";
-    private static final String MATCH_BE_DSB_MITGLIED_ID = "dsb_mitglied_id";
-    private static final String MATCH_BE_DSB_MITGLIED_NAME = "dsb_mitglied_name";
-    private static final String MATCH_BE_MANNSCHAFTSMITGLIED_RUECKENNUMMER = "mannschaftsmitglied_rueckennummer";
-    private static final String MATCH_BE_PASSE_RINGZAHL_PFEIL1 = "passe_ringzahl_pfeil1";
-    private static final String MATCH_BE_PASSE_RINGZAHL_PFEIL2 = "passe_ringzahl_pfeil2";
+    private static final String MATCH_BE_WETTKAMPF_ID = "wettkampfId";
+    private static final String MATCH_BE_MATCH_ID = "matchId";
+    private static final String MATCH_BE_PASSE_ID = "passeId";
+    private static final String MATCH_BE_PASSE_LFDNR = "passeLfdnr";
+    private static final String MATCH_BE_PASSE_MANNSCHAFT_ID = "passeMannschaftId";
+    private static final String MATCH_BE_DSB_MITGLIED_ID = "dsbMitgliedId";
+    private static final String MATCH_BE_DSB_MITGLIED_NAME = "dsbMitgliedName";
+    private static final String MATCH_BE_MANNSCHAFTSMITGLIED_RUECKENNUMMER = "mannschaftsmitglied_Rueckennummer";
+    private static final String MATCH_BE_PASSE_RINGZAHL_PFEIL1 = "passeRingzahlPfeil1";
+    private static final String MATCH_BE_PASSE_RINGZAHL_PFEIL2 = "passeRingzahlPfeil2";
 
 
     // table columns
@@ -43,7 +45,7 @@ public class LigapasseDAO implements DataAccessObject {
     private static final String MATCH_TABLE_MATCH_ID = "ligapasse_match_id";
     private static final String MATCH_TABLE_PASSE_ID = "ligapasse_passe_id";
     private static final String MATCH_TABLE_PASSE_LFDNR = "ligapasse_passe_lfdnr";
-    private static final String MATCH_TABLE_PASSE_MANSCHAFT_ID = "ligapasse_passe_manschaft_id";
+    private static final String MATCH_TABLE_PASSE_MANNSCHAFT_ID = "ligapasse_passe_mannschaft_id";
     private static final String MATCH_TABLE_DSB_MITGLIED_ID = "ligapasse_dsb_mitglied_id";
     private static final String MATCH_TABLE_DSB_MITGLIED_NAME = "ligapasse_dsb_mitglied_name";
     private static final String MATCH_TABLE_MANNSCHAFTSMITGLIED_RUECKENNUMMER = "ligapasse_mannschaftsmitglied_rueckennummer";
@@ -52,10 +54,10 @@ public class LigapasseDAO implements DataAccessObject {
 
 
     // wrap all specific config parameters
-    private static final BusinessEntityConfiguration<LigatabelleBE> LIGAPASSES = new BusinessEntityConfiguration<>(
-            LigatabelleBE.class, TABLE, getColumnsToFieldsMap(), LOGGER);
+    private static final BusinessEntityConfiguration<LigapasseBE> LIGAPASSE = new BusinessEntityConfiguration<>(
+            LigapasseBE.class, TABLE, getColumnsToFieldsMap(), LOGGER);
 
-    private final BasicDAO BasicDao;
+    private final BasicDAO basicDao;
 
     /**
      * Initialize the transaction manager to provide a database connection
@@ -63,7 +65,7 @@ public class LigapasseDAO implements DataAccessObject {
      * @param basicDao to handle the commonly used database operations
      */
     @Autowired
-    public LigapasseDAO(final BasicDAO basicDao){this.BasicDao = basicDao;}
+    public LigapasseDAO(final BasicDAO basicDao){this.basicDao = basicDao;}
 
 
     // table column label mapping to the business entity parameter names
@@ -75,7 +77,7 @@ public class LigapasseDAO implements DataAccessObject {
         columnsToFieldsMap.put(MATCH_TABLE_MATCH_ID, MATCH_BE_MATCH_ID);
         columnsToFieldsMap.put(MATCH_TABLE_PASSE_ID, MATCH_BE_PASSE_ID);
         columnsToFieldsMap.put(MATCH_TABLE_PASSE_LFDNR, MATCH_BE_PASSE_LFDNR);
-        columnsToFieldsMap.put(MATCH_TABLE_PASSE_MANSCHAFT_ID, MATCH_BE_PASSE_MANSCHAFT_ID);
+        columnsToFieldsMap.put(MATCH_TABLE_PASSE_MANNSCHAFT_ID, MATCH_BE_PASSE_MANNSCHAFT_ID);
         columnsToFieldsMap.put(MATCH_TABLE_DSB_MITGLIED_ID, MATCH_BE_DSB_MITGLIED_ID);
         columnsToFieldsMap.put(MATCH_TABLE_DSB_MITGLIED_NAME, MATCH_BE_DSB_MITGLIED_NAME);
         columnsToFieldsMap.put(MATCH_TABLE_MANNSCHAFTSMITGLIED_RUECKENNUMMER, MATCH_BE_MANNSCHAFTSMITGLIED_RUECKENNUMMER);
@@ -84,6 +86,20 @@ public class LigapasseDAO implements DataAccessObject {
 
         return columnsToFieldsMap;
     }
+
+    //SQL Selects
+    private static final String FIND_BY_WETTKAMPF_ID = new QueryBuilder()
+            .selectAll()
+            .from(TABLE)
+            .whereEquals(MATCH_TABLE_WETTKAMPF_ID)
+            .orderBy(MATCH_TABLE_WETTKAMPF_ID)
+            .compose().toString();
+
+
+    public List<LigapasseBE> getLigapasseWettkampf (final Long wettkampfId){
+        return basicDao.selectEntityList(LIGAPASSE, FIND_BY_WETTKAMPF_ID, wettkampfId);
+    }
+
 
 
 }
