f43311eb0f9ecbe2349adeb31e789099cd8c399d
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2021-12-31 11:55:37 +0100

    erste Datentypen für eine Sync Funktion und eine Offline Fähigkeit.

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/service/SyncService.java
@@ -0,0 +1,90 @@
+package de.bogenliga.application.services.v1.sync.service;
+
+import de.bogenliga.application.business.ligatabelle.api.LigatabelleComponent;
+import de.bogenliga.application.business.ligatabelle.api.types.LigatabelleDO;
+import de.bogenliga.application.services.v1.ligatabelle.model.LigatabelleDTO;
+
+import de.bogenliga.application.common.service.ServiceFacade;
+import de.bogenliga.application.common.validation.Preconditions;
+import de.bogenliga.application.services.v1.sync.mapper.LigaSyncLigatabelleDTOMapper;
+import de.bogenliga.application.services.v1.sync.model.LigaSyncLigatabelleDTO;
+import de.bogenliga.application.springconfiguration.security.permissions.RequiresPermission;
+import de.bogenliga.application.springconfiguration.security.types.UserPermission;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * I'm a REST resource and handle liga CRUD requests over the HTTP protocol
+ *
+ * We are implementing SyncServices independent to Online-Sevices as Services may be
+ * updated any time - but data structure in offline Client DB should not be affected
+ * Therefor we will seperate online DTOs drom Offline DTOs
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("v1/sync")
+public class SyncService implements ServiceFacade {
+    private static final String PRECONDITION_MSG_WETTKAMPF_ID = "Wettkampf Id must not be negative";
+
+    private final Logger logger = LoggerFactory.getLogger(de.bogenliga.application.services.v1.sync.service.SyncService.class);
+
+    private final LigatabelleComponent ligatabelleComponent;
+
+
+    /**
+     * Constructor with dependency injection
+     *
+     * @param ligatabelleComponent to handle the database access
+     */
+    @Autowired
+    public SyncService(final LigatabelleComponent ligatabelleComponent) {
+        this.ligatabelleComponent = ligatabelleComponent;
+    }
+
+
+    /**
+     * I return the current "ligatabelle" for a "wettkampftid (tag)" entries of the database.
+     *
+     * @return list of {@link LigatabelleDTO} as JSON
+     */
+    @GetMapping(
+            value = "ligatabelle/wettkampf={id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequiresPermission(UserPermission.CAN_READ_DEFAULT)
+    public List<LigaSyncLigatabelleDTO> getLigatabelleWettkampf(@PathVariable("id") final long id) {
+
+        Preconditions.checkArgument(id >= 0, PRECONDITION_MSG_WETTKAMPF_ID);
+        logger.debug("Receive 'Ligatabelle für Wettkampf' request with ID '{}'", id);
+
+        final List<LigatabelleDO> ligatabelleDOList = ligatabelleComponent.getLigatabelleWettkampf(id);
+
+        return ligatabelleDOList.stream().map(LigaSyncLigatabelleDTOMapper.toDTO).collect(Collectors.toList());
+    }
+    /* TODO
+     * I return the all Matches from "ligamatch"-Table for
+     * a "wettkampftid (tag)" entries of the database.
+     *
+     * @return list of {@link LigaSyncMatchDTO} as JSON
+     **/
+
+    /* TODO
+     * I return the all Passe Entries from "ligapasse"-Table for
+     * a "wettkampftid (tag)" entries of the database.
+     *
+     * @return list of {@link LigaSyncPasseDTO} as JSON
+     */
+
+    /* TODO
+     * I return the all Mannschaftsmitglieder Entries for all Mannschaften
+     * participating a "wettkampftid (tag)" entries of the database.
+     *
+     * @return list of {@link LigaSyncMannschaftsmitgliedDTO} as JSON
+     */
+
+}
