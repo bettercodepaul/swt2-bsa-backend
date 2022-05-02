7525aacb2aa1dcafdc49debe07031dbeed1c6ab7
Imke Gerlach <imke.gerlach@student.reutlingen-university.de>
2022-05-02 11:53:44 +0200

    tests4

--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/user/impl/mapper/UserRoleMapperTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/user/impl/mapper/UserRoleMapperTest.java
@@ -84,6 +84,8 @@ public class UserRoleMapperTest {
         assertThat(actual.getVersion()).isEqualTo(VERSION);
         assertThat(actual.getEmail()).isNull();
         assertThat(actual.getRoleName()).isNull();
+        assertThat(actual.getDsbMitgliedNachname()).isNull();
+        assertThat(actual.getDsbMitgliedVorname()).isNull();
     }
 
 
@@ -134,6 +136,8 @@ public class UserRoleMapperTest {
         assertThat(actual.getLastModifiedAtUtc()).isEqualTo(TIMESTAMP);
         assertThat(actual.getLastModifiedByUserId()).isEqualTo(OTHER_USER_ID);
         assertThat(actual.getVersion()).isEqualTo(VERSION);
+        assertThat(actual.getDsbMitgliedNachname()).isEqualTo(DSBMITGLIED_TABLE_SURNAME);
+        assertThat(actual.getDsbMitgliedVorname()).isEqualTo(DSBMITGLIED_TABLE_FORENAME);
     }
 
 }
