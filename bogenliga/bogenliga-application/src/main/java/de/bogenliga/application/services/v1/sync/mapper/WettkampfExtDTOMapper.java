e81948f3ac7a70b8e79560014c6c2caa05fa3a58
JoSglch <jonas.anmeldemail@gmail.com>
2022-05-29 15:59:21 +0200

    WettkampfExtDTOMapper stub

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/mapper/WettkampfExtDTOMapper.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/mapper/WettkampfExtDTOMapper.java
@@ -1,9 +1,21 @@
 package de.bogenliga.application.services.v1.sync.mapper;
 
+import java.util.function.Function;
+import de.bogenliga.application.business.wettkampf.api.types.WettkampfDO;
+import de.bogenliga.application.common.service.mapping.DataTransferObjectMapper;
+import de.bogenliga.application.services.v1.sync.model.WettkampfExtDTO;
+
 /**
  * I map the {@link WettkampfExtDTO} to the {@link WettkampfDO} object
  *
  * @author Jonas Sigloch, SWT SoSe 2022
  */
-public class WettkampfExtDTOMapper {
+public class WettkampfExtDTOMapper implements DataTransferObjectMapper {
+
+    public static final Function<WettkampfDO, WettkampfExtDTO> toDTO = WettkampfExtDTOMapper::apply;
+
+    private static WettkampfExtDTO apply(WettkampfDO wettkampfDO) {
+
+        return new WettkampfExtDTO();
+    }
 }
