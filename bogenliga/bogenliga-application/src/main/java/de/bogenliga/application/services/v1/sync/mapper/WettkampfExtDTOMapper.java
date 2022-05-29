962480e58606868f138d5f5f56c1d67ec8102001
JoSglch <jonas.anmeldemail@gmail.com>
2022-05-29 15:35:50 +0200

    generateOfflineToken() in WettkampfComponentImpl implementiert

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/mapper/WettkampfExtDTOMapper.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.sync.mapper;
+
+/**
+ * I map the {@link WettkampfExtDTO} to the {@link WettkampfDO} object
+ *
+ * @author Jonas Sigloch, SWT SoSe 2022
+ */
+public class WettkampfExtDTOMapper {
+}
