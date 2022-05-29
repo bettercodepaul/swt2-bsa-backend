4b0b6988a235265634077b9ea26d1c8a84b59711
JoSglch <jonas.anmeldemail@gmail.com>
2022-05-29 16:10:38 +0200

    WettkampfExtDTOMapper except offlinetoken

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/mapper/WettkampfExtDTOMapper.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/sync/mapper/WettkampfExtDTOMapper.java
@@ -1,5 +1,6 @@
 package de.bogenliga.application.services.v1.sync.mapper;
 
+import java.sql.Date;
 import java.util.function.Function;
 import de.bogenliga.application.business.wettkampf.api.types.WettkampfDO;
 import de.bogenliga.application.common.service.mapping.DataTransferObjectMapper;
@@ -15,7 +16,22 @@ public class WettkampfExtDTOMapper implements DataTransferObjectMapper {
     public static final Function<WettkampfDO, WettkampfExtDTO> toDTO = WettkampfExtDTOMapper::apply;
 
     private static WettkampfExtDTO apply(WettkampfDO wettkampfDO) {
+        final Long id = wettkampfDO.getId();
+        final Long veranstaltungsId = wettkampfDO.getWettkampfVeranstaltungsId();
+        final Date datum = wettkampfDO.getWettkampfDatum();
+        final String strasse = wettkampfDO.getWettkampfStrasse();
+        final String plz = wettkampfDO.getWettkampfPlz();
+        final String ortsname = wettkampfDO.getWettkampfOrtsname();
+        final String ortsinfo = wettkampfDO.getWettkampfOrtsinfo();
+        final String beginn = wettkampfDO.getWettkampfBeginn();
+        final Long tag = wettkampfDO.getWettkampfTag();
+        final Long disziplinId = wettkampfDO.getWettkampfDisziplinId();
+        final Long wettkampfTypId = wettkampfDO.getWettkampfTypId();
+        final Long version = wettkampfDO.getVersion();
+        final Long ausrichter = wettkampfDO.getWettkampfAusrichter();
+        final String offlineToken = ""; // TODO
 
-        return new WettkampfExtDTO();
+        return new WettkampfExtDTO(id, veranstaltungsId, datum, strasse, plz, ortsname, ortsinfo, beginn, tag,
+                disziplinId, wettkampfTypId, version, ausrichter, offlineToken);
     }
 }
