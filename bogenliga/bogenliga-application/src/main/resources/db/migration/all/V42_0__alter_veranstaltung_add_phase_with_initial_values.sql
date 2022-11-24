56e9190578881d7c7f1870578f60f43f2bdb7c86
lschiweck <leschiweck@gmail.com>
2022-11-24 18:00:25 +0100

    Added SQL Script 42 to add veranstaltungs_phase to all Veranstaltungen with initial values

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
@@ -0,0 +1,21 @@
+alter table veranstaltung
+add veranstaltung_phase varchar(600);
+
+--Initial values
+update veranstaltung
+set veranstaltung_phase = 'Geplant'
+where veranstaltung_sportjahr >= (
+        extract(year from CURRENT_DATE))
+and veranstaltung_meldedeadline > CURRENT_DATE;
+
+update veranstaltung
+set veranstaltung_phase = 'Laufend'
+where veranstaltung_sportjahr >= (
+        extract(year from CURRENT_DATE))
+and veranstaltung_meldedeadline < CURRENT_DATE;
+
+update veranstaltung
+set veranstaltung_phase = 'Abgeschlossen'
+where veranstaltung_sportjahr < (
+        extract(year from CURRENT_DATE))
+and veranstaltung_meldedeadline < CURRENT_DATE;
