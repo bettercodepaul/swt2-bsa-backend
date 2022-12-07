38aabd334ee02356480442442e35712fa7ba286e
lschiweck <leschiweck@gmail.com>
2022-12-07 16:55:06 +0100

    Removed initial values logic as Gero wanted to let the Ligaleiter set the values

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
@@ -1,21 +1,8 @@
+-- Add new phase element with initial value Geplant
 alter table veranstaltung
-add veranstaltung_phase varchar(20);
+add veranstaltung_phase varchar(20)
+default 'Geplant';
 
---Initial values
-update veranstaltung
-set veranstaltung_phase = 'Geplant'
-where veranstaltung_sportjahr >= (
-        select CAST(configuration_value as int)from configuration where configuration_key = 'aktives-Sportjahr')
-and veranstaltung_meldedeadline > CURRENT_DATE;
 
-update veranstaltung
-set veranstaltung_phase = 'Laufend'
-where veranstaltung_sportjahr >= (
-        select CAST(configuration_value as int)from configuration where configuration_key = 'aktives-Sportjahr')
-and veranstaltung_meldedeadline < CURRENT_DATE;
 
-update veranstaltung
-set veranstaltung_phase = 'Abgeschlossen'
-where veranstaltung_sportjahr < (
-        select CAST(configuration_value as int)from configuration where configuration_key = 'aktives-Sportjahr')
-and veranstaltung_meldedeadline < CURRENT_DATE;
+
