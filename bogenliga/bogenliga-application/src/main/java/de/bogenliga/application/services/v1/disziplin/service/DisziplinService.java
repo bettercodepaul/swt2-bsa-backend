a0ce40511da9253cc0177edfe425107aeac7ae76
nkeilig <nico.keilig@student.reutlingen-university.de>
2022-12-14 15:36:01 +0100

    removed @CrossOrigin

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/disziplin/service/DisziplinService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/disziplin/service/DisziplinService.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +31,6 @@ import de.bogenliga.application.springconfiguration.security.types.UserPermissio
  * @author Nico Keilig
  */
 @RestController
-@CrossOrigin
 @RequestMapping("v1/disziplin")
 public class DisziplinService implements ServiceFacade {
 
