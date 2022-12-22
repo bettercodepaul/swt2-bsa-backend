7a0a8e1016c43f580b05557dce4622116801525e
MarioaAk <Mario.Akermann@icloud.com>
2022-12-22 09:43:15 +0100

    Update V42_0__alter_veranstaltung_add_phase_with_initial_values.sql

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
@@ -1,5 +1,5 @@
--- Add new phase element with initial value Geplant as enum
- CREATE TYPE phase AS ENUM ('Geplant', 'Laufend','Abgeschlossen');
+CREATE TYPE phase AS ENUM ('Geplant', 'Laufend','Abgeschlossen');
 
  alter table veranstaltung
- add column veranstaltung_phase phase
+ add veranstaltung_phase phase
+ default 'Geplant';
