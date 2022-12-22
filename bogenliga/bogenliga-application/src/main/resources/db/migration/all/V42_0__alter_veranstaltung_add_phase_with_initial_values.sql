86da3eebcafa9390be579298b172b640dd629100
MarioaAk <Mario.Akermann@icloud.com>
2022-12-22 10:05:22 +0100

    Update V42_0__alter_veranstaltung_add_phase_with_initial_values.sql

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V42_0__alter_veranstaltung_add_phase_with_initial_values.sql
@@ -1,21 +1,4 @@
-alter table veranstaltung
- add veranstaltung_phase varchar(600);
-
- --Initial values
- update veranstaltung
- set veranstaltung_phase = 'Geplant'
- where veranstaltung_sportjahr >= (
-         extract(year from CURRENT_DATE))
- and veranstaltung_meldedeadline > CURRENT_DATE;
-
- update veranstaltung
- set veranstaltung_phase = 'Laufend'
- where veranstaltung_sportjahr >= (
-         extract(year from CURRENT_DATE))
- and veranstaltung_meldedeadline < CURRENT_DATE;
-
- update veranstaltung
- set veranstaltung_phase = 'Abgeschlossen'
- where veranstaltung_sportjahr < (
-         extract(year from CURRENT_DATE))
- and veranstaltung_meldedeadline < CURRENT_DATE;
+-- Add new phase element with initial value Geplant
+ alter table veranstaltung
+ add column veranstaltung_phase varchar(20)
+ default 'Geplant';
