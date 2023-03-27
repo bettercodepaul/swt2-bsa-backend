592817135e30eccbc6a28af4bf3856680098e1bd
DaumuellerL <Lennart.daumueller@student.Reutlingen-university.de>
2023-03-27 23:23:51 +0200

    Fix(backend): Replaced string concatenation with String.format specifier.

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/service/DsbMannschaftService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/service/DsbMannschaftService.java
@@ -277,7 +277,7 @@ public class DsbMannschaftService implements ServiceFacade {
         DsbMannschaftDO neueMannschaft = dsbMannschaftComponent.create(dsbMannschaftDO, mannschaftId);
         dsbMannschaftComponent.copyMitgliederFromMannschaft(mannschaftId, neueMannschaft.getId());
 
-        LOG.debug("Mannschaft " + dsbMannschaftDO.getName() + "in Veranstaltung mit id " + veranstaltungsId + " kopiert.");
+        LOG.debug(String.format("Manschaft %s in Veranstaltung mit id %s kopiert.", dsbMannschaftDO.getName(), veranstaltungsId));
     }
 
     /**
