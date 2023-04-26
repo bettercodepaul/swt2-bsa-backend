5da68a29462e29264b906cc604ebdbbbdf14e58d
TimZue <tim.zuehlke@student.reutlingen-university.de>
2023-04-26 15:12:44 +0200

    Fixed the SQL Script, that adds the maximum size to the veranstaltungs table.

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V49_0__insert_maximum_size_column_to_veranstalung.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V49_0__insert_maximum_size_column_to_veranstalung.sql
@@ -1,6 +1,6 @@
 ALTER TABLE veranstaltung
-DROP COLUMN IF EXISTS veranstaltung_groesse;
+    DROP COLUMN IF EXISTS veranstaltung_groesse;
 
 ALTER TABLE veranstaltung
-ADD COLUMN veranstaltung_groesse ENUM(4, 6, 8)
-default 8;
+    ADD COLUMN veranstaltung_groesse INT DEFAULT 8
+        CHECK ( veranstaltung_groesse = 4 OR veranstaltung_groesse = 6 OR veranstaltung_groesse = 8 );
