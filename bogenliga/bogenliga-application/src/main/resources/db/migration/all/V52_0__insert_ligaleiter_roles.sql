f55970477a16dad79879df4e07976e438c910d6e
simonjell <simon.jell02@gmail.com>
2023-06-06 11:56:45 +0200

    Rechte in DB eingetragen

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V52_0__insert_ligaleiter_roles.sql
@@ -0,0 +1,23 @@
+INSERT INTO rolle_recht(
+rolle_recht_rolle_id,
+rolle_recht_recht_id
+)
+VALUES (
+2, 10
+);
+
+INSERT INTO rolle_recht(
+rolle_recht_rolle_id,
+rolle_recht_recht_id
+)
+VALUES (
+2, 18
+);
+
+INSERT INTO rolle_recht(
+rolle_recht_rolle_id,
+rolle_recht_recht_id
+)
+VALUES (
+2, 2
+);
