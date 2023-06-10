a1181689f28cc14296ca690a7107ad1770fd5eb1
simonjell <simon.jell02@gmail.com>
2023-06-10 14:44:45 +0200

    delete ligaleiter_recht

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V57_0_delete_ligaleiter_roles.sql
@@ -0,0 +1,11 @@
+delete from rolle_recht(
+where rolle_recht_rolle_id=2 and rolle_recht_recht_id=18
+);
+
+delete from rolle_recht(
+where rolle_recht_rolle_id=2 and rolle_recht_recht_id=2
+);
+
+
+
+
