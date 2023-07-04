19f9b480f558043d85798f4f8c451096d7be51a3
godfred amoah sefa <godfredasefa@icloud.com>
2023-07-04 19:06:08 +0200

    script 62 für einfügen eines Test nutzer im Ordner DEV script 63 für einfügen eines Test nutzer im Ordner LOCAL script 64 für das löschen das zuvor angelegten Test Nutzer in ALL

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V64_0__delet_hsrt_test_user-4.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V64_0__delet_hsrt_test_user-4.sql
@@ -1,6 +1,11 @@
+DELETE FROM benutzer
+WHERE benutzer_id = 13
+AND EXISTS (
+  SELECT 1 FROM benutzer WHERE benutzer_id = 13
+);
 
--- Assuming the primary key for the 'benutzer' table is 'benutzer_id'
-DELETE FROM benutzer WHERE benutzer_id = 13;
-
--- Assuming the primary key for the 'benutzer_rolle' table is 'benutzer_rolle_benutzer_id' and 'benutzer_rolle_rolle_id'
-DELETE FROM benutzer_rolle WHERE benutzer_rolle_benutzer_id = 13 AND benutzer_rolle_rolle_id = 2;
+DELETE FROM benutzer_rolle
+WHERE benutzer_rolle_benutzer_id = 13 AND benutzer_rolle_rolle_id = 2
+AND EXISTS (
+  SELECT 1 FROM benutzer_rolle WHERE benutzer_rolle_benutzer_id = 13 AND benutzer_rolle_rolle_id = 2
+);
