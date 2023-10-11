a24d258b47ccce0f6824fa87649f747eb786b44c
Thomas Hesse <thomas.hesse@student.reutlingen-university.de>
2023-10-11 13:32:49 +0200

    Remove unused import

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/kampfrichter/api/KampfrichterComponent.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/kampfrichter/api/KampfrichterComponent.java
@@ -1,7 +1,6 @@
 package de.bogenliga.application.business.kampfrichter.api;
 
 import java.util.List;
-import de.bogenliga.application.business.dsbmitglied.api.types.DsbMitgliedDO;
 import de.bogenliga.application.business.kampfrichter.api.types.KampfrichterDO;
 import de.bogenliga.application.common.component.ComponentFacade;
 
