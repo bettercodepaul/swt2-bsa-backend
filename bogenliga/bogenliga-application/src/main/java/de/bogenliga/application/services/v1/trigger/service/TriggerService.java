999d6b2417486b028fb5a16ac1d17286c6fa821b
Magdalena Meier <meier_magdalena@t-online.de>
2023-10-30 17:36:00 +0100

    Basic Trigger Endpunkt

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/service/TriggerService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/service/TriggerService.java
@@ -1,9 +1,20 @@
 package de.bogenliga.application.services.v1.trigger.service;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import de.bogenliga.application.common.service.ServiceFacade;
+
 /**
  * TODO [AL] class documentation
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
-public class TriggerService {
+
+@RestController
+@CrossOrigin
+@RequestMapping("v1/trigger")
+
+public class TriggerService implements ServiceFacade {
+
 }
