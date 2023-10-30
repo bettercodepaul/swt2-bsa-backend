cd6e15d1d92e13f4faf407479cf330c9a9bba422
Magdalena Meier <meier_magdalena@t-online.de>
2023-10-30 09:59:33 +0100

    Initial commit

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/service/TriggerService.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.trigger.service;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class TriggerService {
+}
