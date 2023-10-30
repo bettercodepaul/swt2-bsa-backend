6e5bc0aeb126d03b581eaec46e7fa819be982e05
Magdalena Meier <meier_magdalena@t-online.de>
2023-10-30 16:44:55 +0100

    Ordnerstruktur Trigger

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/model/TriggerDTO.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.trigger.model;
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class TriggerDTO {
+}
