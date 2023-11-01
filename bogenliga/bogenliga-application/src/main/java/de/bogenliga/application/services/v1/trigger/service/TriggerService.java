29d4481ac957404169202336ca3e2e25b3590ad3
Magdalena Meier <meier_magdalena@t-online.de>
2023-11-01 14:27:54 +0100

    Timestamp für letzten Trigger

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/service/TriggerService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/trigger/service/TriggerService.java
@@ -1,24 +1,23 @@
 package de.bogenliga.application.services.v1.trigger.service;
 
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import de.bogenliga.application.common.service.ServiceFacade;
 
-/**
- * TODO [AL] class documentation
- *
- * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
- */
-
 @RestController
 @CrossOrigin
 @RequestMapping("v1/trigger")
 
 public class TriggerService implements ServiceFacade {
+
+    private LocalDateTime lastTrigger = null;
+
     @GetMapping("/ping")
     public String ping() {
-        return "pong";
+        lastTrigger = LocalDateTime.now();
+        return lastTrigger.toString();
     }
 }
