b57c9613e18270100efd96940b9c60d86d8cd6e0
Lino Cortese <lino.cortese@student.reutlingen-university.de>
2023-11-15 11:38:20 +0100

    Removed non-functional prior test and improved previous

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/service/TriggerServiceTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/service/TriggerServiceTest.java
@@ -1,19 +1,16 @@
 package de.bogenliga.application.services.v1.trigger.service;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDateTime;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import de.bogenliga.application.services.v1.trigger.service.TriggerService;
 import static org.assertj.core.api.Assertions.assertThat;
-import static java.time.temporal.ChronoUnit.MINUTES;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 
 /**
@@ -23,46 +20,38 @@ import static org.mockito.Mockito.*;
  */
 public class TriggerServiceTest {
 
-    private LocalDateTime currentTime = LocalDateTime.now();
+	private LocalDateTime currentTime = LocalDateTime.now();
 
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Mock
-    private TriggerService triggerService;
+	@Mock
+	private TriggerService triggerService;
 
-    @InjectMocks
-    private TriggerService triggerTest;
-
-
+	@InjectMocks
+	private TriggerService triggerTest;
 
 
 
-    @Test
-    public void getPong(){
-        // Actual result(s) TBD \\
-        assertThat(triggerTest.ping()).isEqualTo("pong");
-
-    }
 
 
-    @Test
-    public void validateSyncDataCall() throws NoSuchFieldException, IllegalAccessException {
-        Field syncTimestampField = TriggerService.class.getDeclaredField("syncTimestamp"); //Get field 'syncTimestamp'
-        syncTimestampField.setAccessible(true); // Enables Access
-        syncTimestampField.set(triggerService,currentTime); // Sets syncTimestamp to current time
+	@Test
+	public void getPong(){
+		// Actual result(s) TBD \\
+		assertThat(triggerTest.ping()).isEqualTo("pong");
+
+	}
 
 
-        // Evil past-midnight coding snippets
-        Class[] scheduleFields = TriggerService.class.getDeclaredClasses(); // Put classes of trigger service in array
-        for(Class triggerSchedule: scheduleFields){ // Find inner class 'triggerSchedule'
-            Field[] amountField = triggerSchedule.getDeclaredFields(); // Get fields of inner class 'triggerSchedule'
-            for(Field amount: amountField){ // Find field named amount
-                amount.setAccessible(true); // Enables access
-                amount.set(triggerTest,MINUTES.addTo(currentTime, 10)); // Sets currentTime += 10 minutes
-                verify(triggerTest, times(1)).syncData(); // Checks if syncData() gets executed
-            }
-        }
+	@Test
+	public void validateSyncDataCall() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
 
-    }
+		Field syncTimestampField = triggerTest.getClass().getDeclaredField("syncTimestamp");
+		assertEquals(LocalDateTime.class, syncTimestampField.getType());
+
+		syncTimestampField.setAccessible(true); // Enables Access
+		assertNull(syncTimestampField.get(triggerTest)); // Tests if syncTimestamp has it's default value of null
+		syncTimestampField.set(triggerTest, currentTime); // Sets syncTimestamp to current time
+		assertNotNull(syncTimestampField.get(triggerTest)); // Tests if syncTimestamp has been set
+	}
 }
