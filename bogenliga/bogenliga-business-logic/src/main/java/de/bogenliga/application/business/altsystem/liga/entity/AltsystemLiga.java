ca4931294b82fa9d5a2fb04d503df811de451d39
Thomas Hesse <thomas.hesse@student.reutlingen-university.de>
2023-12-06 19:56:20 +0100

    Altsystem Liga Klassen

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/liga/entity/AltsystemLiga.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/liga/entity/AltsystemLiga.java
@@ -2,7 +2,8 @@ package de.bogenliga.application.business.altsystem.liga.entity;
 
 import de.bogenliga.application.business.altsystem.liga.dataobject.AltsystemLigaDO;
 import de.bogenliga.application.business.altsystem.liga.mapper.AltsystemLigaMapper;
-import de.bogenliga.application.business.liga.impl.entity.LigaBE;
+import de.bogenliga.application.business.liga.api.LigaComponent;
+import de.bogenliga.application.business.liga.api.types.LigaDO;
 import de.bogenliga.application.common.altsystem.AltsystemEntity;
 
 /**
@@ -12,29 +13,38 @@ import de.bogenliga.application.common.altsystem.AltsystemEntity;
  */
 public class AltsystemLiga implements AltsystemEntity<AltsystemLigaDO> {
 
-    public void create(AltsystemLigaDO altsystemDataObject){
-        // Check Preconditions
+    private final LigaComponent ligaComponent;
 
+    public AltsystemLiga(final LigaComponent ligaComponent) {
+        this.ligaComponent = ligaComponent;
+    }
+
+    @Override
+    public void create(AltsystemLigaDO altsystemDataObject){
         // Map data to new object, add default fields
-        LigaBE ligaBE = AltsystemLigaMapper.toBE(altsystemDataObject);
-        AltsystemLigaMapper.addDefaultFields(ligaBE);
+        LigaDO ligaDO = new LigaDO();
+        ligaDO = AltsystemLigaMapper.toDO(ligaDO, altsystemDataObject);
+        ligaDO = AltsystemLigaMapper.addDefaultFields(ligaDO);
 
         // Add data to table
+        // ligaComponent.create(ligaDO, <UserID>);
 
         // Add to translation table
 
     }
 
+    @Override
     public void update(AltsystemLigaDO altsystemDataObject){
-        // Check Preconditions
-
-        // Map data to new object, don't add default fields
-        AltsystemLigaMapper.toBE(altsystemDataObject);
-
         // Get primary key from translation table
 
-        // Update data in table with given primary key
+        // Find data in table with corresponding id
+        LigaDO ligaDO = ligaComponent.findById(1);
 
+        // Map data to new object, don't add default fields
+        AltsystemLigaMapper.toDO(ligaDO, altsystemDataObject);
+
+        // Update data in table with given primary key
+        // ligaComponent.update(ligaDO, <UserID>);
     }
 
 }
