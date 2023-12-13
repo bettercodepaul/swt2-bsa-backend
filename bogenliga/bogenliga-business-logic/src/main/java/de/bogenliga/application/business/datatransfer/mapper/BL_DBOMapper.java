ed3a1406b2cc7a32f7d02878ebbfbe6aaa696fc8
erayosso <erayduezenli@googlemail.com>
2023-12-13 19:37:44 +0100

    feature/#1496 while schleife muss überarbeitet werden.

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/datatransfer/mapper/BL_DBOMapper.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/datatransfer/mapper/BL_DBOMapper.java
@@ -9,6 +9,9 @@ import de.bogenliga.application.business.datatransfer.TableType;
 import de.bogenliga.application.business.datatransfer.model.MannschaftDBO;
 import de.bogenliga.application.business.datatransfer.model.SchuetzeDBO;
 import de.bogenliga.application.business.datatransfer.model.VereinDBO;
+import java.util.HashSet;
+import java.util.Set;
+import java.sql.Timestamp;
 
 /**
  * TODO [AL] class documentation
@@ -31,6 +34,9 @@ public class BL_DBOMapper {
                 break;
             case MANNSCHAFT:
                 query = "SELECT * FROM bl_mannschaft";
+                break;
+            case VEREIN:
+                query = "SELECT * FROM verein";
                 break;
             default:
                 break;
@@ -92,10 +98,113 @@ public class BL_DBOMapper {
     }
 
 
-    public MannschaftDBO mapMannschaft() {
+    public MannschaftDBO mapMannschaft() throws SQLException {
         MannschaftDBO mannschaftDBO = new MannschaftDBO();
-        SchuetzeDBO schuetze_new = new SchuetzeDBO();
+        ResultSet mannschaft_oldData = getData(TableType.MANNSCHAFT);
+        ResultSet verein = getData(TableType.VEREIN);
 
+        int sort = 0;
+
+        while (mannschaft_oldData.next()) {
+            char[] nameOld = mannschaft_oldData.getString(4).toCharArray();
+            String nameNew = "";
+            String nameNewWithoutNumber = "";
+            String sonderzeichen = "/-.";
+            String num = "1234567890";
+            int data = 0;
+
+            String currentName = new String(nameOld);
+            if (currentName.equals("fehlender Verein") || currentName.equals("<leer>") || currentName.equals(null)) {
+                continue; // Überspringe den Eintrag, wenn der Name "fehlender Verein" oder "<leer>" ist
+            }
+
+            // Anpassen des Strings, sodass dieser genauso aussieht wie bei mapVerein, aber die Zahl am Ende beibehalten wird
+            for (int i = 0; i < nameOld.length; i++) {
+                // Speichere das aktuelle Zeichen am Index, um getString().charAt()-Aufrufe zu minimieren (für bessere Speichereffizienz)
+                if (i + 1 < mannschaft_oldData.getString(4).length()) {
+                    // Wenn ein Punkt ohne nachfolgendem Leerzeichen kommt, füge ein Leerzeichen hinzu
+                    if (mannschaft_oldData.getString(4).charAt(i) == '.' && mannschaft_oldData.getString(4).charAt(
+                            i + 1) != ' ') {
+                        nameNew += ". ";
+                    } else if (sonderzeichen.contains(mannschaft_oldData.getString(4).charAt(i) + "") &&
+                            !num.contains(mannschaft_oldData.getString(4).charAt(i - 1) + "")) {
+                        // Wenn das Zeichen ein Sonderzeichen ist und nicht von einer Zahl gefolgt wird, füge ein Leerzeichen hinzu
+                        nameNew += " ";
+                    } else if (num.contains(mannschaft_oldData.getString(4).charAt(i) + "") && i > 7) {
+                        // Wenn das Zeichen eine Zahl ist und der Index größer als 7 ist, füge nichts hinzu (überspringe Zahl in der Mitte des Namens)
+                        nameNew += "";
+                    } else {
+                        // Andernfalls füge das Zeichen zum neuen Namen hinzu
+                        nameNew += mannschaft_oldData.getString(4).charAt(i);
+                    }
+                } else {
+                    // Für das letzte Zeichen
+                    if (mannschaft_oldData.getString(4).charAt(i) == '.') {
+                        nameNew += ".";
+                    } else if (sonderzeichen.contains(mannschaft_oldData.getString(4).charAt(i) + "") &&
+                            !num.contains(mannschaft_oldData.getString(4).charAt(i - 1) + "")) {
+                        // Wenn das Zeichen ein Sonderzeichen ist und nicht von einer Zahl gefolgt wird, füge nichts hinzu
+                        nameNew += "";
+                    } else {
+                        // Andernfalls füge das Zeichen zum neuen Namen hinzu
+                        nameNew += mannschaft_oldData.getString(4).charAt(i);
+                    }
+                }
+            }
+
+            // Überprüfen, ob die letzte Zeichenfolge eine Zahl ist
+            if (Character.isDigit(nameNew.charAt(nameNew.length() - 1))) {
+                // Wenn ja, setze data auf die gefundene Nummer
+                StringBuilder numberStringBuilder = new StringBuilder();
+                for (int j = nameNew.length() - 1; j >= 0 && Character.isDigit(nameNew.charAt(j)); j--) {
+                    numberStringBuilder.insert(0, nameOld[j]);
+                }
+                data = Integer.parseInt(numberStringBuilder.toString());
+
+                // Setze nameNewWithoutNumber auf den Namen ohne die Zahl am Ende
+                nameNewWithoutNumber = nameNew.substring(0, nameNew.length() - numberStringBuilder.length());
+            } else {
+                // Wenn nicht, setze data auf 1 und nameNewWithoutNumber auf den gesamten Namen
+                data = 1;
+                nameNewWithoutNumber = nameNew;
+            }
+
+            if (verein != null) {
+                try {
+                    while (verein.next()) {
+                        String name = verein.getString("verein_name");
+                        if (nameNewWithoutNumber.equals(name)) {
+                            int Id = verein.getInt("verein_id");
+                            mannschaftDBO.setMannschaftVereinId(Id);
+                            break;
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    verein.close();
+                }
+
+
+                // For the timestamp
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                mannschaftDBO.setMannschaftId(sort);
+                mannschaftDBO.setMannschaftNummer(data);
+                mannschaftDBO.setMannschaftBenutzerId(1000);
+                //@TODO
+                //mannschaftDBO.setMannschaftVeranstaltungId(1);
+                mannschaftDBO.setCreatedAtUtc(timestamp);
+                mannschaftDBO.setCreatedBy(1);
+                mannschaftDBO.setLastModifiedAtUtc(timestamp);
+                mannschaftDBO.setLastModifiedBy(0);
+                mannschaftDBO.setVersion(1);
+                mannschaftDBO.setMannschaftSortierung(sort);
+
+                // Für Sortierung mannschaftDBO;
+                sort += 1;
+            }
+
+        }
         return mannschaftDBO;
     }
 
