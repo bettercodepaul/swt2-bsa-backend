8f3ad6755a7f428cee4ae33b0306568680c91918
Thomas Hesse <thomas.hesse@student.reutlingen-university.de>
2023-12-14 20:26:20 +0100

    Altsystem Liga Mapper

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/liga/mapper/AltsystemLigaMapper.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/liga/mapper/AltsystemLigaMapper.java
@@ -2,7 +2,6 @@ package de.bogenliga.application.business.altsystem.liga.mapper;
 
 import de.bogenliga.application.business.altsystem.liga.dataobject.AltsystemLigaDO;
 import de.bogenliga.application.business.liga.api.types.LigaDO;
-import de.bogenliga.application.business.liga.impl.entity.LigaBE;
 import de.bogenliga.application.common.component.mapping.ValueObjectMapper;
 
 /**
@@ -13,15 +12,33 @@ import de.bogenliga.application.common.component.mapping.ValueObjectMapper;
  */
 public class AltsystemLigaMapper implements ValueObjectMapper {
     public static LigaDO toDO(LigaDO ligaDO, AltsystemLigaDO altsystemLigaDO){
-        ligaDO.setName(altsystemLigaDO.getName());
-        ligaDO.setLigaUebergeordnetId(200L);
+        String ligaName = altsystemLigaDO.getName();
+        long ligaUebergeordnetId = altsystemLigaDO.getIdNextLiga();
+        long disziplinId;
+
+
+        ligaDO.setName(ligaName);
+
+        // Id muss noch übersetzt werden
+        ligaDO.setLigaUebergeordnetId(ligaUebergeordnetId);
+
+        // Checken ob es eine Recurve oder Compound Liga ist
+        if(ligaName.toLowerCase().contains("comp")){
+            disziplinId = 1;
+        }
+        else{
+            disziplinId = 0;
+        }
+        ligaDO.setDisziplinId(disziplinId);
+
         return ligaDO;
     }
 
     public static LigaDO addDefaultFields(LigaDO ligaDO) {
+        // ID vom Admin User herausfinden
         ligaDO.setLigaVerantwortlichId(1L);
+        // ID von DSB Region herausfinden
         ligaDO.setRegionId(1000L);
-        ligaDO.setDisziplinId(0L);
         return ligaDO;
     }
 
