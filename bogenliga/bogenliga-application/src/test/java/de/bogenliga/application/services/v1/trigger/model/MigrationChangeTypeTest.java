3e1dea42148444b797b99e18ca0354d3705e0606
Lino Cortese <lino.cortese@student.reutlingen-university.de>
2023-12-14 20:14:24 +0100

    Added MigrationChangeTypeTest

---
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/model/MigrationChangeTypeTest.java
@@ -0,0 +1,20 @@
+package de.bogenliga.application.services.v1.trigger.model;
+
+import org.junit.Test;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Tests the MigrationChangeType enum
+ *
+ */
+public class MigrationChangeTypeTest {
+
+	private MigrationChangeType migrationChangeType;
+
+	@Test
+	public void testType(){
+		assertThat(migrationChangeType.CREATE, notNullValue());
+		assertThat(migrationChangeType.UPDATE, notNullValue());
+	}
+}
