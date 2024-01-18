82a59f2758499603396c4384e3f0c70ab6f97b30
jan-404 <jan.schnaidt@student.reutlingen-university.de>
2024-01-18 21:01:23 +0100

    Refactoring

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/Uebersetzung/Kategorien.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/Uebersetzung/Kategorien.java
@@ -6,9 +6,14 @@ package de.bogenliga.application.business.altsystem.Uebersetzung;
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
 public enum Kategorien {
-    Mannschaft_Verein,
-    Mannschaft_Mannschaft,
-    Schütze_Verein,
-    Mannschaft_Veranstaltung,
+    Mannschaft_Verein("Mannschaft_Verein"),
+    Mannschaft_Mannschaft("Mannschaft_Mannschaft"),
+    Schütze_Verein("Schütze_Verein"),
+    Mannschaft_Veranstaltung("Mannschaft_Veranstaltung");
 
+
+    String label;
+    Kategorien(String label) {
+        this.label = label;
+    }
 }
