6bf48ecd4f58d6f475bab33bc51364b4c67a64fd
jan-404 <jan.schnaidt@student.reutlingen-university.de>
2024-01-23 20:16:45 +0100

    Anpassungen in semtlichen Classen.

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/mannschaft/mapper/AltsystemMannschaftMapper.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/mannschaft/mapper/AltsystemMannschaftMapper.java
@@ -1,9 +1,16 @@
 package de.bogenliga.application.business.altsystem.mannschaft.mapper;
 
 import org.springframework.stereotype.Component;
+import de.bogenliga.application.business.altsystem.liga.mapper.AltsystemLigaMapper;
 import de.bogenliga.application.business.altsystem.mannschaft.dataobject.AltsystemMannschaftDO;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzung;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzungDO;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzungKategorie;
+import de.bogenliga.application.business.dsbmannschaft.api.DsbMannschaftComponent;
 import de.bogenliga.application.business.dsbmannschaft.api.types.DsbMannschaftDO;
 import de.bogenliga.application.common.component.mapping.ValueObjectMapper;
+import de.bogenliga.application.common.errorhandling.ErrorCode;
+import de.bogenliga.application.common.errorhandling.exception.BusinessException;
 
 /**
  * TODO [AL] class documentation
@@ -13,12 +20,75 @@ import de.bogenliga.application.common.component.mapping.ValueObjectMapper;
 @Component
 public class AltsystemMannschaftMapper implements ValueObjectMapper {
 
+    private final AltsystemUebersetzung altsystemUebersetzung;
+    private final AltsystemLigaMapper altsystemLigaMapper;
+    private final DsbMannschaftComponent dsbMannschaftComponent;
+
+
+
+    public AltsystemMannschaftMapper(AltsystemUebersetzung altsystemUebersetzung,
+                                     AltsystemLigaMapper altsystemLigaMapper,
+                                     DsbMannschaftComponent dsbMannschaftComponent) {
+        this.altsystemUebersetzung = altsystemUebersetzung;
+        this.altsystemLigaMapper = altsystemLigaMapper;
+        this.dsbMannschaftComponent = dsbMannschaftComponent;
+    }
+
+
     public static DsbMannschaftDO toDO(DsbMannschaftDO dsbMannschaftDO, AltsystemMannschaftDO altsystemMannschaftDO){
 
+        String currentName = altsystemMannschaftDO.getName().toString();
+        int nameLong = currentName.length() - 1;
+        char lastChar = altsystemMannschaftDO.getName().charAt(nameLong);
+        String lastCharAsString = String.valueOf(lastChar);
+        int mannNr;
+        String num = "1234567890";
+
+        //String currentName = new String(nameOld);
+        if (currentName.equals("fehlender Verein") || currentName.equals("<leer>") || currentName.equals(null)) {
+            return null;
+        }
+        if (num.contains(lastCharAsString)){
+            mannNr = Integer.parseInt(lastCharAsString);
+        } else {
+            mannNr = 1;
+        }
+
+        dsbMannschaftDO.setNummer(mannNr);
 
         return dsbMannschaftDO;
     }
 
+
+    public DsbMannschaftDO addDefaultFields (AltsystemMannschaftDO altsystemDataObject, DsbMannschaftDO dsbMannschaftDO, long currentDSBMitglied){
+
+        dsbMannschaftDO.setBenutzerId(currentDSBMitglied);
+
+        AltsystemUebersetzungDO vereinUebersetzung = altsystemUebersetzung.findByAltsystemID(
+                AltsystemUebersetzungKategorie.Mannschaft_Verein, altsystemDataObject.getId());
+
+        if(vereinUebersetzung == null){
+            throw new BusinessException(ErrorCode.ENTITY_NOT_FOUND_ERROR,
+                    String.format("No result found for ID '%s'", altsystemDataObject.getId()));
+        }
+
+        long vereinId = Long.parseLong(String.valueOf(vereinUebersetzung));
+        dsbMannschaftDO.setVereinId(vereinId);
+
+
+        AltsystemUebersetzungDO veranstaltungUebersetzung = altsystemUebersetzung.findByAltsystemID(
+                AltsystemUebersetzungKategorie.Mannschaft_Veranstaltung, altsystemDataObject.getId());
+
+        if(veranstaltungUebersetzung == null){
+            throw new BusinessException(ErrorCode.ENTITY_NOT_FOUND_ERROR,
+                    String.format("No result found for ID '%s'", altsystemDataObject.getId()));
+        }
+
+        long veranstaltungId = Long.parseLong(String.valueOf(veranstaltungUebersetzung));
+        dsbMannschaftDO.setVeranstaltungId(veranstaltungId);
+
+        return dsbMannschaftDO;
+    }
 
 
 
