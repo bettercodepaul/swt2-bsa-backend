0e6902b39d52a7209606afb3686152f766e22b48
Nikolay Nikolaev <nikolay.borislavov@gmail.com>
2024-01-24 16:19:15 +0100

    Call Sync Function from olddb Main

--- bogenliga/bogenliga-olddb-import/src/main/java/de/bogenliga/olddb/Main.java
+++ bogenliga/bogenliga-olddb-import/src/main/java/de/bogenliga/olddb/Main.java
@@ -28,11 +28,16 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     // Verbindungsinformationen
-    private static String host = "host";
+/*    private static String host = "My SQL Host hinzufügen";
     private static int port = 3306;
-    private static String dbName = "name";
-    private static String user = "user";
-    private static String password = "pw";
+    private static String dbName = "My SQL Name hinzufügen";
+    private static String user = "My SQL User hinzufügen";
+    private static String password = "My Password Host hinzufügen";*/
+    private static String host = "mysql2f19.netcup.net";
+    private static int port = 3306;
+    private static String dbName = "k74918_bogenliga";
+    private static String user = "k74918_bsapp_ro";
+    private static String password = "BsApp@100%";
 
     private static DatabaseConfiguration databaseConfiguration;
 
@@ -106,7 +111,7 @@ public class Main {
                 e.printStackTrace();
             }
             finally{
-                executeScript("temptable.sql", "jdbc:postgresql: url hinzufügen", "user hinzufügen","pw hinzufügen" );
+                executeScript("temptable.sql", "jdbc:postgresql://localhost:5432/swt2", "swt2","swt2" );
             }
         }
         catch (ClassNotFoundException e) {
