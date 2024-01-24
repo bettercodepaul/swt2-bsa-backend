8e5cc5710e1985db28f839f5892dec17dd8cfa2d
Lino Cortese <lino.cortese@student.reutlingen-university.de>
2024-01-24 15:42:30 +0100

    Updated TriggerDTOMapperTest for new values

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/mapper/TriggerDTOMapperTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/mapper/TriggerDTOMapperTest.java
@@ -1,6 +1,9 @@
 package de.bogenliga.application.services.v1.trigger.mapper;
 
+import java.time.OffsetDateTime;
 import org.junit.Test;
+import de.bogenliga.application.business.trigger.api.types.TriggerChangeOperation;
+import de.bogenliga.application.business.trigger.api.types.TriggerChangeStatus;
 import de.bogenliga.application.business.trigger.api.types.TriggerDO;
 import de.bogenliga.application.services.v1.trigger.model.TriggerDTO;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,22 +12,25 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Tests the TriggerDTOMapper class
  *
  */
-/*public class TriggerDTOMapperTest{
+public class TriggerDTOMapperTest{
 
 	//Test data
+	// id, kategorie, altsystemId, operation, status, nachricht, runAtUtc
 	private static final Long ID = 24L;
-	private static final Long VERSION = 3L;
-	private static final String TIMESTAMP = "12.12";
-	private static final String DESCRIPTION = "sampleData";
-	private static final String STATUS = "bad";
+	private static final String KATEGORIE = "needles";
+	private static final Long ALTSYSTEM_ID = 25L;
+	private static final TriggerChangeOperation OPERATION = null;
+	private static final TriggerChangeStatus STATUS = null;
+	private static final String NACHRICHT = "too bad!";
+	private static final OffsetDateTime RUN_AT_UTC = OffsetDateTime.MAX;
 
 
 	private TriggerDTO getDTO(){
-		return new TriggerDTO(ID, VERSION, TIMESTAMP, DESCRIPTION, STATUS);
+		return new TriggerDTO(ID, KATEGORIE, ALTSYSTEM_ID, OPERATION, STATUS, NACHRICHT, RUN_AT_UTC);
 	}
 
 	private TriggerDO getDO(){
-		return new TriggerDO(ID, VERSION, TIMESTAMP, DESCRIPTION, STATUS);
+		return new TriggerDO(ID, KATEGORIE, ALTSYSTEM_ID, OPERATION, STATUS, NACHRICHT, RUN_AT_UTC);
 	}
 
 
@@ -33,10 +39,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 		TriggerDTO triggerDTO = getDTO();
 		final TriggerDO actual = TriggerDTOMapper.toDO.apply(triggerDTO);
 		assertThat(actual.getId()).isEqualTo(ID);
-		assertThat(actual.getVersion()).isEqualTo(VERSION);
-		assertThat(actual.getTimestamp()).isEqualTo(TIMESTAMP);
-		assertThat(actual.getDescription()).isEqualTo(DESCRIPTION);
+		assertThat(actual.getKategorie()).isEqualTo(KATEGORIE);
+		assertThat(actual.getAltsystemId()).isEqualTo(ALTSYSTEM_ID);
+		assertThat(actual.getOperation()).isEqualTo(OPERATION);
 		assertThat(actual.getStatus()).isEqualTo(STATUS);
+		assertThat(actual.getNachricht()).isEqualTo(NACHRICHT);
+		assertThat(actual.getRunAtUtc()).isEqualTo(RUN_AT_UTC);
 	}
 
 	@Test
@@ -44,10 +52,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 		TriggerDO triggerDO = getDO();
 		final TriggerDTO actual = TriggerDTOMapper.toDTO.apply(triggerDO);
 		assertThat(actual.getId()).isEqualTo(ID);
-		assertThat(actual.getVersion()).isEqualTo(VERSION);
-		assertThat(actual.getTimestamp()).isEqualTo(TIMESTAMP);
-		assertThat(actual.getDescription()).isEqualTo(DESCRIPTION);
+		assertThat(actual.getKategorie()).isEqualTo(KATEGORIE);
+		assertThat(actual.getAltsystemId()).isEqualTo(ALTSYSTEM_ID);
+		assertThat(actual.getOperation()).isEqualTo(OPERATION);
 		assertThat(actual.getStatus()).isEqualTo(STATUS);
+		assertThat(actual.getNachricht()).isEqualTo(NACHRICHT);
+		assertThat(actual.getRunAtUtc()).isEqualTo(RUN_AT_UTC);
 	}
 
-}*/
+}
