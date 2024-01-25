28bf7126249336d0c5ff472f9ddc5c8dcc7e93ee
Lino Cortese <lino.cortese@student.reutlingen-university.de>
2024-01-25 10:23:16 +0100

    Moved OldDbImport out of its own package

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/service/TriggerServiceTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/service/TriggerServiceTest.java
@@ -1,48 +1,50 @@
-//package de.bogenliga.application.services.v1.trigger.service;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
-//import java.lang.reflect.Field;
-//import java.lang.reflect.InvocationTargetException;
-//import java.time.LocalDateTime;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.MockitoJUnit;
-//import org.mockito.junit.MockitoRule;
-//import de.bogenliga.application.common.component.dao.BasicDAO;
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.*;
-//import static org.mockito.Mockito.*;
-//
-//
-///**
-// * TODO [AL] class documentation
-// *
-// * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
-// */
-//public class TriggerServiceTest {
-//
-//	private LocalDateTime currentTime = LocalDateTime.now();
-//
-//	@Rule
-//	public MockitoRule mockitoRule = MockitoJUnit.rule();
-//
-//	@Mock
-//	private BasicDAO basicDao;
-//
-//	@Mock
-//	private TriggerService triggerService;
-//
-//	@InjectMocks
-//	private TriggerService triggerTest;
-//
-//
-//	// To be changed
-//	//@Test
-//	public void testSync(){
-//
-//	}
-//}
+package de.bogenliga.application.services.v1.trigger.service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import de.bogenliga.application.business.liga.api.LigaComponent;
+import de.bogenliga.application.business.trigger.impl.dao.TriggerDAO;
+import de.bogenliga.application.common.component.dao.BasicDAO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+/**
+ * TODO [AL] class documentation
+ *
+ * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
+ */
+public class TriggerServiceTest {
+
+	private LocalDateTime currentTime = LocalDateTime.now();
+
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+	@Mock
+	private BasicDAO basicDao;
+	@Mock
+	private TriggerDAO triggerDAO;
+	@Mock
+	private LigaComponent ligaComponent;
+	@InjectMocks
+	private TriggerService triggerTest;
+
+
+	// To be changed
+	//@Test
+	public void testSync(){
+
+	}
+}
