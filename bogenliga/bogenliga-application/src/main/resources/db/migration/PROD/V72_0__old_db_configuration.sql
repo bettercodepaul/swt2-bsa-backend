ed16bc14a28c144e523c7bd13b6936f177d9ee68
R4IDx <robin.steiner@student.reutlingen-university.de>
2024-01-25 10:03:20 +0100

    ticket#1501maventest - V72 updated

--- bogenliga/bogenliga-application/src/main/resources/db/migration/PROD/V72_0__old_db_configuration.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/PROD/V72_0__old_db_configuration.sql
@@ -6,4 +6,11 @@ INSERT INTO configuration(configuration_key, configuration_value) VALUES ('OLDDB
 INSERT INTO configuration(configuration_key, configuration_value) VALUES ('OLDDBPort', 'default');
 INSERT INTO configuration(configuration_key, configuration_value) VALUES ('OLDDBName', 'default');
 
+
+UPDATE configuration SET configuration_value = '*****' WHERE configuration_key = 'OLDDBBenutzer';
+UPDATE configuration SET configuration_value = '*****' WHERE configuration_key = 'OLDDBPassword';
+UPDATE configuration SET configuration_value = '*****' WHERE configuration_key = 'OLDDBHost';
+UPDATE configuration SET configuration_value = '*****' WHERE configuration_key = 'OLDDBPort';
+UPDATE configuration SET configuration_value = '*****' WHERE configuration_key = 'OLDDBName';
+
 COMMIT;
