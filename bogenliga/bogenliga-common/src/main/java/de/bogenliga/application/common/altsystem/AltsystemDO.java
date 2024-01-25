9dcf757647eaed0e9bdc56593ef0e88ced508887
wieseth <theo.wiese@student.reutlingen-university.de>
2024-01-25 11:49:08 +0100

    determine trigger operation (create/update) from timestamp

--- bogenliga/bogenliga-common/src/main/java/de/bogenliga/application/common/altsystem/AltsystemDO.java
+++ bogenliga/bogenliga-common/src/main/java/de/bogenliga/application/common/altsystem/AltsystemDO.java
@@ -1,12 +1,14 @@
 package de.bogenliga.application.common.altsystem;
 
+import de.bogenliga.application.common.component.types.CommonDataObject;
+
 /**
  * Parent class to store the imported data of entities of Bogenliga.de
  * Entity-specific fields have to be added in the concrete class
  *
  * @author Andre Lehnert, eXXcellent solutions consulting & software gmbh
  */
-public abstract class AltsystemDO {
+public abstract class AltsystemDO extends CommonDataObject {
     long id;
 
     public void setId(long id){
