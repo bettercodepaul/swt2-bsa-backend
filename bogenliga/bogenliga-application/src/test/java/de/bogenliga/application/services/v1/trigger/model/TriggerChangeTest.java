768a113e81c17bcb4c001a7763de4be9450b485c
wieseth <theo.wiese@student.reutlingen-university.de>
2024-01-26 02:43:46 +0100

    apply changes to tests

--- bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/model/TriggerChangeTest.java
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/trigger/model/TriggerChangeTest.java
@@ -1,12 +1,15 @@
 package de.bogenliga.application.services.v1.trigger.model;
 
+import java.time.OffsetDateTime;
 import org.junit.Rule;
-import org.mockito.InjectMocks;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import de.bogenliga.application.business.trigger.api.types.TriggerChangeStatus;
+import de.bogenliga.application.business.trigger.api.TriggerComponent;
 import de.bogenliga.application.business.trigger.api.types.TriggerChangeOperation;
+import de.bogenliga.application.business.trigger.api.types.TriggerChangeStatus;
+import de.bogenliga.application.business.trigger.api.types.TriggerDO;
 import de.bogenliga.application.common.altsystem.AltsystemDO;
 import de.bogenliga.application.common.altsystem.AltsystemEntity;
 import static org.junit.Assert.*;
@@ -17,57 +20,64 @@ import static org.junit.Assert.*;
  */
 public class TriggerChangeTest<T extends AltsystemDO> {
 
+	private static final long triggeringUserId = 21;
 
-	private static final TriggerChangeStatus isNew = TriggerChangeStatus.NEW;
-	private static final TriggerChangeStatus isInProgress = TriggerChangeStatus.IN_PROGRESS;
-	private static final TriggerChangeStatus isError= TriggerChangeStatus.ERROR;
-	private static final TriggerChangeStatus isSuccess = TriggerChangeStatus.SUCCESS;
-	private static final TriggerChangeOperation isUpdate = TriggerChangeOperation.UPDATE;
-	private static final TriggerChangeOperation isCreate = TriggerChangeOperation.CREATE;
+	private static final Long ID = 4L;
+	private static final String KATEGORIE = "re";
+	private static final Long ALTSYSTEM_ID = 5L;
+	private static final TriggerChangeOperation OPERATION = null;
+	private static final TriggerChangeStatus STATUS = null;
+	private static final String NACHRICHT = "ree";
+	private static final OffsetDateTime CREATED_AT_UTC = OffsetDateTime.MIN;
+	private static final OffsetDateTime RUN_AT_UTC = OffsetDateTime.MIN;
 
 
 	@Rule
 	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
 	@Mock
-	public AltsystemDO altsystemDO;
-
+	private TriggerComponent triggerComponent;
 	@Mock
-	public AltsystemEntity altsystemEntity;
+	public AltsystemEntity<T> altsystemEntity;
+	@Mock
+	public TriggerDO triggerDO;
+	@Mock
+	public T altsystemDataObject;
 
-	@InjectMocks
-	public TriggerChange triggerChange;
 
 
-	//NEW, IN_PROGRESS, ERROR, SUCCESS
-	//@Test
-	public void testTryMigration(){
-		//TODO
+	private TriggerDO getExpectedDO(){
+		return new TriggerDO(ID, KATEGORIE, ALTSYSTEM_ID, OPERATION, STATUS, NACHRICHT, CREATED_AT_UTC, RUN_AT_UTC);
+	}
+
+	private TriggerChange getExpectedTC(){
+		return new TriggerChange(triggerComponent, triggerDO, altsystemDataObject, altsystemEntity, triggeringUserId);
+	}
+
+	@Test
+	public void testTryMigration() {
+
+		TriggerDO triggerData = getExpectedDO();
+
+		TriggerChange<T> triggerChange = new TriggerChange<>(triggerComponent, triggerData, altsystemDataObject, altsystemEntity, triggeringUserId);
+
+		boolean result = triggerChange.tryMigration();
+
+		assertFalse(result);
+		assertEquals(TriggerChangeStatus.ERROR, triggerChange.getState());
 	}
 
 
-	//@Test
-	public void testGetSetState(){
+	@Test
+	public void testGetAltsystemDataObject(){
+		TriggerChange actual = getExpectedTC();
+		AltsystemDO actualAltsystemDataObject = actual.getAltsystemDataObject();
 
-		TriggerChangeStatus actual = triggerChange.getState();
-		assertNull(actual);
-
-		triggerChange.setState(TriggerChangeStatus.NEW);
-		actual = triggerChange.getState();
-		assertEquals(isNew, actual);
-
-		triggerChange.setState(TriggerChangeStatus.IN_PROGRESS);
-		actual = triggerChange.getState();
-		assertEquals(isInProgress, actual);
-
-		triggerChange.setState(TriggerChangeStatus.ERROR);
-		actual = triggerChange.getState();
-		assertEquals(isError, actual);
-
-		triggerChange.setState(TriggerChangeStatus.SUCCESS);
-		actual = triggerChange.getState();
-		assertEquals(isSuccess, actual);
+		assertEquals(altsystemDataObject, actualAltsystemDataObject);
 	}
-
 
 }
+
+
+
+
