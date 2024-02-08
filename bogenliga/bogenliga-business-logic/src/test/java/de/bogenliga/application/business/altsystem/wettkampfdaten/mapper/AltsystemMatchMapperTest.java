0b294c46ef971aceb1c0565cfc73ca997d140ba0
Thomas Hesse <thomas.hesse@student.reutlingen-university.de>
2024-02-08 14:12:58 +0100

    AltsystemMatchMapper Test

--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/altsystem/wettkampfdaten/mapper/AltsystemMatchMapperTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/altsystem/wettkampfdaten/mapper/AltsystemMatchMapperTest.java
@@ -1,5 +1,7 @@
 package de.bogenliga.application.business.altsystem.wettkampfdaten.mapper;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -12,6 +14,7 @@ import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUeberse
 import de.bogenliga.application.business.altsystem.wettkampfdaten.dataobject.AltsystemWettkampfdatenDO;
 import de.bogenliga.application.business.match.api.MatchComponent;
 import de.bogenliga.application.business.match.api.types.MatchDO;
+import de.bogenliga.application.business.wettkampf.api.types.WettkampfDO;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -31,6 +34,26 @@ public class AltsystemMatchMapperTest {
     @InjectMocks
     private AltsystemMatchMapper altsystemMatchMapper;
 
+    public List<MatchDO> getMockMatches(){
+        List<MatchDO> matches = new LinkedList<>();
+        matches.add(new MatchDO());
+        matches.add(new MatchDO());
+        matches.add(new MatchDO());
+        matches.add(new MatchDO());
+        return matches;
+    }
+
+    public List<WettkampfDO> getMockWettkaempfe(){
+        List<WettkampfDO> wettkaempfe = new LinkedList<>();
+        for (long i = 0; i < 4; i++){
+            WettkampfDO wettkampfDO = new WettkampfDO();
+            wettkampfDO.setId(i + 1);
+            wettkampfDO.setWettkampfTag(i + 1);
+            wettkaempfe.add(wettkampfDO);
+        }
+        return wettkaempfe;
+    }
+
     @Test
     public void testToDO() {
         // prepare test data
@@ -45,6 +68,13 @@ public class AltsystemMatchMapperTest {
         altsystemWettkampfdatenDO.setSatzMinus(2);
         altsystemWettkampfdatenDO.setMatchPlus(2);
         altsystemWettkampfdatenDO.setMatchMinus(0);
+        altsystemWettkampfdatenDO.setSatz1(10);
+        altsystemWettkampfdatenDO.setSatz2(10);
+        altsystemWettkampfdatenDO.setSatz3(10);
+        altsystemWettkampfdatenDO.setSatz4(10);
+        altsystemWettkampfdatenDO.setSatz5(10);
+        altsystemWettkampfdatenDO.setSaisonID(2);
+        altsystemWettkampfdatenDO.setSec(1);
         // mock Uebersetzung
         AltsystemUebersetzungDO mannschaftUebersetzung = new AltsystemUebersetzungDO();
         mannschaftUebersetzung.setBogenligaId(10L);
@@ -88,4 +118,72 @@ public class AltsystemMatchMapperTest {
         assertThat(actual[1].getNr()).isEqualTo(expected[1].getNr());
     }
 
+    @Test
+    public void testAddDefaultFields() {
+        // existing matches for Wettkampf
+        List<MatchDO> matches = getMockMatches();
+        List<WettkampfDO> wettkaempfe = getMockWettkaempfe();
+
+        // configure mocks
+        when(matchComponent.findByWettkampfId(anyLong())).thenReturn(matches);
+
+
+        // call test method
+        MatchDO[] actual = new MatchDO[2];
+        actual[0] = new MatchDO();
+        actual[0].setNr(14L);
+        actual[1] = new MatchDO();
+        actual = altsystemMatchMapper.addDefaultFields(actual, wettkaempfe);
+
+        // assert result
+        for(MatchDO match : actual){
+            assertThat(match.getWettkampfId()).isEqualTo(2L);
+            assertThat(match.getBegegnung()).isEqualTo((matches.size() % 4) + 1);
+            assertThat(match.getStrafPunkteSatz1()).isEqualTo(0L);
+            assertThat(match.getStrafPunkteSatz2()).isEqualTo(0L);
+            assertThat(match.getStrafPunkteSatz3()).isEqualTo(0L);
+            assertThat(match.getStrafPunkteSatz4()).isEqualTo(0L);
+            assertThat(match.getStrafPunkteSatz5()).isEqualTo(0L);
+        }
+
+        assertThat(actual[0].getMatchScheibennummer()).isEqualTo((matches.size() % 8) + 1);
+        assertThat(actual[1].getMatchScheibennummer()).isEqualTo((matches.size() % 8) + 2);
+
+    }
+
+    @Test
+    public void getFirstWettkampfTag(){
+        MatchDO matchDO = new MatchDO();
+        matchDO.setNr(7L);
+        WettkampfDO wettkampfDO = altsystemMatchMapper.getCurrentWettkampfTag(matchDO, getMockWettkaempfe());
+
+        assertThat(wettkampfDO.getWettkampfTag()).isEqualTo(1L);
+    }
+
+    @Test
+    public void getSecondWettkampfTag(){
+        MatchDO matchDO = new MatchDO();
+        matchDO.setNr(14L);
+        WettkampfDO wettkampfDO = altsystemMatchMapper.getCurrentWettkampfTag(matchDO, getMockWettkaempfe());
+
+        assertThat(wettkampfDO.getWettkampfTag()).isEqualTo(2L);
+    }
+
+    @Test
+    public void getThirdWettkampfTag(){
+        MatchDO matchDO = new MatchDO();
+        matchDO.setNr(21L);
+        WettkampfDO wettkampfDO = altsystemMatchMapper.getCurrentWettkampfTag(matchDO, getMockWettkaempfe());
+
+        assertThat(wettkampfDO.getWettkampfTag()).isEqualTo(3L);
+    }
+
+    @Test
+    public void getLastWettkampfTag(){
+        MatchDO matchDO = new MatchDO();
+        matchDO.setNr(28L);
+        WettkampfDO wettkampfDO = altsystemMatchMapper.getCurrentWettkampfTag(matchDO, getMockWettkaempfe());
+
+        assertThat(wettkampfDO.getWettkampfTag()).isEqualTo(4L);
+    }
 }
