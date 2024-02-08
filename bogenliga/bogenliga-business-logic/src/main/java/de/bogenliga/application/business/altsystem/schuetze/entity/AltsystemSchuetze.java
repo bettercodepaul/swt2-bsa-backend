1d5172bc3c5c446b5f9a3313debce8c1c66d09ec
Michael Ghebremussie <michael.ghebremussie@student.reutlingen-university.de>
2024-02-08 13:35:23 +0100

    findByWert anpassung

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/schuetze/entity/AltsystemSchuetze.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/altsystem/schuetze/entity/AltsystemSchuetze.java
@@ -1,13 +1,18 @@
 package de.bogenliga.application.business.altsystem.schuetze.entity;
 
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import de.bogenliga.application.business.altsystem.schuetze.dataobject.AltsystemSchuetzeDO;
 import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzung;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzungDAO;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzungDO;
+import de.bogenliga.application.business.altsystem.uebersetzung.AltsystemUebersetzungKategorie;
 import de.bogenliga.application.business.dsbmitglied.api.DsbMitgliedComponent;
 import de.bogenliga.application.business.dsbmitglied.api.types.DsbMitgliedDO;
 import de.bogenliga.application.common.altsystem.AltsystemEntity;
 import de.bogenliga.application.business.altsystem.schuetze.mapper.AltsystemSchuetzeMapper;
+import de.bogenliga.application.common.component.dao.BasicDAO;
 
 
 /**
@@ -33,13 +38,40 @@ public class AltsystemSchuetze implements AltsystemEntity<AltsystemSchuetzeDO> {
 
 
     @Override
-    public void create(AltsystemSchuetzeDO altsystemSchuetzeDO, long currentUserId) {
-        // Map data to new object, add default fields
+    public void create(AltsystemSchuetzeDO altsystemSchuetzeDO, long currentUserId) throws SQLException {
+        // leeres Objekt erstellen
         DsbMitgliedDO dsbMitgliedDO = new DsbMitgliedDO();
 
-        // dsbMitgliedDO = altsystemSchuetzeMapper.toDO(dsbMitgliedDO, altsystemSchuetzeDO);
+        // identifier speichern
+        String parsedIdentifier = altsystemSchuetzeMapper.getIdentifier(altsystemSchuetzeDO);
 
-        //dsbMitgliedDO = altsystemSchuetzeMapper.addDefaultFields(dsbMitgliedDO);
+        // Objekt anhand des identifier zurückgeben
+        AltsystemUebersetzungDO altsystemUebersetzungDO = altsystemSchuetzeMapper.getDsbMitgliedDO(parsedIdentifier);
+
+
+        // wenn es noch keinen identifier in der UbersetzungsTabelle gibt
+        if (altsystemUebersetzungDO == null) {
+            // dann schreib Daten in diesen schuetzen rein
+            dsbMitgliedDO = altsystemSchuetzeMapper.toDO(dsbMitgliedDO, altsystemSchuetzeDO);
+
+            dsbMitgliedDO = dsbMitgliedComponent.create(dsbMitgliedDO, currentUserId);
+
+            altsystemUebersetzung.updateOrInsertUebersetzung(AltsystemUebersetzungKategorie.Schütze_Verein, altsystemSchuetzeDO.getId(),
+                    altsystemUebersetzungDO.getBogenligaId(), altsystemUebersetzungDO.getWert());
+
+        }
+        else {
+            String mannschaftName = String.valueOf(altsystemUebersetzung.findByAltsystemID(
+                    AltsystemUebersetzungKategorie.Schütze_Mannschaft, altsystemSchuetzeDO.getMannschaft_id()));
+
+            // TODO Hier noch die Kategorie Schuetze_Ergebnis
+            altsystemUebersetzung.updateOrInsertUebersetzung(AltsystemUebersetzungKategorie.Schütze_Mannschaft, altsystemSchuetzeDO.getMannschaft_id(),
+                    altsystemSchuetzeDO.getMannschaft_id(), mannschaftName);
+
+
+        }
+
+
     }
 
     @Override
