2a8dfb02c803cbfd64015ebbb709944255aad3b6
ellie-okay <elliewandel@gmail.com>
2024-04-23 12:28:31 +0200

    Made the handling of braces bidirectional and modified tests accordingly

--- bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/schuetzenstatistik/api/types/SchuetzenstatistikDOTest.java
+++ bogenliga/bogenliga-business-logic/src/test/java/de/bogenliga/application/business/schuetzenstatistik/api/types/SchuetzenstatistikDOTest.java
@@ -24,11 +24,11 @@ public class SchuetzenstatistikDOTest {
     private static final String dsbMitgliedName = "Mitglied_Name";
     private static final int rueckenNummer = 5;
     private static final float pfeilpunkteSchnitt = (float) 3.7;
-    private static final String schuetzeSatz1 = "10,9";
-    private static final String schuetzeSatz2 = "4,10";
-    private static final String schuetzeSatz3 = "10,6";
-    private static final String schuetzeSatz4 = "3,8";
-    private static final String schuetzeSatz5 = "8,7";
+    private static final String schuetzeSatz1 = "{10,9}";
+    private static final String schuetzeSatz2 = "{4,10}";
+    private static final String schuetzeSatz3 = "{10,6}";
+    private static final String schuetzeSatz4 = "{3,8}";
+    private static final String schuetzeSatz5 = "{8,7}";
 
 
     @Rule
