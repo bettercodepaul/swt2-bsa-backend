d41c454ab2d49f6f1daff926f00846135c4ac1e7
mki4 <tomislav.lucic@stundent.reutlingen-university.de>
2024-04-25 00:58:25 +0200

    Warteschlange als Darstellung einer Veranstaltung funktioniert mit Anzahl Mannschaften = 1000

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V49_0__insert_maximum_size_column_to_veranstalung.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V49_0__insert_maximum_size_column_to_veranstalung.sql
@@ -3,4 +3,4 @@ ALTER TABLE veranstaltung
 
 ALTER TABLE veranstaltung
     ADD COLUMN veranstaltung_groesse INT DEFAULT 8
-        CHECK ( veranstaltung_groesse = 4 OR veranstaltung_groesse = 6 OR veranstaltung_groesse = 8 );
+        CHECK ( veranstaltung_groesse = 4 OR veranstaltung_groesse = 6 OR veranstaltung_groesse = 8 OR veranstaltung_groesse = 1000);
