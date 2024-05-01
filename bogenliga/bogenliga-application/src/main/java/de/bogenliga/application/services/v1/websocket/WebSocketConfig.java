be1703d21edeba0d59ee37a0768aeba4e260c3ef
enzoAbbas <enzofamboupe@gmail.com>
2024-05-01 13:13:37 +0200

    Websocket implementation with Stomp Enzo + Dome

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/websocket/WebSocketConfig.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/websocket/WebSocketConfig.java
@@ -1,29 +1,24 @@
 package de.bogenliga.application.services.v1.websocket;
 
-import java.util.concurrent.Executors;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(tradeWebSocketHandler(), "/websocket").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
-    @Bean
-    public org.springframework.web.socket.WebSocketHandler tradeWebSocketHandler() {
-        return new WebSocketHandler();
-    }
-
-    @Bean
-    public TaskScheduler taskScheduler() {
-        return new ConcurrentTaskScheduler(Executors.newSingleThreadScheduledExecutor());
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
     }
 }
