d647de320f788b8f83cc7b4e84dfabbe6ae1e751
Lennart Raach <Lennart.Raach@student.reutlingen-university.de>
2024-05-20 21:38:09 +0200

    adjusted SchuetzenstatistikMatchBE and MatchDO

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schuetzenstatistikmatch/impl/entity/SchuetzenstatistikMatchBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schuetzenstatistikmatch/impl/entity/SchuetzenstatistikMatchBE.java
@@ -30,96 +30,74 @@ public class SchuetzenstatistikMatchBE extends CommonBusinessEntity implements B
         return rueckennummer;
     }
 
-
-    public void setRueckennummer(int rueckennummer) {
-        this.rueckennummer = rueckennummer;
-    }
-
-
     public String getDsbMitgliedName() {
         return dsbMitgliedName;
     }
 
-
-    public void setDsbMitgliedName(String dsbMitgliedName) {
-        this.dsbMitgliedName = dsbMitgliedName;
-    }
-
-
     public float getMatch1() {
         return match1;
     }
-
-
-    public void setMatch1(float match1) {
-        this.match1 = match1;
-    }
-
-
     public float getMatch2() {
         return match2;
     }
-
-
-    public void setMatch2(float match2) {
-        this.match2 = match2;
-    }
-
-
     public float getMatch3() {
         return match3;
     }
 
-
-    public void setMatch3(float match3) {
-        this.match3 = match3;
-    }
-
-
     public float getMatch4() {
         return match4;
     }
-
-
-    public void setMatch4(float match4) {
-        this.match4 = match4;
-    }
-
-
     public float getMatch5() {
         return match5;
     }
-
-
-    public void setMatch5(float match5) {
-        this.match5 = match5;
-    }
-
 
     public float getMatch6() {
         return match6;
     }
 
-
-    public void setMatch6(float match6) {
-        this.match6 = match6;
-    }
-
-
     public float getMatch7() {
         return match7;
     }
-
-
-    public void setMatch7(float match7) {
-        this.match7 = match7;
-    }
-
 
     public float getPfeilpunkteSchnitt() {
         return pfeilpunkteSchnitt;
     }
 
+    public void setRueckennummer(int rueckennummer) {
+        this.rueckennummer = rueckennummer;
+    }
+
+    public void setDsbMitgliedName(String dsbMitgliedName) {
+        this.dsbMitgliedName = dsbMitgliedName;
+    }
+
+    public void setMatch1(float match1) {
+        this.match1 = match1;
+    }
+
+    public void setMatch2(float match2) {
+        this.match2 = match2;
+    }
+
+    public void setMatch3(float match3) {
+        this.match3 = match3;
+    }
+
+    public void setMatch4(float match4) {
+        this.match4 = match4;
+    }
+
+    public void setMatch5(float match5) {
+        this.match5 = match5;
+    }
+
+    public void setMatch6(float match6) {
+        this.match6 = match6;
+    }
+
+    public void setMatch7(float match7) {
+        this.match7 = match7;
+    }
 
     public void setPfeilpunkteSchnitt(float pfeilpunkteSchnitt) {
         this.pfeilpunkteSchnitt = pfeilpunkteSchnitt;
