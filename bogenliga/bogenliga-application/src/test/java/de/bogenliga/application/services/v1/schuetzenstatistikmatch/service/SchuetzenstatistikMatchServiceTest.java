dd93ae28e4f674a101b054b903730d17a7841489
Lennart Raach <Lennart.Raach@student.reutlingen-university.de>
2024-05-20 21:29:17 +0200

    adjusted SchuetzenstatistikMatchBE

---
+++ bogenliga/bogenliga-application/src/test/java/de/bogenliga/application/services/v1/schuetzenstatistikmatch/service/SchuetzenstatistikMatchServiceTest.java
@@ -0,0 +1,9 @@
+package de.bogenliga.application.services.v1.schuetzenstatistikmatch.service;
+
+/**
+ * Tests the SchuetzenstatistikMatchService
+ *
+ * @author Lennart Raach
+ */
+public class SchuetzenstatistikMatchServiceTest {
+}
