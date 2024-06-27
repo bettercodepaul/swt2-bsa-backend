0fc37dd8149c98b7581a93834f0a85fc69afafba
Konisonis <35600104+Konisonis@users.noreply.github.com>
2024-06-27 13:29:18 +0200

    Update V11_0__alter_table_dsb_mitglied_add_column_dsb_mitglied_beitrittsdatum.sql

--- bogenliga/bogenliga-application/src/main/resources/db/migration/all/V11_0__alter_table_dsb_mitglied_add_column_dsb_mitglied_beitrittsdatum.sql
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V11_0__alter_table_dsb_mitglied_add_column_dsb_mitglied_beitrittsdatum.sql
@@ -1,2 +1,2 @@
 ALTER TABLE dsb_mitglied
-ADD COLUMN dsb_mitglied_beitrittsdatum date NOT NULL DEFAULT current_date;
+    ADD COLUMN IF NOT EXISTS dsb_mitglied_beitrittsdatum date NOT NULL DEFAULT current_date;
