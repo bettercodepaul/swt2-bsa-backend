9ae355a54f50a53a30b0e9c356b555c2232ca365
Sarah Keifer <sarah_andrea.keifer@student.reutlingen>
2024-10-07 16:27:20 +0200

    Remove this unused import 'java.util.stream.Collectors'

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/user/service/UserService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/user/service/UserService.java
@@ -41,6 +41,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * I´m a REST resource and handle configuration CRUD requests over the HTTP protocol.
  *
