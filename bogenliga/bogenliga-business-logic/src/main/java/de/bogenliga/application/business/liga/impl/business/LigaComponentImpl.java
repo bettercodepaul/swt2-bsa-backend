bf5243d59dbb7cba4eb62c03aca248125b74d74d
Justin Klein <klein.justin@gmx.de>
2024-12-04 13:00:32 +0100

    Performance Improvement for the Liga Übersicht and Related Tests

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/business/LigaComponentImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/liga/impl/business/LigaComponentImpl.java
@@ -7,9 +7,9 @@ import de.bogenliga.application.business.disziplin.api.DisziplinComponent;
 import de.bogenliga.application.business.liga.api.LigaComponent;
 import de.bogenliga.application.business.liga.api.types.LigaDO;
 import de.bogenliga.application.business.liga.impl.dao.LigaDAO;
-import de.bogenliga.application.business.liga.impl.dao.LigaExtendedDAO;
+import de.bogenliga.application.business.liga.impl.dao.LigaDAOext;
 import de.bogenliga.application.business.liga.impl.entity.LigaBE;
-import de.bogenliga.application.business.liga.impl.entity.LigaExtendedBE;
+import de.bogenliga.application.business.liga.impl.entity.LigaBEext;
 import de.bogenliga.application.business.liga.impl.mapper.LigaMapper;
 import de.bogenliga.application.business.regionen.api.RegionenComponent;
 import de.bogenliga.application.business.user.api.UserComponent;
@@ -33,19 +33,19 @@ public class LigaComponentImpl implements LigaComponent {
     private static final String PRECONDITION_MSG_CURRENT_LIGA_ID = "Current liga id must not be Null";
 
     private final LigaDAO ligaDAO;
-    private final LigaExtendedDAO ligaExtendedDAO;
+    private final LigaDAOext ligaExtendedDAO;
 
 
     @Autowired
     public LigaComponentImpl(final LigaDAO ligaDAO, @Lazy final RegionenComponent regionenComp, @Lazy final UserComponent userComp, @Lazy final DisziplinComponent disziplinComp,
-                             LigaExtendedDAO ligaExtendedDAO) {
+                             LigaDAOext ligaExtendedDAO) {
         this.ligaDAO = ligaDAO;
         this.ligaExtendedDAO = ligaExtendedDAO;
     }
 
     @Override
     public List<LigaDO> findAll() {
-        final List<LigaExtendedBE> ligaExtendedBEList = ligaExtendedDAO.findEverything();
+        final List<LigaBEext> ligaExtendedBEList = ligaExtendedDAO.findEverything();
 
         return ligaExtendedBEList.stream()
                 .map(LigaMapper::mapToLigaDO)
@@ -54,7 +54,7 @@ public class LigaComponentImpl implements LigaComponent {
 
     @Override
     public List<LigaDO> findBySearch(final String searchTerm){
-        final List<LigaExtendedBE> ligaExtendedBEList = ligaExtendedDAO.findBySearch(searchTerm);
+        final List<LigaBEext> ligaExtendedBEList = ligaExtendedDAO.findBySearch(searchTerm);
 
         return ligaExtendedBEList.stream()
                 .map(LigaMapper::mapToLigaDO)
@@ -168,10 +168,10 @@ public class LigaComponentImpl implements LigaComponent {
 
 
     private LigaDO completeLiga(LigaBE ligaBE) {
-        LigaExtendedBE additionalData = ligaExtendedDAO.findAdditionalDataByLigaId(ligaBE.getLigaId());
+        LigaBEext additionalData = ligaExtendedDAO.findAdditionalDataByLigaId(ligaBE.getLigaId());
 
         if (additionalData == null) {
-            additionalData = new LigaExtendedBE();
+            additionalData = new LigaBEext();
         }
 
         return LigaMapper.toLigaDO(ligaBE, additionalData);
