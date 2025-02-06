d2525b759456b7a9e8b08ed5f868c0b4a0c05833
Sarah Keifer <sarah_andrea.keifer@student.reutlingen>
2025-02-06 20:47:03 +0100

    Update DSB Mitglied Berechtigung

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V23_0__update_DSB_Mitglied.sql
@@ -0,0 +1,11 @@
+/*
+Add Role Can create DSB-Mitglied for Ausrichter for their own Veranstaltung
+*/
+/* Ausrichter, DSB-Mitglied anlegen und DSB-Mitglied bearbeiten für seinen eigenen Verein*/
+INSERT INTO rolle_recht(rolle_recht_rolle_id,
+                        rolle_recht_recht_id)
+VALUES (4, 22);
+
+INSERT INTO rolle_recht(rolle_recht_rolle_id,
+                        rolle_recht_recht_id)
+VALUES (4, 10);
