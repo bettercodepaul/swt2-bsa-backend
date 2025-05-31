6fe1646f4291bc8292b938a3b5f2659faa8753aa
mklemmingen <104225647+mklemmingen@users.noreply.github.com>
2025-05-31 16:46:28 +0200

    added sonar cube warning supresses

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schusszettel/api/types/inside/WettkampfInfoDO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schusszettel/api/types/inside/WettkampfInfoDO.java
@@ -6,8 +6,11 @@ import java.sql.Date;
  * Business-Objekt zur Darstellung von Wettkampf-Informationen.
  * Wird im Business-Layer für den Tablet-Schusszettel verwendet.
  *
+ * @SuppressWarnings("java:S4144") // Suppress SonarQube duplication warning
+ *
  * @author Marty Lauterbach
  */
+@SuppressWarnings("squid:S4144")
 public class WettkampfInfoDO {
 
     private Long wettkampfId;
