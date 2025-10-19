1e0d8e6a2c88893260c894d664df6b7ddd68e561
Michael Dirksmöller <michael.dirksmoeller@exxcellent.de>
2025-10-19 05:13:55 +0200

    Funktion zum Kopieren einer Mannschaft (Zielveranstaltung null) ergänzt

---
+++ bogenliga/bogenliga-application/src/main/resources/db/migration/all/V27_0__alter_mannschaften_dublicate_constrain.sql
@@ -0,0 +1,6 @@
+-- limit the number of copies in table mannschaften
+-- by adding a constraint of a unique key (verein_id & nummer & veranstaltung_id
+-- this ia a week limit, but it will limit the numer of copies without assigment to a league to 1
+
+ALTER TABLE mannschaft
+    ADD CONSTRAINT cu_mannschaft_veranstaltung UNIQUE (mannschaft_verein_id, mannschaft_nummer, mannschaft_veranstaltung_id);
