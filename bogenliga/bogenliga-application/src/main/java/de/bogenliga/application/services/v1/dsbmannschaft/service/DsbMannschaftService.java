d9ccea26396440c7eb0abe41a2c7bba98049acb3
bedanc25 <daniel.betz@student.reutlingen-university.de>
2026-03-18 14:57:31 +0100

    loged nullpointer exception

--- bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/service/DsbMannschaftService.java
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/dsbmannschaft/service/DsbMannschaftService.java
@@ -350,7 +350,9 @@ public class DsbMannschaftService implements ServiceFacade {
                         (long) i+1);
                 list.add(mannschaftsMitgliedDTO);
             }
-        }catch (NullPointerException ignored) {}
+        } catch (NullPointerException e) {
+            LOG.warn("NullPointerException in createMannschaftsMitgliedForPlatzhalter: {}", e.getMessage());
+        }
     }
 
 
