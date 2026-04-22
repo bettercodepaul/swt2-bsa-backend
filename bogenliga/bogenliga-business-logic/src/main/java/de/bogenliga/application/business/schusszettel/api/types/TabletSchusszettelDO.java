fac9000df271dd38e4b21f506b8aa23d4b472a1d
Maik Pinto <pintomaik@gmail.com>
2026-04-22 21:41:28 +0200

    Bei TabletSchusszettel Scheibennummer hinzugefügt

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schusszettel/api/types/TabletSchusszettelDO.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/schusszettel/api/types/TabletSchusszettelDO.java
@@ -47,6 +47,7 @@ public class TabletSchusszettelDO {
     // Match-IDs für Frontend-Navigation
     private Long eigenesTeamMatchId;
     private Long gegnerischesTeamMatchId;
+    private Long eigenesTeamScheibennummer;
 
     // Getter/Setter
     public TabletSchusszettelStatus getStatus() { return status; }
@@ -86,6 +87,9 @@ public class TabletSchusszettelDO {
 
     public Long getGegnerischesTeamMatchId() { return gegnerischesTeamMatchId; }
     public void setGegnerischesTeamMatchId(Long gegnerischesTeamMatchId) { this.gegnerischesTeamMatchId = gegnerischesTeamMatchId; }
+
+    public Long getEigenesTeamScheibennummer() { return eigenesTeamScheibennummer; }
+    public void setEigenesTeamScheibennummer(Long eigenesTeamScheibennummer) { this.eigenesTeamScheibennummer = eigenesTeamScheibennummer; }
 
     /**
      * Statuswerte, die den aktuellen Zustand der Tablet-Eingabemaske repräsentieren.
