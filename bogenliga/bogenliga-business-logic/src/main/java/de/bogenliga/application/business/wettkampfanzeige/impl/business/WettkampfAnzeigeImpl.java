db04c80141d691ae736294b2135b99c1d7135d99
Mira Dietschmann <miradietschmann@gmail.com>
2026-05-25 19:35:52 +0200

    Fixed SonarQube issues

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampfanzeige/impl/business/WettkampfAnzeigeImpl.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampfanzeige/impl/business/WettkampfAnzeigeImpl.java
@@ -2,19 +2,20 @@ package de.bogenliga.application.business.wettkampfanzeige.impl.business;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 /**
  * @author Mira Dietschmann, mira.dietschmann@student.reutlingen-university.de
  */
 @Component
 public class WettkampfAnzeigeImpl {
+    private final SecureRandom random = new SecureRandom();
+
     public String generatePhysischeBildschirmId() {
         // Alpha-numeric characters
         final String validCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
                 "abcdefghijklmnopqrstuvwxyz" +
                 "0123456789";
-        Random random = new Random();
         char[] characters = new char[4];
         for (int i = 0; i < characters.length; i++) {
             final int characterInt = random.nextInt(validCharacters.length());
