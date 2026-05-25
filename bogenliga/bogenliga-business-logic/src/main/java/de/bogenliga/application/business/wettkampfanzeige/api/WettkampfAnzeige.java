52f59d29c2379ede270dec6175b7ac20af202637
Mira Dietschmann <miradietschmann@gmail.com>
2026-05-25 17:33:58 +0200

    Created rudimentary WettkampfAnzeige interface.

---
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampfanzeige/api/WettkampfAnzeige.java
@@ -0,0 +1,10 @@
+package de.bogenliga.application.business.wettkampfanzeige.api;
+
+/**
+ * I represent anzeigen.
+ *
+ * @author Mira Dietschmann, mira.dietschmann@student.reutlingen-university.de
+ */
+public interface WettkampfAnzeige {
+    public String generatePhysischeBildschirmId();
+}
