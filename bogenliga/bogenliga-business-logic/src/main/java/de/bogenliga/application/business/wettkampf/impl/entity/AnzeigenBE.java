b780a4137faf38089567d471bdb5300e1a9b4516
KaterinaP <mki4@pop-os.localdomain>
2026-06-01 01:25:10 +0200

    Mapper für anzeigen Tabelle erstellt + Nötige Änderungen an Datenklassen

--- bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/impl/entity/AnzeigenBE.java
+++ bogenliga/bogenliga-business-logic/src/main/java/de/bogenliga/application/business/wettkampf/impl/entity/AnzeigenBE.java
@@ -14,6 +14,9 @@ public class AnzeigenBE extends CommonBusinessEntity implements BusinessEntity {
     public Long getId() {
         return id;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getPhysischeBildschirmId() {
         return physischeBildschirmId;
