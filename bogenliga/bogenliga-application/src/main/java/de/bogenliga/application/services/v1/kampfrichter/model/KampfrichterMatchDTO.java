65a9d4c7e5c307f6bb3021190c704c352f9355ee
maxsc <maximilian.schichov@student.reutlingen-university.de>
2026-06-10 22:58:56 +0200

    Fix WARTE deadlock, WETTKAMPF_ENDE enum mismatch, add Kampfrichter session infrastructure

---
+++ bogenliga/bogenliga-application/src/main/java/de/bogenliga/application/services/v1/kampfrichter/model/KampfrichterMatchDTO.java
@@ -0,0 +1,51 @@
+package de.bogenliga.application.services.v1.kampfrichter.model;
+
+import de.bogenliga.application.common.service.types.DataTransferObject;
+
+public class KampfrichterMatchDTO implements DataTransferObject {
+
+    private Long matchId;
+    private Long nr;
+    private Long begegnung;
+    private Long matchScheibennummer;
+    private Long mannschaftId;
+    private String mannschaftName;
+    private Long strafPunkteSatz1;
+    private Long strafPunkteSatz2;
+    private Long strafPunkteSatz3;
+    private Long strafPunkteSatz4;
+    private Long strafPunkteSatz5;
+
+    public Long getMatchId() { return matchId; }
+    public void setMatchId(Long matchId) { this.matchId = matchId; }
+
+    public Long getNr() { return nr; }
+    public void setNr(Long nr) { this.nr = nr; }
+
+    public Long getBegegnung() { return begegnung; }
+    public void setBegegnung(Long begegnung) { this.begegnung = begegnung; }
+
+    public Long getMatchScheibennummer() { return matchScheibennummer; }
+    public void setMatchScheibennummer(Long matchScheibennummer) { this.matchScheibennummer = matchScheibennummer; }
+
+    public Long getMannschaftId() { return mannschaftId; }
+    public void setMannschaftId(Long mannschaftId) { this.mannschaftId = mannschaftId; }
+
+    public String getMannschaftName() { return mannschaftName; }
+    public void setMannschaftName(String mannschaftName) { this.mannschaftName = mannschaftName; }
+
+    public Long getStrafPunkteSatz1() { return strafPunkteSatz1; }
+    public void setStrafPunkteSatz1(Long strafPunkteSatz1) { this.strafPunkteSatz1 = strafPunkteSatz1; }
+
+    public Long getStrafPunkteSatz2() { return strafPunkteSatz2; }
+    public void setStrafPunkteSatz2(Long strafPunkteSatz2) { this.strafPunkteSatz2 = strafPunkteSatz2; }
+
+    public Long getStrafPunkteSatz3() { return strafPunkteSatz3; }
+    public void setStrafPunkteSatz3(Long strafPunkteSatz3) { this.strafPunkteSatz3 = strafPunkteSatz3; }
+
+    public Long getStrafPunkteSatz4() { return strafPunkteSatz4; }
+    public void setStrafPunkteSatz4(Long strafPunkteSatz4) { this.strafPunkteSatz4 = strafPunkteSatz4; }
+
+    public Long getStrafPunkteSatz5() { return strafPunkteSatz5; }
+    public void setStrafPunkteSatz5(Long strafPunkteSatz5) { this.strafPunkteSatz5 = strafPunkteSatz5; }
+}
